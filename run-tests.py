64a9b03ed19e6a982522e0093fd982f37ad93ce0
jesko <rattle@nullteilerfrei.de>
2020-01-09 17:14:18 +0100

    fixes xvfb run command and test runner script

--- run-tests.py
+++ run-tests.py
@@ -5,10 +5,12 @@ Runs all tests from the command line.
 """
 import unittest
 import os
+import sys
 
 os.chdir('test')
 os.environ['REFINERY_VERBOSITY'] = 'DETACHED'
 
 suite = unittest.TestLoader().discover('test')
 tests = unittest.TextTestRunner(verbosity=2)
-tests.run(suite)
+result = tests.run(suite)
+sys.exit(0 if result.wasSuccessful() else 1)
