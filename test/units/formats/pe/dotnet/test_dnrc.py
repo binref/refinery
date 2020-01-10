9153756aa6d94b085758e8a5a2892acc74a7a5c4
jesko <rattle@nullteilerfrei.de>
2020-01-10 18:14:11 +0100

    improves dnrc test

--- test/units/formats/pe/dotnet/test_dnrc.py
+++ test/units/formats/pe/dotnet/test_dnrc.py
@@ -6,6 +6,11 @@ from .... import TestUnitBase
 class TestDotNetDeserializer(TestUnitBase):
 
     def test_real_world_01(self):
+        unit = self.load()
+        sample = self.download_from_malshare('82831deadbb41d00df1f45c1b1e7cb89901531ab784a55171f11c891f92fffaf')
+        self.assertIn(B'b091b52a-98c2-06', unit(sample))
+
+    def test_real_world_02(self):
         unit = self.load('b091b52a-98c2-06')
         sample = self.download_from_malshare('82831deadbb41d00df1f45c1b1e7cb89901531ab784a55171f11c891f92fffaf')
         self.assertEqual(unit(sample), B'29')
