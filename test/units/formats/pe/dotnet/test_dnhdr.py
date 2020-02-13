b05dd51b333c8c2780bdbff0b06b8c25b779dc11
jesko <rattle@nullteilerfrei.de>
2020-02-13 12:19:16 +0100

    fixes typo

--- test/units/formats/pe/dotnet/test_dnhdr.py
+++ test/units/formats/pe/dotnet/test_dnhdr.py
@@ -7,7 +7,7 @@ from .... import TestUnitBase
 
 class TestDotNetHeaderParser(TestUnitBase):
 
-    def test_hawkeye_header(self):
+    def test_hawkeye_header_01(self):
         unit = self.load()
         data = self.download_from_malshare('ee790d6f09c2292d457cbe92729937e06b3e21eb6b212bf2e32386ba7c2ff22c')
         header = json.loads(unit(data))
@@ -19,4 +19,24 @@ class TestDotNetHeaderParser(TestUnitBase):
         )
 
         self.assertEqual('Reborn Stub', streams['Tables']['Assembly'][0]['Name'])
+        self.assertEqual(0x2050, streams['Tables']['FieldRVA'][0]['RVA'])
+
+    def test_hawkeye_header_02(self):
+        unit = self.load()
+        data = self.download_from_malshare('094e7d3e6aebf99663993e342401423dff3f444c332eae0b8f0d9eeda1b809a7')
+        header = json.loads(unit(data))
+        streams = header['Meta']['Streams']
+
+        self.assertIn('75AE5928-D641-49B7-A7D0-768DBD3A3D80', streams['GUID'].values())
+
+        for sentinel in {
+            '_FakeMessageShow',
+            '_FakeMessageTitle',
+            '_FakeMessageText',
+            '_FakeMessageIcon',
+            'RebornX Stub.exe'
+        }:
+            self.assertIn(sentinel, streams['Strings'].values())
+
+        self.assertEqual('RebornX Stub', streams['Tables']['Assembly'][0]['Name'])
         self.assertEqual(0x2050, streams['Tables']['FieldRVA'][0]['RVA'])
