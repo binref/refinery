e119837bdd5f84bdc543138ee2caf72c19e55804
jesko <rattle@nullteilerfrei.de>
2020-02-13 12:04:43 +0100

    adds length of input variable

--- refinery/units/blockwise/blockop.py
+++ refinery/units/blockwise/blockop.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from ...lib.argformats import PythonExpression, number
+from ...lib.argformats import PythonExpression
 from . import ArithmeticUnit, NoNumpy
 
 
-_BLOCK_OPERATION_PARSER = PythonExpression('B', 'A', 'S', 'V')
+_BLOCK_OPERATION_PARSER = PythonExpression('B', 'A', 'S', 'N', 'V')
 
 
 def blockop_expression(definition):
@@ -13,8 +13,8 @@ def blockop_expression(definition):
     parse the expressions that can be passed to `refinery.blockop`. Essentially, these
     are Python expressions which can contain variables `B`, `A`, `S`, and `V`.
     """
-    def wrapper(B, S, *V):
-        return wrapper.parsed(B=B, A=V[0], S=S, V=V) if V else wrapper.parsed(B=B, S=S)
+    def wrapper(B, S, N, *V):
+        return wrapper.parsed(B=B, A=V[0], N=N, S=S, V=V) if V else wrapper.parsed(B=B, S=S)
     wrapper.parsed = _BLOCK_OPERATION_PARSER(definition)
     return wrapper
 
@@ -23,10 +23,11 @@ class blockop(ArithmeticUnit):
     """
     This unit allows you to specify a custom Python expression where the following variables are allowed.
 
+    - the variable `A`: same as `V[0]`
     - the variable `B`: current block
+    - the variable `N`: number of bytes in the input
     - the variable `S`: an optional seed value for an internal state
     - the variable `V`: the vector of arguments
-    - the variable `A`: same as `V[0]`
 
     Each block of the input is replaced by the value of this expression. Additionally, it is possible to
     specify prologue and epilogue expressions which are used to update the state variable `S` before and
@@ -41,12 +42,16 @@ class blockop(ArithmeticUnit):
         argp.epilogue = argp.add_mutually_exclusive_group()
         argp.epilogue.add_argument('-e', '--epilogue', type=blockop_expression, metavar='E', default=None, help=(
             'Optional expression with which the state variable S is updated after a block was operated on.'))
-        argp.epilogue.add_argument('--ctr', action='store_const', dest='epilogue', const=blockop_expression('S+1'),
+        argp.epilogue.add_argument('--inc', action='store_const', dest='epilogue', const=blockop_expression('S+1'),
             help='equivalent to --epilogue=S+1')
+        argp.epilogue.add_argument('--dec', action='store_const', dest='epilogue', const=blockop_expression('S-1'),
+            help='equivalent to --epilogue=S-1')
         argp.epilogue.add_argument('--cbc', action='store_const', dest='epilogue', const=blockop_expression('B'),
             help='equivalent to --epilogue=B')
-        argp.add_argument('-s', '--seed', type=number, default=0,
-            help='Optional seed value for the state variable S. The default is zero.')
+        argp.add_argument('-s', '--seed', type=PythonExpression('N'), default=0, help=(
+            'Optional seed value for the state variable S. The default is zero. This can be an expression '
+            'involving the variable N.'
+        ))
         return super().interface(argp)
 
     @property
@@ -59,13 +64,16 @@ class blockop(ArithmeticUnit):
         return super().process_ecb_fast(data)
 
     def process(self, data):
+        self._total = len(data)
         self._state = self.args.seed
+        if callable(self._state):
+            self._state = self._state(N=self._total)
         return super().process(data)
 
     def operate(self, block, *args):
         if self.args.prologue:
-            self._state = self.args.prologue(block, self._state, *args)
-        block = self.args.operation(block, self._state, *args) & self.fmask
+            self._state = self.args.prologue(block, self._state, self._total, *args)
+        block = self.args.operation(block, self._state, self._total, *args) & self.fmask
         if self.args.epilogue:
-            self._state = self.args.epilogue(block, self._state, *args)
+            self._state = self.args.epilogue(block, self._state, self._total, *args)
         return block
