d5496613c45eafd36346487994c24a212cd6550d
jesko <rattle@nullteilerfrei.de>
2020-02-23 20:10:18 +0100

    improves test interface

--- run-tests.py
+++ run-tests.py
@@ -3,14 +3,19 @@
 """
 Runs all tests from the command line.
 """
+import argparse
 import unittest
 import os
 import sys
 
+argp = argparse.ArgumentParser()
+argp.add_argument('pattern', type=str, nargs='?', default='*')
+args = argp.parse_args()
+
 os.chdir('test')
 os.environ['REFINERY_VERBOSITY'] = 'DETACHED'
 
-suite = unittest.TestLoader().discover('test')
+suite = unittest.TestLoader().discover('test', F'test_{args.pattern}')
 tests = unittest.TextTestRunner(verbosity=2)
 result = tests.run(suite)
 sys.exit(0 if result.wasSuccessful() else 1)
