ccc6e46fce8e22dd8d1252a5005d0df660243891
jesko <rattle@nullteilerfrei.de>
2020-03-09 18:26:43 +0100

    adds digital signature parsing to pemeta

--- refinery/units/formats/pe/pemeta.py
+++ refinery/units/formats/pe/pemeta.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 import pefile
 import json
+import struct
+
+from asn1crypto import cms
+from asn1crypto import x509
 
 from ... import Unit
 
 
 class pemeta(Unit):
     """
-    Extract metadata from PE files.
+    Extract metadata from PE files, including code signing information.
     """
 
     def _ensure_string(self, x):
@@ -22,8 +26,103 @@ class pemeta(Unit):
             self._ensure_string(val)
         ) for key, val in bin.items())
 
-    def process(self, data):
+    def _extract_pkcs7(self, pe, data):
+        ENTRY_SECURITY = pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_SECURITY']
 
+        pe.parse_data_directories(directories=[ENTRY_SECURITY])
+        security = pe.OPTIONAL_HEADER.DATA_DIRECTORY[ENTRY_SECURITY]
+        sgnoff = security.VirtualAddress + 8
+        sgnend = sgnoff + security.Size
+
+        length, revision, certtype = struct.unpack('<IHH', data[sgnoff - 8:sgnoff])
+        signature = data[sgnoff:sgnend]
+
+        self.log_debug('revision:', revision)
+        self.log_debug('certtype:', certtype)
+        self.log_debug('length:', length)
+
+        if len(signature) + 8 != length:
+            raise ValueError(F'Found {len(signature) + 8} bytes of signature, but length should be {length}.')
+
+        return bytes(signature)
+
+    def _parse_certs(self, pe, data):
+        cert = self._extract_pkcs7(pe, data)
+        info = {}
+
+        try:
+            signature = cms.ContentInfo.load(cert)
+        except Exception as E:
+            raise ValueError(F'PKCS7 parser failed with error: {E!s}')
+
+        def tscrawl(entry, native, /, *path):
+            try:
+                if entry['type'] == 'signing_time':
+                    return entry['values']
+            except KeyError:
+                pass
+            except TypeError:
+                pass
+
+            try:
+                keys = list(entry)
+                assert all(isinstance(k, str) for k in keys)
+            except Exception:
+                try:
+                    length = len(entry)
+                except TypeError:
+                    return None
+
+                for k in range(length):
+                    try:
+                        item = entry[k]
+                    except IndexError:
+                        continue
+                    results = tscrawl(item, native, *path, F'[{k}]')
+                    if results:
+                        return results
+            else:
+                for key in keys:
+                    value = entry[key]
+                    results = tscrawl(value, native, *path, F'["{key}"]')
+                    if isinstance(results, list) and len(results) == 1:
+                        result = dict(Timestamp=str(results[0]))
+                        try:
+                            acc = entry if native else entry.native
+                            result['Timestamp Issuer'] = acc['sid']['issuer']['common_name']
+                        except Exception:
+                            pass
+                        return result
+                    elif results is not None:
+                        return results
+
+            try:
+                return tscrawl(entry.native, True, *path)
+            except Exception:
+                pass
+
+        timestamps = []
+        for signer in signature['content']['signer_infos']:
+            timestamps.append(tscrawl(signer, False))
+        if len(timestamps) == 1:
+            info.update(timestamps[0])
+
+        for entry in signature['content']['certificates']:
+            cert = x509.Certificate.load(entry.dump())
+            if cert.ca: continue
+            tbs = cert.native['tbs_certificate']
+
+            for extension in tbs['extensions']:
+                if extension['extn_id'] == 'extended_key_usage' and 'code_signing' in extension['extn_value']:
+                    tbs = dict(
+                        Issuer=tbs['issuer']['common_name'],
+                        Subject=tbs['subject']['common_name'],
+                        Serial=F"{tbs['serial_number']:x}",
+                    )
+                    info.update(tbs)
+                    return info
+
+    def process(self, data):
         pe = pefile.PE(data=data, fast_load=True)
         pe.parse_data_directories(
             directories=[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_RESOURCE']])
@@ -31,7 +130,7 @@ class pemeta(Unit):
         try:
             FileInfoList = pe.FileInfo
         except AttributeError:
-            return
+            return B'{}'
 
         for FileInfo in FileInfoList:
             for FileInfoEntry in FileInfo:
@@ -40,6 +139,15 @@ class pemeta(Unit):
                 for StringTableEntry in FileInfoEntry.StringTable:
                     StringTableEntryParsed = self._parse_pedict(StringTableEntry.entries)
                     StringTableEntryParsed['LangID'] = self._ensure_string(StringTableEntry.LangID)
+                    Signature = self._parse_certs(pe, data)
+                    try:
+                        pass
+                    except Exception as E:
+                        self.log_info(F'could not extract certificate: {E!s}')
+                        Signature = None
+                    StringTableEntryParsed['Signature'] = Signature
                     return json.dumps(
                         StringTableEntryParsed, sort_keys=True, indent=4, ensure_ascii=False
                     ).encode(self.codec)
+
+        return B'{}'
