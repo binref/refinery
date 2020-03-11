ddf266e07b1469105a2f3144e9464c7821baef56
jesko <rattle@nullteilerfrei.de>
2020-03-11 23:15:52 +0100

    adds pefile signature extractor

---
+++ refinery/units/formats/pe/pesig.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from struct import unpack
+from pefile import PE, DIRECTORY_ENTRY
+
+from ... import Unit, RefineryPartialResult
+
+
+class pesig(Unit):
+    """
+    Extracts the contents of the IMAGE_DIRECTORY_ENTRY_SECURITY entry of a PE file,
+    i.e. the digital signatures in DER format.
+    """
+
+    _SECDIRID = DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_SECURITY']
+
+    def __init__(self) -> Unit: pass
+
+    def process(self, data: bytearray) -> bytearray:
+        """
+        Extracts the raw digital signature from a PE file.
+        """
+        pe = PE(data=data, fast_load=True)
+        pe.parse_data_directories(directories=[self._SECDIRID])
+        security = pe.OPTIONAL_HEADER.DATA_DIRECTORY[self._SECDIRID]
+
+        sgnoff = security.VirtualAddress + 8
+        sgnend = sgnoff + security.Size
+        length, revision, certtype = unpack('<IHH', data[sgnoff - 8:sgnoff])
+        signature = data[sgnoff:sgnend]
+
+        if len(signature) + 8 != length:
+            raise RefineryPartialResult(
+                F'Found {len(signature) + 8} bytes of signature, but length should be {length}.',
+                partial=signature)
+
+        return signature
