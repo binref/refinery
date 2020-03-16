acbf730933c85af5c9399fc541066847c80a76e4
jesko <rattle@nullteilerfrei.de>
2020-03-16 18:33:06 +0100

    improves trim performance

--- refinery/units/strings/trim.py
+++ refinery/units/strings/trim.py
@@ -21,24 +21,51 @@ class trim(Unit):
 
         super().__init__(junk=junk, left=left, right=right)
 
-    def process(self, data):
+    def process(self, data: bytearray):
         keep_running = True
+        unview = False
+        triml, trimr = self.args.left, self.args.right
         mv = memoryview(data)
 
-        if self.args.left:
-            jpl = tuple(re.compile(B'^(?:%s)+' % re.escape(j)) for j in self.args.junk)
-        if self.args.right:
-            jpr = tuple(re.compile(B'(?:%s)+$' % re.escape(j)) for j in self.args.junk)
+        strip = bytes(j[0] for j in self.args.junk if len(j) == 1)
+
+        junkbig = [j for j in self.args.junk if len(j) >= 2]
+        escaped = [re.escape(j) for j in junkbig]
+
+        if triml:
+            jpl = tuple(re.compile(B'^(?:%s)+' % j) for j in escaped)
+        if trimr:
+            jpr = tuple(re.compile(B'(?:%s)+$' % j) for j in escaped)
 
         while keep_running:
             keep_running = False
-            for k, junk in enumerate(self.args.junk):
-                jl = len(junk)
-                if self.args.left and mv[:jl] == junk:
-                    mv = mv[jpl[k].search(mv).end():]
+
+            if strip:
+                if triml and trimr and (mv[0] in strip or mv[-1] in strip):
                     keep_running = True
-                if self.args.right and mv[-jl:] == junk:
+                    data = data.strip(strip)
+                elif triml and mv[0] in strip:
+                    keep_running = True
+                    data = data.lstrip(strip)
+                elif trimr and mv[-1] in strip:
+                    keep_running = True
+                    data = data.rstrip(strip)
+                if keep_running:
+                    mv = memoryview(data)
+                    unview = False
+
+            for k, junk in enumerate(junkbig):
+                jl = len(junk)
+                if triml and mv[:jl] == junk:
+                    mv = mv[jpl[k].search(mv).end():]
+                    unview = True
+                    keep_running = True
+                if trimr and mv[-jl:] == junk:
                     mv = mv[:jpr[k].search(mv).start()]
+                    unview = True
                     keep_running = True
 
-        return bytearray(mv)
+        if unview:
+            return bytearray(mv)
+
+        return data
