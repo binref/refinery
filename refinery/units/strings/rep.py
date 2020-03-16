84c7f0fba6dd3677f787e5d6eb8801876b7b8c94
jesko <rattle@nullteilerfrei.de>
2020-03-16 02:09:05 +0100

    retrofit

--- refinery/units/strings/rep.py
+++ refinery/units/strings/rep.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from .. import Unit
-from ...lib.argformats import number
+from .. import arg, Unit
 
 
 class rep(Unit):
@@ -9,11 +8,12 @@ class rep(Unit):
     Duplicates the given input a given number of times.
     """
 
-    @classmethod
-    def interface(cls, argp):
-        argp.add_argument('count', type=number[1:], nargs='?', default=2,
-            help='Defines the number of outputs for each input. The default is 2.')
-        return super().interface(argp)
+    def __init__(self, count: arg.help(
+        'Defines the number of outputs for each input. The default is the minimum of 2.') = 2
+    ):
+        if count < 2:
+            raise ValueError('The count must be at least two.')
+        super().__init__(count=count)
 
     def process(self, data: bytes):
         from itertools import repeat
