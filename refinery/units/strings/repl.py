b8a2f2f8fe5743a950353f6abe7f8b7433905a82
jesko <rattle@nullteilerfrei.de>
2020-03-16 01:57:32 +0100

    retrofit

--- refinery/units/strings/repl.py
+++ refinery/units/strings/repl.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from .. import Unit
-from ...lib.argformats import multibin, number
+from .. import arg, Unit
 
 
 class repl(Unit):
@@ -9,15 +8,13 @@ class repl(Unit):
     Performs a simple binary string replacement on the input data.
     """
 
-    @classmethod
-    def interface(cls, argp):
-        argp.add_argument('-n', '--count', type=number, default=-1,
-            help='Only replace the given number of occurrences')
-        argp.add_argument('search', type=multibin,
-            help='This is the search term.')
-        argp.add_argument('replace', type=multibin, nargs='?', default=B'',
-            help='The substitution string. Leave this empty to remove all occurrences of the search term.')
-        return super().interface(argp)
+    def __init__(
+        self,
+        search : arg.help('This is the search term.'),
+        replace: arg.help('The substitution string. Leave this empty to remove all occurrences of the search term.') = B'',
+        count  : arg.number('-n', help='Only replace the given number of occurrences') = -1
+    ):
+        super().__init__(search=search, replace=replace, count=count)
 
     def process(self, data: bytes):
         return data.replace(
