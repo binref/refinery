6c4c6d893a8c812f694316554fa2806b5e8a495f
jesko <rattle@nullteilerfrei.de>
2020-03-19 23:42:26 +0100

    fixes tests

--- test/units/meta/test_scope.py
+++ test/units/meta/test_scope.py
@@ -7,13 +7,13 @@ from refinery import rep, scope, cca, ccp, chop
 class TestScope(TestMetaBase):
 
     def test_only_local_scope(self):
-        pipeline = rep [ scope(1) | rep [ scope(0) | cca('.') ]] # noqa
+        pipeline = rep [ scope(1) | rep [ scope(0) | cca(B'.') ]] # noqa
         self.assertEqual(pipeline(B'FOO'), B'FOOFOO.FOO')
 
     def test_layer1_rescope(self):
-        pipeline = rep [ scope(0) | cca(',') | scope(1) | cca('.') ] # noqa
+        pipeline = rep [ scope(0) | cca(B',') | scope(1) | cca(B'.') ] # noqa
         self.assertEqual(pipeline(B'FOO'), B'FOO,FOO.')
 
     def test_layer2_rescope(self):
-        pipeline = rep(6) [ scope('4:') | chop(1) [ scope('1:') | cca('A') | scope(0) | ccp('-') ]] # noqa
+        pipeline = rep(6) [ scope('4:') | chop(1) [ scope('1:') | cca(B'A') | scope(0) | ccp(B'-') ]] # noqa
         self.assertEqual(pipeline(B'NA'), B'NANANANA-NAA-NAA')
