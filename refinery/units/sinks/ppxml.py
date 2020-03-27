1d83389985763cd64e11cc4ffad796bf0d974b85
jesko <rattle@nullteilerfrei.de>
2020-03-27 21:02:05 +0100

    why is XML so hard

--- refinery/units/sinks/ppxml.py
+++ refinery/units/sinks/ppxml.py
@@ -1,9 +1,47 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import defusedxml.minidom
+import io
+import uuid
+
+from xml.parsers import expat
+from defusedxml.ElementTree import parse as ParseXML, XMLParser
 
 from .. import arg, Unit
-from ...lib.decorators import unicoded
+
+
+class ForgivingXMLParser(XMLParser):
+
+    def __init__(self, emap):
+        class ForgivingEntityResolver(dict):
+            def __getitem__(self, key):
+                if key in self:
+                    return dict.__getitem__(self, key)
+                uid = str(uuid.uuid4())
+                self[key] = uid
+                emap[uid] = key
+                return uid
+
+        self.__entity = ForgivingEntityResolver()
+        _ParserCreate = expat.ParserCreate
+
+        try:
+            def PC(encoding, _):
+                parser = _ParserCreate(
+                    encoding, namespace_separator=None)
+                parser.UseForeignDTD(True)
+                return parser
+            expat.ParserCreate = PC
+            super().__init__()
+        finally:
+            expat.ParserCreate = _ParserCreate
+
+    @property
+    def entity(self):
+        return self.__entity
+
+    @entity.setter
+    def entity(self, value):
+        self.__entity.update(value)
 
 
 class ppxml(Unit):
@@ -11,15 +49,52 @@ class ppxml(Unit):
     Expects XML input data and outputs it in a neatly formatted manner.
     """
 
-    def __init__(self, indent: arg.number('-i', help=(
-        'Controls the amount of space characters used for indentation in the output. Default is 4.')) = 4
+    def __init__(self,
+        indent: arg.number('-i', help=(
+            'Controls the amount of space characters used for indentation in the output. Default is 4.')) = 4,
+        header: arg.switch('-x', help='Add an XML header to the formatted output.') = False
     ):
-        super().__init__(indent=indent)
+        super().__init__(indent=indent, header=header)
 
-    @unicoded
     def process(self, data: str) -> str:
-        node = defusedxml.minidom.parseString(data)
-        if '<?xml' not in data:
-            node = node.childNodes[0]
-        return '\n'.join(s for s in node.toprettyxml(
-            self.args.indent * ' ').splitlines() if s and not s.isspace())
+
+        pad = self.args.indent * ' '
+        etm = {}
+        dom = ParseXML(io.BytesIO(data), parser=ForgivingXMLParser(etm))
+
+        def indent(element, level=0, more_sibs=False):
+            """
+            The credit for this one goes to:
+            https://stackoverflow.com/a/12940014
+            """
+            indentation = '\n'
+            if level:
+                indentation += (level - 1) * pad
+            childcount = len(element)
+            if childcount:
+                if not element.text or not element.text.strip():
+                    element.text = indentation + pad
+                    if level:
+                        element.text += pad
+                for count, child in enumerate(element):
+                    indent(child, level + 1, count < childcount - 1)
+                if level and (not element.tail or element.tail.isspace()):
+                    element.tail = indentation
+                    if more_sibs:
+                        element.tail += pad
+            elif level and (not element.tail or element.tail.isspace()):
+                element.tail = indentation
+                if more_sibs: element.tail += pad
+
+        indent(dom.getroot())
+
+        with io.BytesIO() as output:
+            dom.write(output, encoding=self.codec, xml_declaration=self.args.header)
+            result = output.getvalue()
+
+        for uid, key in etm.items():
+            entity = F'&{key};'.encode(self.codec)
+            needle = uid.encode(self.codec)
+            result = result.replace(needle, entity)
+
+        return result
--- test/units/sinks/test_ppxml.py
+++ test/units/sinks/test_ppxml.py
@@ -8,15 +8,36 @@ class TestXML(TestUnitBase):
     def test_simple(self):
         for k in range(5):
             unit = self.load(indent=k)
-            self.assertTrue(
-                unit(b'<foo><bar>baz</bar><bar>bat</bar></foo>').endswith(
-                    B'<foo>\n%s<bar>baz</bar>\n%s<bar>bat</bar>\n</foo>' % (2 * (k * B' ',))
-                )
-            )
+            result = unit(b'<foo><bar>baz</bar><bar>bat</bar></foo>')
+            self.assertTrue(result.endswith(
+                B'<foo>\n%s<bar>baz</bar>\n%s<bar>bat</bar>\n</foo>' % (2 * (k * B' ',))))
 
     def test_xml_header(self):
-        unit = self.load(indent=1)
-        self.assertEqual(
-            B'<?xml version="1.0" ?>\n<foo>\n <bar>baz</bar>\n <bar>bamf</bar>\n</foo>',
-            unit(B'<?xml version="1.0" encoding="UTF-8"?>\n\n<foo>\n\n\n<bar>baz</bar>\n<bar>bamf</bar></foo>')
+        unit = self.load(indent=1, header=True)
+        result = unit(B'<?xml version="1.0" encoding="UTF-8"?>\n\n<foo>\n\n\n<bar>baz</bar>\n<bar>bamf</bar></foo>')
+        self.assertTrue(result.startswith(B'<?xml'))
+        self.assertTrue(result.endswith(B'?>\n<foo>\n <bar>baz</bar>\n <bar>bamf</bar>\n</foo>'))
+
+    def test_unknwon_namespaces_and_entities(self):
+        data = (
+            B'<ac:structured-macro ac:name="multiexcerpt" ac:schema-version="1">'
+            B'<ac:parameter ac:name="MultiExcerptName">Foo</ac:parameter>'
+            B'<ac:parameter ac:name="atlassian-macro-output-type">INLINE</ac:parameter>'
+            B'<ac:rich-text-body><p><span>Some Atlassian text &ndash; and &ndash; weird entities.</span></p></ac:rich-text-body>'
+            B'</ac:structured-macro>'
         )
+        pretty = (
+            B'<ac:structured-macro ac:name="multiexcerpt" ac:schema-version="1">\n'
+            B'    <ac:parameter ac:name="MultiExcerptName">Foo</ac:parameter>\n'
+            B'    <ac:parameter ac:name="atlassian-macro-output-type">INLINE</ac:parameter>\n'
+            B'    <ac:rich-text-body>\n'
+            B'        <p>\n'
+            B'            <span>Some Atlassian text &ndash; and &ndash; weird entities.</span>\n'
+            B'        </p>\n'
+            B'    </ac:rich-text-body>\n'
+            B'</ac:structured-macro>'
+        )
+
+        unit = self.load()
+        result = unit(data)
+        self.assertEqual(pretty, result)
