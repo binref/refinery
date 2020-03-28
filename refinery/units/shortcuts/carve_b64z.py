aa4d980d156e7846b455b5ababe9484fd91ba674
jesko <rattle@nullteilerfrei.de>
2020-03-28 16:18:37 +0100

    improved shortcut

--- refinery/units/shortcuts/carve_b64z.py
+++ refinery/units/shortcuts/carve_b64z.py
@@ -1,13 +1,22 @@
-from .. import Unit
-from ..pattern.carve import carve
-from ..encoding.b64 import b64
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from ..pattern.carve import arg, carve
 from ..compression.decompress import decompress
 
 
-class carve_b64z(Unit):
+class carveb64z(carve):
     """
-    Carves the longest base64 encoded expression and decodes it, then applies
-    the `refinery.decompress` unit to the result.
+    Carves base64 encoded expressions, decodes them, and then applies the `refinery.decompress`
+    unit to the result. By default, only the longest base64 string is processed.
     """
+    def __init__(
+        self, single: arg.switch('-m', '--multi', help='Process all base64 strings instead of just the longest.') = True,
+        min=1, max=None, stripspace=False, unique=False, longest=False, take=None, utf16=True, ascii=True
+    ):
+        super().__init__('b64', decode=True,
+            single=single, min=min, max=max, stripspace=stripspace, unique=unique, longest=longest, take=take, utf16=utf16, ascii=ascii)
+        self.decompressor = decompress()
+
     def process(self, data):
-        return (carve('b64', longest=True, take=1) | b64 | decompress)(data)
+        for chunk in super().process(data):
+            yield self.decompressor(chunk)
