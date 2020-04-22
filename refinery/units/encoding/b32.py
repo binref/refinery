3446551e95e408a7a2884bac2c149097d661fce7
jesko <rattle@nullteilerfrei.de>
2020-04-22 19:42:12 +0200

    adds base32 unit

---
+++ refinery/units/encoding/b32.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import base64
+
+from .. import Unit
+
+
+class b32(Unit):
+    """
+    Base32 encoding and decoding.
+    """
+    def __init__(self): super().__init__()
+
+    def reverse(self, data):
+        return base64.b32encode(data)
+
+    def process(self, data):
+        return base64.b32decode(data)
