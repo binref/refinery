50b40a1cfc172291d6e9d8d03c4161ac26ea09c7
jesko <rattle@nullteilerfrei.de>
2020-04-24 15:57:23 +0200

    adds typecasting test

---
+++ test/units/obfuscation/ps1/test_typecast.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from ... import TestUnitBase
+
+
+class TestPs1Typecast(TestUnitBase):
+
+    def setUp(self):
+        super().setUp()
+        self.unit = self.load()
+
+    def test_useless_string_cast(self):
+        self.assertEqual(
+            self.unit.deobfuscate('''.replAce(("M0I"),[strIng]"'")'''),
+            '''.replAce(("M0I"),"'")'''
+        )
