725e69f447aa99eca12fa429e657d9602ab0b168
jesko <rattle@nullteilerfrei.de>
2020-05-06 13:04:09 +0200

    adds vba constants deobfuscator

---
+++ refinery/units/obfuscation/vba/constants.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import re
+
+from .. import Deobfuscator
+
+
+class deob_vba_constants(Deobfuscator):
+    def deobfuscate(self, data):
+        def extract_constants(lines):
+            for line in lines:
+                match = re.match(R'(?i)^\s*const\s*(\w+)\s*=\s*(.*?)(?:\'|rem|$)', line)
+                if match is None:
+                    yield line
+                    continue
+                constants[match[1]] = match[2]
+                self.log_debug(F'const {match[1]} = {match[2]}')
+
+        constants = {}
+        data = '\n'.join(extract_constants(data.splitlines(keepends=False)))
+        for name, value in constants.items():
+            data = data.replace(name, value)
+
+        return data
