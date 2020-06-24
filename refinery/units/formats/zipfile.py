754a79898ea552ede401d1c7cbb2bc8dd2c8b96b
jesko <rattle@nullteilerfrei.de>
2020-06-24 16:42:50 +0200

    adds password option

--- refinery/units/formats/zipfile.py
+++ refinery/units/formats/zipfile.py
@@ -3,17 +3,24 @@
 import io
 import zipfile
 
-from . import PathExtractorUnit, UnpackResult
+from . import arg, PathExtractorUnit, UnpackResult
 
 
 class xtzip(PathExtractorUnit):
     """
     Extract files from a Zip archive.
     """
+    def __init__(
+        self, *paths, list=False, join=False,
+        pwd: arg('-p', help='Optionally specify an extraction password.') = B''
+    ):
+        super().__init__(*paths, list=list, join=join, pwd=pwd)
+
     def unpack(self, data):
         with io.BytesIO(data) as stream:
             with zipfile.ZipFile(stream) as archive:
                 for info in archive.infolist():
                     if info.is_dir():
                         continue
-                    yield UnpackResult(info.filename, lambda info=info: archive.read(info.filename))
+                    yield UnpackResult(info.filename,
+                        lambda info=info, pwd=self.args.pwd: archive.read(info.filename, pwd=pwd))
