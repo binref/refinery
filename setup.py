1e5eaa07b2e0ac33c2d02b1f637fb825c4698497
jesko <rattle@nullteilerfrei.de>
2020-06-25 01:40:25 +0200

    bugfix release

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def main():
 
     setuptools.setup(
         name='binary-refinery',
-        version='0.2.2',
+        version='0.2.3',
         author='Jesko Hüttenhain',
         description='A toolkit to transform and refine (mostly) binary data.',
         long_description=readme,
