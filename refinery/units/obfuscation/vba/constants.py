b7647c6bee75a4c5deafa3133c1a027b82920140
jesko <rattle@nullteilerfrei.de>
2020-08-16 11:29:12 +0200

    improves constant folder

--- refinery/units/obfuscation/vba/constants.py
+++ refinery/units/obfuscation/vba/constants.py
@@ -3,21 +3,38 @@
 import re
 
 from .. import Deobfuscator
+from ....lib.patterns import formats
 
 
 class deob_vba_constants(Deobfuscator):
     def deobfuscate(self, data):
-        def extract_constants(lines):
-            for line in lines:
-                match = re.match(R'(?i)^\s*const\s*(\w+)\s*=\s*(.*?)(?:\'|rem|$)', line)
-                if match is None:
-                    yield line
-                    continue
-                constants[match[1]] = match[2]
-                self.log_debug(F'const {match[1]} = {match[2]}')
-
+        codelines = data.splitlines(keepends=True)
         constants = {}
-        data = '\n'.join(extract_constants(data.splitlines(keepends=False)))
+        constline = {}
+        variables = set()
+        for k, line in enumerate(codelines):
+            match = re.match(R'(?im)^\s*(?:sub|function)\s*(\w+)', line)
+            if match:
+                variables.add(match[1])
+                continue
+            match = re.match(
+                R'(?im)^(?:\s*const)?\s*(\w+)\s*=\s*({i}|{s})\s*(?:\'|rem|$)'.format(
+                    s=formats.ps1str,
+                    i=formats.integer
+                ), line)
+            if match is None or match[1] in variables:
+                pass
+            elif match[2] != constants.get(match[1], match[2]):
+                self.log_debug(F'del {match[1]}')
+                del constants[match[1]]
+                del constline[match[1]]
+                variables.add(match[1])
+            else:
+                self.log_debug(F'add {match[1]} = {match[2]}')
+                constants[match[1]] = match[2]
+                constline[match[1]] = k
+        codelines = [line for k, line in enumerate(codelines) if k not in constline.values()]
+        data = ''.join(codelines)
         for name, value in constants.items():
             data = re.sub(RF'\b{re.escape(name)!s}\b', value, data)
 
