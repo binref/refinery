4762709cf26130ee108a2923801c82a3eb792127
jesko <rattle@nullteilerfrei.de>
2020-10-03 01:36:18 +0200

    implements the RABBIT cipher

---
+++ refinery/units/crypto/cipher/rabbit.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Pure Python implementation of the RABBIT stream cipher.
+"""
+import struct
+from typing import Iterable, Optional, ByteString, List
+
+from . import arg, StreamCipherUnit
+
+
+class RabbitCipher:
+
+    _COUNTER = 0x4D34D34D, 0xD34D34D3, 0x34D34D34
+
+    def __init__(self, key: ByteString, iv: Optional[ByteString] = B''):
+        key = struct.unpack('<8H', key)
+        self.c: List[int] = []
+        self.x: List[int] = []
+        for j in range(8):
+            v1, v2, w1, w2 = (
+                key[j + 0 & 7],
+                key[j + 1 & 7],
+                key[j + 4 & 7],
+                key[j + 5 & 7]
+            )
+            if j % 2 == 0:
+                self.c.append((w1 << 16) | w2)
+                self.x.append((v2 << 16) | v1)
+            else:
+                self.c.append((v1 << 16) | v2)
+                self.x.append((w2 << 16) | w1)
+        self.b = 0
+        self.hop4()
+        self.c = [c ^ v for c, v in zip(self.c, self.x[4:] + self.x[:4])]
+        if not iv:
+            return
+        i0, i2 = struct.unpack('<LL', iv)
+        i1 = ((i0 >> 16) | (i2 & 0xFFFF0000))
+        i3 = ((i2 << 16) | (i0 & 0x0000FFFF)) & 0xFFFFFFFF
+        self.c = [c ^ v for c, v in zip(self.c, 2 * (i0, i1, i2, i3))]
+        self.hop4()
+
+    def hop4(self):
+        self.hop(False)
+        self.hop(False)
+        self.hop(False)
+        self.hop(False)
+
+    def hop(self, derive=True):
+        for k in range(8):
+            self.b += self.c[k] + self._COUNTER[k % 3]
+            self.b, self.c[k] = divmod(self.b, 0x100000000)
+        g = [self._compute_g(*t) for t in zip(self.x, self.c)]
+        self.x = g[:]
+        for j in range(8):
+            if j % 2:
+                self.x[j + 1 & 7] += self.r16(g[j])
+                self.x[j + 2 & 7] += g[j]
+            else:
+                self.x[j + 1 & 7] += self.r08(g[j])
+                self.x[j + 2 & 7] += self.r16(g[j])
+        self.x = [t & 0xFFFFFFFF for t in self.x]
+        if derive:
+            return self.key
+
+    @staticmethod
+    def _compute_g(a, b):
+        t = (a + b & 0xFFFFFFFF) ** 2
+        return (t ^ (t >> 32)) & 0xFFFFFFFF
+
+    @staticmethod
+    def r08(y): return ((y & 0x00FFFFFF) << 0x08) | (y >> 0x18)
+
+    @staticmethod
+    def r16(y): return ((y & 0x0000FFFF) << 0x10) | (y >> 0x10)
+
+    @property
+    def key(self) -> bytes:
+        s = ((self.x[a] & 0xFFFF) ^ (self.x[b] >> 0x10)
+            for a, b in ((0, 5), (3, 0), (2, 7), (5, 2), (4, 1), (7, 4), (6, 3), (1, 6)))
+        return struct.pack('<8H', *s)
+
+    def __iter__(self):
+        while True: yield from self.hop()
+
+
+class rabbit(StreamCipherUnit):
+    """
+    RABBIT encryption and decryption.
+    """
+    key_sizes = 16
+
+    def __init__(self, key, iv: arg('-I', '--iv', help='Optional initialization vector.') = B''):
+        super().__init__(key=key, iv=iv)
+
+    def keystream(self) -> Iterable[int]:
+        if len(self.args.iv) not in (0, 8):
+            raise ValueError('The IV length must be exactly 8 bytes.')
+        return RabbitCipher(self.args.key, self.args.iv)
---
+++ test/units/crypto/cipher/test_rabbit.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from ... import TestUnitBase
+
+
+class TestRabbit(TestUnitBase):
+
+    def test_rabbit_01(self):
+        unit = self.load(bytes.fromhex(
+            '00000000000000000000000000000000'
+        ))
+        data = bytearray(48)
+        self.assertEqual(unit(data), bytes.fromhex(
+            '02F74A1C26456BF5ECD6A536F05457B1'
+            'A78AC689476C697B390C9CC515D8E888'
+            '96D6731688D168DA51D40C70C3A116F4'
+        ))
+
+    def test_rabbit_02(self):
+        unit = self.load(0x_9128_1329_2E3D_36FE_3BFC_62F1_DC51_C3AC.to_bytes(16, 'little'))
+        data = bytearray(48)
+        self.assertEqual(unit(data), bytes.fromhex(
+            '9C51E28784C37FE9A127F63EC8F32D3D'
+            '19FC5485AA53BF96885B40F461CD76F5'
+            '5E4C4D20203BE58A5043DBFB737454E5'
+        ))
+
+    def test_rabbit_03(self):
+        unit = self.load(0x_8395_7415_87E0_C733_E9E9_AB01_C09B_0043.to_bytes(16, 'little'))
+        data = bytearray(48)
+        self.assertEqual(unit(data), bytes.fromhex(
+            '9B60D002FD5CEB32ACCD41A0CD0DB10C'
+            'AD3EFF4C1192707B5A01170FCA9FFC95'
+            '2874943AAD4741923F7FFC8BDEE54996'
+        ))
+
+    def test_rabbit_iv_01(self):
+        unit = self.load(0x00.to_bytes(16, 'big'), iv=0x0000000000000000.to_bytes(8, 'little'))
+        data = bytearray(48)
+        self.assertEqual(unit(data), bytes.fromhex(
+            'EDB70567375DCD7CD89554F85E27A7C6'
+            '8D4ADC7032298F7BD4EFF504ACA6295F'
+            '668FBF478ADB2BE51E6CDE292B82DE2A'
+        ))
+
+    def test_rabbit_iv_02(self):
+        unit = self.load(0x00.to_bytes(16, 'big'), iv=0xC373F575C1267E59.to_bytes(8, 'little'))
+        data = bytearray(48)
+        self.assertEqual(unit(data), bytes.fromhex(
+            '6D7D012292CCDCE0E2120058B94ECD1F'
+            '2E6F93EDFF99247B012521D1104E5FA7'
+            'A79B0212D0BD56233938E793C312C1EB'
+        ))
+
+    def test_rabbit_iv_03(self):
+        unit = self.load(0x00.to_bytes(16, 'big'), iv=0xA6EB561AD2F41727.to_bytes(8, 'little'))
+        data = bytearray(48)
+        self.assertEqual(unit(data), bytes.fromhex(
+            '4D1051A123AFB670BF8D8505C8D85A44'
+            '035BC3ACC667AEAE5B2CF44779F2C896'
+            'CB5115F034F03D31171CA75F89FCCB9F'
+        ))
