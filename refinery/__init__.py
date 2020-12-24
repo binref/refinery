78ec9bfd6706ecc9ca163c8963827a6bc4f06578
jesko <rattle@nullteilerfrei.de>
2020-12-24 15:22:03 +0100

    move exports back into __all__ for auto-docs

--- refinery/__init__.py
+++ refinery/__init__.py
@@ -154,7 +154,7 @@ class __pdoc__(dict):
         return super().items()
 
 
-__all__ = ['Unit', 'arg', '__pdoc__']
+__all__ = ['Unit', 'arg', '__pdoc__'] + [x for x, _ in sorted(_cache.units.items(), key=lambda x: x[1])]
 
 
 def __getattr__(name):
@@ -162,6 +162,4 @@ def __getattr__(name):
 
 
 def __dir__():
-    if len(__all__) < 5:
-        __all__.extend(x for x, _ in sorted(_cache.units.items(), key=lambda x: x[1]))
     return __all__
