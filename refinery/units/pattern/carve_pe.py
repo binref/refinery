9ca17f77c717c435cc29f2936f7046cf457272d7
jesko <rattle@nullteilerfrei.de>
2021-01-01 23:29:00 +0100

    turns pe carver into path extractor

--- refinery/units/pattern/carve_pe.py
+++ refinery/units/pattern/carve_pe.py
@@ -2,27 +2,35 @@
 # -*- coding: utf-8 -*-
 from pefile import PE, PEFormatError
 from struct import unpack
-from hashlib import sha256
 
-from ... import arg, Unit
+from ..formats import arg, PathExtractorUnit, UnpackResult
 from ...units.formats.pe.pemeta import pemeta
 from ..formats.pe import get_pe_size
 
 
-class carve_pe(Unit):
+class carve_pe(PathExtractorUnit):
     """
     Extracts anything from the input data that looks like a Portable
     Executable (PE) file.
     """
     def __init__(
-        self,
-        recursive: arg.switch('-r', help='Also extract PE files that are contained in already extracted PEs.') = False,
+        self, *paths, list=False, join=False,
+        recursive: arg.switch('-r', help='Extract PE files that are contained in already extracted PEs.') = False,
         keep_root: arg.switch('-R', help='If the input chunk is itself a PE, include it as an output chunk.') = False,
-        memdump  : arg.switch('-m', help='Use the virtual memory layout of a PE file to calculate its size.') = False
+        memdump  : arg.switch('-m', help='Use the virtual memory layout of a PE file to calculate its size.') = False,
+        fileinfo : arg.switch('-f', help='Use the PE meta information to deduce a file name meta variable.') = False
     ):
-        super().__init__(recursive=recursive, keep_root=keep_root, memdump=memdump)
+        super().__init__(
+            *paths,
+            list=list,
+            join=join,
+            recursive=recursive,
+            keep_root=keep_root,
+            memdump=memdump,
+            fileinfo=fileinfo,
+        )
 
-    def process(self, data):
+    def unpack(self, data):
         cursor = 0
         mv = memoryview(data)
 
@@ -41,19 +49,18 @@ class carve_pe(Unit):
 
             pesize = get_pe_size(pe, memdump=self.args.memdump)
             pedata = mv[p:p + pesize]
-
-            try:
-                info = pemeta.parse_file_info(pe) or {}
-            except Exception:
-                info = {}
+            info = {}
+            if self.args.fileinfo:
+                try: info = pemeta.parse_file_info(pe) or {}
+                except Exception: pass
             try:
                 path = info['OriginalFilename']
             except KeyError:
                 extension = 'exe' if pe.is_exe() else 'dll' if pe.is_dll() else 'sys'
-                path = F'{sha256(pedata).hexdigest()}.{extension}'
+                path = F'carve-0x{p:08X}.{extension}'
 
             if p > 0 or self.args.keep_root:
-                yield self.labelled(pedata, path=path, offset=p)
+                yield UnpackResult(path, pedata, offset=p)
                 self.log_info(F'extracted PE file of size 0x{pesize:08X} from 0x{p:08X}')
             else:
                 self.log_info(F'ignored root file of size 0x{pesize:08X} from 0x{p:08X}')
