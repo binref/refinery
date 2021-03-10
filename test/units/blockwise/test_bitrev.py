66e6a0f07ae442cc73b1cd0bc0dea472e963705a
jesko <rattle@nullteilerfrei.de>
2021-03-10 17:55:02 +0100

    improve test message

--- test/units/blockwise/test_bitrev.py
+++ test/units/blockwise/test_bitrev.py
@@ -9,4 +9,4 @@ class TestBitReversal(TestUnitBase):
         for b in (1, 2, 3, 4, 5, 7, 8, 12, 17):
             unit = self.load(blocksize=b)
             data = self.generate_random_buffer(841)
-            self.assertEqual(data, unit(unit(data)))
+            self.assertEqual(data, unit(unit(data)), F'failed idempotence test for block size {b}')
