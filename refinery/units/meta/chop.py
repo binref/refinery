469ba7dd3b172d70bfda59d08ce74d1d917489a0
jesko <rattle@nullteilerfrei.de>
2021-03-22 15:49:12 +0100

    allow size to be lazy

--- refinery/units/meta/chop.py
+++ refinery/units/meta/chop.py
@@ -17,14 +17,12 @@ class chop(Unit):
             'If this flag is specified, the size parameter determines the number of blocks to be produced rather than the size '
             'of each block. In this case, truncation is performed before the data is split.')) = False
     ):
-        if size < 1:
-            raise ValueError('The chunk size has to be a positive integer value.')
         return super().__init__(size=size, into=into, truncate=truncate)
 
     def process(self, data):
-
         size = self.args.size
-
+        if size < 1:
+            raise ValueError('The chunk size has to be a positive integer value.')
         if self.args.into:
             size, remainder = divmod(len(data), size)
             if remainder and not self.args.truncate:
