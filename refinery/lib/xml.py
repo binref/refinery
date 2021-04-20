91c76df179810e3f7335dd3b7398c9668b2c88a1
jesko <rattle@nullteilerfrei.de>
2021-04-20 19:00:00 +0200

    adds simple xml parser

--- refinery/lib/xml.py
+++ refinery/lib/xml.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 import uuid
 
-from .structures import MemoryFile
 from xml.parsers import expat
 from defusedxml.ElementTree import parse as ParseXML, XMLParser, ParseError
-from xml.etree.ElementTree import ElementTree
+from xml.etree.ElementTree import Element, ElementTree
+
+from .structures import MemoryFile
 
 
 def ForgivingParse(data, entities=None) -> ElementTree:
-    if entities is None:
-        entities = {}
     try:
         return ParseXML(MemoryFile(data), parser=ForgivingXMLParser(entities))
     except ParseError as PE:
@@ -19,14 +18,15 @@ def ForgivingParse(data, entities=None) -> ElementTree:
 
 class ForgivingXMLParser(XMLParser):
 
-    def __init__(self, emap):
+    def __init__(self, emap=None):
         class ForgivingEntityResolver(dict):
             def __getitem__(self, key):
                 if key in self:
                     return dict.__getitem__(self, key)
                 uid = str(uuid.uuid4())
                 self[key] = uid
-                emap[uid] = key
+                if emap is not None:
+                    emap[uid] = key
                 return uid
 
         self.__entity = ForgivingEntityResolver()
@@ -50,3 +50,58 @@ class ForgivingXMLParser(XMLParser):
     @entity.setter
     def entity(self, value):
         self.__entity.update(value)
+
+
+class XMLNodeCollection(list):
+    def __getattr__(self, tag):
+        result = self.__class__()
+        for node in self:
+            for child in node.children:
+                if child.tag != tag:
+                    continue
+                result.append(child)
+        return result
+
+
+class XMLNode:
+    __slots__ = 'tag', 'children', 'attributes', 'content', 'parent'
+
+    def __init__(self, tag: str):
+        self.tag: str = tag
+        self.children = []
+        self.attributes = {}
+        self.content = None
+        self.parent = None
+
+    def __iter__(self):
+        return iter(self.children)
+
+    def __getitem__(self, key):
+        return self.attributes[key]
+
+    def get_attribute(self, key, default=None):
+        return self.attributes.get(key, default)
+
+    @property
+    def subtree(self):
+        return XMLNodeCollection((self,))
+
+    def __enter__(self):
+        return self.subtree
+
+    def __exit__(self, *a):
+        return False
+
+
+def parse(data) -> XMLNode:
+    def translate(element: Element, cursor: XMLNode, level: int = 0):
+        for child in element:
+            node = XMLNode(child.tag)
+            translate(child, node, level + 1)
+            node.parent = cursor
+            cursor.children.append(node)
+        cursor.attributes = element.attrib
+        cursor.content = element.text or element.tail or ''
+        return cursor
+    root = ForgivingParse(data).getroot()
+    return translate(root, XMLNode(root.tag))
