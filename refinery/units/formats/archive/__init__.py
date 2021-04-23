f525446b5749ebb2ee15047e81d55718ac101156
jesko <rattle@nullteilerfrei.de>
2021-04-23 16:16:15 +0200

    relax date parsing

--- refinery/units/formats/archive/__init__.py
+++ refinery/units/formats/archive/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from typing import ByteString, Callable, Union
+from typing import ByteString, Callable, Optional, Union
 from datetime import datetime
 
 from .. import arg, PathExtractorUnit, UnpackResult
@@ -11,10 +11,20 @@ class ArchiveUnit(PathExtractorUnit, abstract=True):
         self, *paths, list=False, join=False, path=b'path',
         date: arg('-D', metavar='NAME',
             help='Name of the meta variable to receive the extracted file date. The default value is "{default}".') = b'date',
-        pwd: arg('-p', help='Optionally specify an extraction password.') = B''
+        pwd: arg('-p', help='Optionally specify an extraction password.') = B'',
+        **kwargs
     ):
-        super().__init__(*paths, list=list, join=join, path=path, pwd=pwd, date=date)
+        super().__init__(*paths, list=list, join=join, path=path, pwd=pwd, date=date, **kwargs)
 
-    def _pack(self, path: str, date: datetime, data: Union[ByteString, Callable[[], ByteString]], **meta) -> UnpackResult:
-        meta[self.args.date.decode(self.codec)] = date.isoformat(' ', 'seconds')
+    def _pack(
+        self,
+        path: str,
+        date: Optional[Union[datetime, str]],
+        data: Union[ByteString, Callable[[], ByteString]],
+        **meta
+    ) -> UnpackResult:
+        if isinstance(date, datetime):
+            date = date.isoformat(' ', 'seconds')
+        if isinstance(date, str):
+            meta[self.args.date.decode(self.codec)] = date
         return UnpackResult(path, data, **meta)
