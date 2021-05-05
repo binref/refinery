2a6482ace852a54636868d5109637affa445c435
jesko <rattle@nullteilerfrei.de>
2021-05-05 20:27:04 +0200

    do not use alignment by default

--- refinery/units/pattern/struct.py
+++ refinery/units/pattern/struct.py
@@ -10,14 +10,15 @@ class struct(Unit):
     """
     Read structured data from the beginning of a chunk and store the extracted fields in chunk meta variables.
     The structure format is specified in Python struct format, and all remaining arguments to this unit are the
-    names of the variables that receive the values from this struct
+    names of the variables that receive the values from this struct.
     """
     def __init__(
         self,
         spec: arg(type=str, help=(
             'Specify the structure format in Python struct syntax. For example, the string LLxxH will read '
             'two unsigned 32bit integers, then skip two bytes, and then read one unsigned 16bit integer from '
-            'the input data. Three variable names have to be specified to hold these parsed values.'
+            'the input data. Three variable names have to be specified to hold these parsed values. The unit '
+            'defaults to using native byte order with no alignment.'
         )),
         *variables: arg(metavar='variable', type=str, help=(
             'The names of the variables to receive the fields of the parsed struct, in the same order as they '
@@ -30,6 +31,8 @@ class struct(Unit):
     def process(self, data: bytearray):
         try:
             spec = self.args.spec
+            if not any(spec.startswith(f) for f in '<@=!>'):
+                spec = F'={spec}'
             meta = ByteStringWrapper.FormatMap(data, self.codec)
             spec = spec.format_map(meta)
             size = struct_.calcsize(spec)
