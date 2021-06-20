0f0ac7c71561b11357fec9e85c97d1df1709ef46
jesko <rattle@nullteilerfrei.de>
2021-06-20 15:40:30 +0200

    adds tabular option test

--- test/units/sinks/test_ppjson.py
+++ test/units/sinks/test_ppjson.py
@@ -30,3 +30,20 @@ class TestJSON(TestUnitBase):
         test = unit(json.dumps(data, indent=4).encode(unit.codec))
         self.assertEqual(len(test), 38)
         self.assertEqual(json.loads(test), data)
+
+    def test_tabular_output(self):
+        unit = self.load(tabular=True)
+        data = {"binary": "foo", "bar": ["rattle", "tattle", {"baz": 1, "baf": 2}]}
+        data = json.dumps(data).encode(unit.codec)
+        test = {
+            tuple(p.strip() for p in line.split(':'))
+            for line in str(data | unit).splitlines(False)
+        }
+        for line in [
+            ('binary', 'foo'),
+            ('bar[0x0]', 'rattle'),
+            ('bar[0x1]', 'tattle'),
+            ('bar[0x2].baz', '1'),
+            ('bar[0x2].baf', '2')
+        ]:
+            self.assertIn(line, test)
