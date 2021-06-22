7425eea1a41e64a2cdb4911d4320e441a201546d
jesko <rattle@nullteilerfrei.de>
2021-06-22 12:18:00 +0200

    test and bugfix

--- test/units/sinks/test_peek.py
+++ test/units/sinks/test_peek.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import contextlib
 import io
 import sys
 
 from .. import TestUnitBase
 
 
+@contextlib.contextmanager
+def errbuf():
+    sys_stderr = sys.stderr
+    sys.stderr = io.StringIO()
+    yield sys.stderr
+    sys.stderr = sys_stderr
+
+
 class TestPeek(TestUnitBase):
 
     def test_hex_peek(self):
         peek = self.load(width=8, lines=14, meta=True)
-        sys_stderr = sys.stderr
-        sys.stderr = io.StringIO()
-        peek(bytes.fromhex(
-            '4D 5A 90 00 03 00 00 00 04 00 00 00 FF FF 00 00'  # MZ..............
-            'B8 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00'  # ........@.......
-            '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'  # ................
-            '00 00 00 00 00 00 00 00 00 00 00 00 F8 00 00 00'  # ................
-            '0E 1F BA 0E 00 B4 09 CD 21 B8 01 4C CD 21 54 68'  # ........!..L.!Th
-            '69 73 20 70 72 6F 67 72 61 6D 20 63 61 6E 6E 6F'  # is.program.canno
-            '74 20 62 65 20 72 75 6E 20 69 6E 20 44 4F 53 20'  # t.be.run.in.DOS.
-            '6D 6F 64 65 2E 0D 0D 0A 24 00 00 00 00 00 00 00'  # mode....$.......
-        ))
-
-        output = sys.stderr.getvalue()
-        sys.stderr = sys_stderr
+        with errbuf() as stderr:
+            peek(bytes.fromhex(
+                '4D 5A 90 00 03 00 00 00 04 00 00 00 FF FF 00 00'  # MZ..............
+                'B8 00 00 00 00 00 00 00 40 00 00 00 00 00 00 00'  # ........@.......
+                '00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00'  # ................
+                '00 00 00 00 00 00 00 00 00 00 00 00 F8 00 00 00'  # ................
+                '0E 1F BA 0E 00 B4 09 CD 21 B8 01 4C CD 21 54 68'  # ........!..L.!Th
+                '69 73 20 70 72 6F 67 72 61 6D 20 63 61 6E 6E 6F'  # is.program.canno
+                '74 20 62 65 20 72 75 6E 20 69 6E 20 44 4F 53 20'  # t.be.run.in.DOS.
+                '6D 6F 64 65 2E 0D 0D 0A 24 00 00 00 00 00 00 00'  # mode....$.......
+            ))
+            output = stderr.getvalue()
 
         for info in ('entropy = 45.87%', 'MS-DOS'):
             self.assertIn(info, output)
@@ -46,3 +52,11 @@ class TestPeek(TestUnitBase):
             '70: 6D 6F 64 65 2E 0D 0D 0A  mode....\n'),
             output
         )
+
+    def test_regression_all_output(self):
+        data = b'Refining Binaries since 2019'
+        peek = self.load(all=True, decode=True)
+        with errbuf() as stderr:
+            peek(data)
+            test = stderr.getvalue()
+        self.assertIn(data.decode('ascii'), test)
