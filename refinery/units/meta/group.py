d7d079ba1fb0dbc4a6d735e1d537ce8ba14a9705
jesko <rattle@nullteilerfrei.de>
2021-06-25 22:03:23 +0200

    refactors group

--- refinery/units/meta/group.py
+++ refinery/units/meta/group.py
@@ -3,9 +3,6 @@
 from .. import arg, Unit
 
 
-_GROUP_MEMBER_ATTRIBUTE = '_group_members'
-
-
 class group(Unit):
     """
     Group incoming chunks into frames of the given size.
@@ -14,7 +11,7 @@ class group(Unit):
         super().__init__(size=size)
 
     def process(self, data):
-        members = data.__dict__.pop(_GROUP_MEMBER_ATTRIBUTE, [])
+        members = data.temp or ()
         if len(members) >= self.args.size:
             raise RuntimeError(F'received {len(members) + 1} items in group')
         yield data
@@ -31,10 +28,9 @@ class group(Unit):
                 yield header
                 header = None
             if header is None:
+                chunk.temp = members
                 header = chunk
                 members.clear()
-                assert _GROUP_MEMBER_ATTRIBUTE not in header.__dict__
-                setattr(header, _GROUP_MEMBER_ATTRIBUTE, members)
             else:
                 members.append(chunk)
         if header is not None:
