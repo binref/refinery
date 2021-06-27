daf4b6a466769a495e731617c783015326b5bef8
jesko <rattle@nullteilerfrei.de>
2021-06-27 22:31:04 +0200

    improve json extractor

--- refinery/units/formats/json.py
+++ refinery/units/formats/json.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import json
 
-from ...lib.json import flattened
 from . import PathExtractorUnit, UnpackResult
 
 
@@ -10,6 +9,28 @@ class xtjson(PathExtractorUnit):
     """
     Extract values from a JSON document.
     """
+    _STRICT_PATH_MATCHING = True
+
     def unpack(self, data):
-        for path, value in flattened(json.loads(data), separator='/'):
-            yield UnpackResult(path, str(value).encode(self.codec))
+
+        def crawl(path, cursor):
+            if isinstance(cursor, (dict, list)) and path:
+                path = F'{path}/'
+            if isinstance(cursor, dict):
+                for key, value in cursor.items():
+                    yield from crawl(F'{path}{key}', value)
+            elif isinstance(cursor, list):
+                width = len(F'{len(cursor)-1:d}')
+                for key, value in enumerate(cursor):
+                    yield from crawl(F'{path}#{key:0{width}d}', value)
+            if path:
+                yield path, cursor, cursor.__class__.__name__
+
+        for path, item, typename in crawl('', json.loads(data)):
+            def extract(item=item):
+                if isinstance(item, (list, dict)):
+                    dumped = json.dumps(item, indent=4)
+                else:
+                    dumped = str(item)
+                return dumped.encode(self.codec)
+            yield UnpackResult(path, extract, type=typename)
