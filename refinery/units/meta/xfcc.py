c6fd3e133e6ff0486790e91e21db07bc7e95f6b8
jesko <rattle@nullteilerfrei.de>
2021-07-08 02:19:54 +0200

    adds relative option and count variable re-use

--- refinery/units/meta/xfcc.py
+++ refinery/units/meta/xfcc.py
@@ -13,17 +13,33 @@ class xfcc(Unit):
     this leaf as its only child. The leaves of this tree have been enriched with a meta variable containing
     the number of times the corresponding chunk has occurred in the input frame tree.
     """
-    def __init__(self, variable: arg(help='The variable which is used as the accumulator') = 'count'):
-        super().__init__(variable=variable)
+    def __init__(
+        self,
+        variable: arg(help='The variable which is used as the accumulator') = 'count',
+        relative: arg.switch('-r', help='Normalize the accumulator to a number between 0 and 1.') = False
+    ):
+        super().__init__(variable=variable, relative=relative)
         self._trunk = None
         self._store = collections.defaultdict(int)
 
     def finish(self):
+        if self.args.relative and self._store:
+            maximum = max(self._store.values())
         for k, (chunk, count) in enumerate(self._store.items()):
+            if self.args.relative:
+                count /= maximum
             chunk._meta[self.args.variable] = count
             chunk._path = chunk.path[:-2] + (0, k)
             yield chunk
         self._store.clear()
+
+    def _getcount(self, chunk):
+        try:
+            count = int(chunk.meta[self.args.variable])
+        except (AttributeError, KeyError, TypeError):
+            return 1
+        else:
+            return count
 
     def filter(self, chunks):
         it = iter(chunks)
@@ -32,12 +48,17 @@ class xfcc(Unit):
         except StopIteration:
             return
         if len(head.path) < 2:
-            raise ValueError('requires at least two of frame layers')
+            self.log_warn(F'the current frame is nested {len(head.path)} layers deep, at least two layers are required.')
+            yield head
+            for item in it:
+                self.log_debug(repr(item))
+                yield item
+            return
         trunk = head.path[:-2]
         store = self._store
         if trunk != self._trunk:
             yield from self.finish()
             self._trunk = trunk
-        store[head] += 1
+        store[head] += self._getcount(head)
         for chunk in it:
-            store[chunk] += 1
+            store[chunk] += self._getcount(chunk)
