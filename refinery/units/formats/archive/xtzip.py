26f2dcb68c881e2871d5d266aaadb5c9f3fe1cc6
jesko <rattle@nullteilerfrei.de>
2021-07-18 23:45:03 +0200

    test default passwords in zip unit

--- refinery/units/formats/archive/xtzip.py
+++ refinery/units/formats/archive/xtzip.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import zipfile
-import datetime
+from typing import Optional
+
+from datetime import datetime
+from zipfile import ZipInfo, ZipFile
 
 from . import ArchiveUnit
 from ....lib.structures import MemoryFile
@@ -12,20 +14,45 @@ class xtzip(ArchiveUnit):
     Extract files from a Zip archive.
     """
     def unpack(self, data):
-        archive = zipfile.ZipFile(MemoryFile(data))
-        for info in archive.infolist():
-            def xt(archive=archive, info=info, pwd=self.args.pwd):
+        password = self.args.pwd.decode(self.codec)
+        archive = ZipFile(MemoryFile(data))
+
+        if password:
+            archive.setpassword(self.args.pwd)
+        else:
+            def password_invalid(pwd: Optional[str]):
+                if pwd is not None:
+                    archive.setpassword(pwd.encode(self.codec))
                 try:
-                    return archive.read(info.filename, pwd=pwd)
+                    archive.testzip()
                 except RuntimeError as E:
                     if 'password' not in str(E):
                         raise
-                    pwdstr = pwd.decode(self.codec)
-                    raise RuntimeError(F'invalid password: {pwdstr}') from E
+                    return True
+                else:
+                    self.log_debug(pwd)
+                    return False
+            for pwd in [None, *self._COMMON_PASSWORDS]:
+                if not password_invalid(pwd):
+                    break
+            else:
+                raise RuntimeError('Archive is password-protected.')
+
+        for info in archive.infolist():
+            def xt(archive: ZipFile = archive, info: ZipInfo = info):
+                try:
+                    return archive.read(info.filename)
+                except RuntimeError as E:
+                    if 'password' not in str(E):
+                        raise
+                    if not password:
+                        raise RuntimeError('archive is password-protected')
+                    else:
+                        raise RuntimeError(F'invalid password: {password}') from E
             if info.is_dir():
                 continue
             try:
-                date = datetime.datetime(*info.date_time)
+                date = datetime(*info.date_time)
             except Exception:
                 date = None
             yield self._pack(info.filename, date, xt)
