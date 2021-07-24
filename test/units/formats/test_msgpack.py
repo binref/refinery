afefc69493823764d6ad0507d432e72eef36f1d9
jesko <rattle@nullteilerfrei.de>
2021-07-24 21:15:04 +0200

    adds reversible test for msgpack

---
+++ test/units/formats/test_msgpack.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+import inspect
+
+from .. import TestUnitBase
+
+
+class TestMessagePack(TestUnitBase):
+
+    def test_reversible_property(self):
+        @inspect.getdoc
+        class data:
+            """
+            {
+                "jsonrpc": "2.0",
+                "method": "refine",
+                "id": 1254,
+                "params": [
+                    "Binary",
+                    78,
+                    0,
+                    false,
+                    true,
+                    2,
+                    false,
+                    "FOOBAR"
+                ]
+            }
+            """
+        msgpack = self.load()
+        data = data.encode(msgpack.codec)
+        self.assertEqual(
+            json.loads(data),
+            json.loads(str(data | -msgpack | msgpack))
+        )
