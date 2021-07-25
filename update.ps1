5cbb917155092dc7f1c639d05d3e5759411ef6d6
jesko <rattle@nullteilerfrei.de>
2021-07-25 21:58:27 +0200

    adds local install scripts

---
+++ update.ps1
@@ -0,0 +1,15 @@
+param(
+  [string] [ValidateNotNullOrEmpty()] $env = 'venv'
+)
+
+$ErrorActionPreference = "Stop"
+
+if (-not (Test-Path $env)) {
+    . py -3 -m venv $env
+}
+
+& "./$env/Scripts/Activate.ps1"
+
+git pull --rebase --autostash
+pip uninstall -y binary-refinery
+pip install -e .
---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+env="$1"
+
+if [ -z "$env" ]; then
+    env="venv"
+fi
+
+if [ ! -d "$env" ]; then
+  python3 -m venv "$env"
+fi
+
+source "$env/bin/activate"
+
+git pull --rebase --autostash
+pip uninstall -y binary-refinery
+pip install -e .
