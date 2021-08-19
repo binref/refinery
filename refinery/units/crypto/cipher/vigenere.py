ea4023d92ed0473623bfce4b0ce88dd8b264fe19
jesko <rattle@nullteilerfrei.de>
2021-08-20 00:11:22 +0200

    implement slight vigenere variations

--- refinery/units/crypto/cipher/vigenere.py
+++ refinery/units/crypto/cipher/vigenere.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from itertools import cycle
+from operator import (
+    __add__,
+    __sub__,
+    __xor__,
+)
 
 from ... import arg, Unit
 from ....lib.decorators import unicoded
+
+
+_opeator_inverse = {
+    __add__: __sub__,
+    __sub__: __add__,
+    __xor__: __xor__
+}
 
 
 class vigenere(Unit):
@@ -17,17 +29,24 @@ class vigenere(Unit):
         alphabet: arg(
             help='The alphabet, by default the Latin one is used: "{default}"'
         ) = 'abcdefghijklmnopqrstuvwxyz',
+        operator: arg.choice('-:', choices=['add', 'sub', 'xor'], metavar='OP', help=(
+            'Choose the vigenere block operation. The default is {default}, and the available options are: {choices}')) = 'add',
         case_sensitive: arg.switch('-c', help=(
-            'Unless this option is set, the key will be case insensitive and '
-            'the alphabet is assumed to contain only lowercase letters. Any '
-            'uppercase letter is transformed using the same shift as would be '
-            'the lowercase variant, but case is retained.'
-        )) = False,
+            'Unless this option is set, the key will be case insensitive. Uppercase letters from the input are transformed '
+            'using the same shift as would be the lowercase variant, but case is retained.')) = False,
         ignore_unknown: arg.switch('-i', help=(
             'Unless this option is set, the key stream will be iterated even '
             'for letters that are not contained in the alphabet.'
         )) = False
     ):
+        if not callable(operator):
+            operator = {
+                'add': __add__,
+                'sub': __sub__,
+                'xor': __xor__,
+            }.get(operator.lower(), None)
+            if operator is None:
+                raise ValueError(F'The value {operator!r} is not valid as an operator.')
         if not case_sensitive:
             key = key.lower()
             alphabet = alphabet.lower()
@@ -40,6 +59,9 @@ class vigenere(Unit):
     def _tabula_recta(self, data, reverse=True):
         keystream = cycle(self.args.key)
         alphabet_size = len(self.args.alphabet)
+        op = self.args.operator
+        if reverse:
+            op = _opeator_inverse[op]
         for letter in data:
             uppercase = not self.args.case_sensitive and letter.isupper()
             if uppercase:
@@ -52,11 +74,7 @@ class vigenere(Unit):
                     next(keystream)
                 continue
             shift = self.args.alphabet.index(next(keystream))
-            if reverse:
-                position -= shift
-            else:
-                position += shift
-            result = self.args.alphabet[position % alphabet_size]
+            result = self.args.alphabet[op(position, shift) % alphabet_size]
             yield result.upper() if uppercase else result
 
     @unicoded
