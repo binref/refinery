d27ed158369aa3f2708519f3e7f6a375d01b193b
jesko <rattle@nullteilerfrei.de>
2021-08-20 04:24:56 +0200

    removes type hint unavailable in Python 3.7

--- refinery/units/crypto/hash/__init__.py
+++ refinery/units/crypto/hash/__init__.py
@@ -4,17 +4,12 @@
 Implements various hashing algorithms.
 """
 from refinery.units import arg, Unit, abc
-from typing import ByteString, Protocol, Union
-
-
-class HashType(Protocol):
-    def digest(self) -> ByteString: ...
 
 
 class HashUnit(Unit, abstract=True):
 
     @abc.abstractmethod
-    def _algorithm(self, data: bytes) -> Union[ByteString, HashType]:
+    def _algorithm(self, data: bytes):
         raise NotImplementedError
 
     def __init__(self, text: arg('-t', help='Output a hexadecimal representation of the hash.') = False, **kwargs):
@@ -22,8 +17,10 @@ class HashUnit(Unit, abstract=True):
 
     def process(self, data: bytes) -> bytes:
         digest = self._algorithm(data)
-        try: digest = digest.digest()
-        except AttributeError: pass
+        try:
+            digest = digest.digest()
+        except AttributeError:
+            pass
         if self.args.text:
             digest = digest.hex().encode(self.codec)
         return digest
