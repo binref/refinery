5436f64af58e8769db1b69fcb2355acc0dbeed5b
jesko <rattle@nullteilerfrei.de>
2021-09-12 12:55:00 +0200

    mild performance improvement

--- refinery/units/crypto/cipher/rot.py
+++ refinery/units/crypto/cipher/rot.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from ... import arg, Unit
-from ....lib.argformats import number
+
+_UCASE = range(ord('A'), ord('Z') + 1)
+_LCASE = range(ord('a'), ord('z') + 1)
 
 
 class rot(Unit):
@@ -10,14 +12,15 @@ class rot(Unit):
     amount is 13, providing the common (and weak) string obfuscation method.
     """
 
-    def __init__(self, amount: arg(help='Number of letters to rotate by; Default is 13.', type=number[1:25]) = 13):
+    def __init__(self, amount: arg.number(help='Number of letters to rotate by; Default is 13.') = 13):
         super().__init__(amount=amount)
 
-    def process(self, data):
-        def rotate(char):
-            if 0x41 <= char <= 0x5A:  # A-Z
-                return (char - 0x41 + self.args.amount) % 26 + 0x41
-            if 0x61 <= char <= 0x7A:  # a-z
-                return (char - 0x61 + self.args.amount) % 26 + 0x61
-            return char
-        return bytes(rotate(char) for char in data)
+    def process(self, data: bytearray):
+        rot = self.args.amount % 26
+        for index, byte in enumerate(data):
+            for alphabet in _LCASE, _UCASE:
+                if byte in alphabet:
+                    zero = alphabet[0]
+                    data[index] = zero + (byte - zero + rot) % 26
+                    break
+        return data
