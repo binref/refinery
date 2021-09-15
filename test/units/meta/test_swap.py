30fe9d9515603fb5111ab561c976ff1b00f65859
jesko <rattle@nullteilerfrei.de>
2021-09-15 03:41:10 +0200

    adds swap tests

---
+++ test/units/meta/test_swap.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from refinery.lib.loader import load_detached as L
+from .. import TestUnitBase
+
+
+class TestSwap(TestUnitBase):
+
+    def test_swap_variable_01(self):
+        pl = L('emit BAZ [') | L('put BAR S:FOO') | L('put FOO S:BAR') | L('swap FOO BAR') | L('cfmt {FOO}{BAR} ]')
+        self.assertEqual(pl(), B'FOOBAR')
+
+    def test_swap_variable_02(self):
+        pl = L('emit BAZ [') | L('put BAR FOO') | L('swap BAR FOO') | L('cfmt {FOO} ]')
+        self.assertEqual(pl(), B'FOO')
+
+    def test_swap_with_data(self):
+        pl = L('emit BAR [') | L('put BAR FOO') | L('swap BAR') | L('cfmt {}{BAR}{FOO} ]')
+        self.assertEqual(pl(), B'FOOBAR{FOO}')
