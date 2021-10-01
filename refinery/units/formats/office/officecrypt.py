f52cdee436e2122f1726e5ab1ab9de33f648862a
jesko <rattle@nullteilerfrei.de>
2021-10-02 01:42:32 +0200

    adds Excel default password

--- refinery/units/formats/office/officecrypt.py
+++ refinery/units/formats/office/officecrypt.py
@@ -10,13 +10,20 @@ class officecrypt(Unit):
     A simple proxy for the `msoffcrypto` package to decrypt office documents.
     """
 
-    def __init__(self, password: arg(help='The document password.', type=str)):
+    def __init__(self, password: arg.binary(help=(
+        'The document password. By default, the Excel default password "{default}" is used.'
+    )) = b'VelvetSweatshop'):
         super().__init__(password=password)
 
     def process(self, data):
+        password: bytes = self.args.password
         with MemoryFile(data) as stream:
             doc = msoffcrypto.OfficeFile(stream)
-            doc.load_key(password=self.args.password)
+            if not doc.is_encrypted():
+                self.log_warn('the document is not encrypted; returning input')
+                return data
+            if password:
+                doc.load_key(password=password.decode(self.codec))
             with MemoryFile(bytearray()) as output:
                 doc.decrypt(output)
                 return output.getvalue()
