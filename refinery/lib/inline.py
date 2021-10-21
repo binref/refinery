1afa5e38f61ce2f4b88e3ee3ddd51cb881c07497
jesko <rattle@nullteilerfrei.de>
2021-10-21 03:42:18 +0200

    adds inline module and tests

---
+++ refinery/lib/inline.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from typing import Any, Callable, Generator, Optional, Type, TypeVar, Union
+
+import ast
+import inspect
+import functools
+import sys
+
+from ast import (
+    Assign,
+    BinOp,
+    BitAnd,
+    Bytes,
+    Call,
+    Constant,
+    Expr,
+    For,
+    FunctionDef,
+    GeneratorExp,
+    Load,
+    Name,
+    NodeTransformer,
+    Num,
+    Return,
+    Store,
+    Str,
+    Yield,
+    arg,
+    comprehension,
+)
+
+_R = TypeVar('_R')
+
+
+class inline(Callable[..., _R]):
+    def __init__(self, function: Callable[..., _R]):
+        self._function = function
+        functools.update_wrapper(self, function)
+
+    def __call__(self, *args, **kwargs):
+        return self._function(*args, **kwargs)
+
+
+def getsource(f):
+    return inspect.cleandoc(F'\n{inspect.getsource(f)}')
+
+
+class ArgumentCountMismatch(ValueError):
+    pass
+
+
+class NoFunctionDefinitionFound(ValueError):
+    pass
+
+
+def iterspread(
+    function: Callable[..., _R],
+    iterator,
+    *inline_args,
+    mask: Optional[int] = None
+) -> Callable[..., Generator[_R, None, None]]:
+
+    code = ast.parse(getsource(function))
+    closure_vars = inspect.getclosurevars(function)
+    context = closure_vars.nonlocals
+    reserved_names = set(context)
+    reserved_names.update(closure_vars.globals)
+    reserved_names.update(closure_vars.builtins)
+
+    try:
+        function_head = next(node for node in ast.walk(code) if isinstance(node, FunctionDef))
+    except StopIteration:
+        raise NoFunctionDefinitionFound
+
+    function_name = None
+
+    def as_arg(name: str): return F'_arg_{name}'
+    def as_var(name: str): return F'_var_{name}'
+    def as_tmp(name: str): return F'_tmp_{name}'
+
+    def apply_node_transformation(cls: Type[NodeTransformer]):
+        nonlocal code
+        code = ast.fix_missing_locations(cls().visit(code))
+
+    if sys.version_info >= (3, 8):
+        def constant(value):
+            return Constant(value=value)
+    else:
+        @functools.singledispatch
+        def constant(value: Union[int, str, bytes]):
+            raise NotImplementedError(F'The type {type(value).__name__} is not supported for inlining')
+        @constant.register # noqa
+        def _(value: bytes): return Bytes(s=value)
+        @constant.register
+        def _(value: int): return Num(n=value)
+        @constant.register
+        def _(value: str): return Str(s=value)
+
+    @apply_node_transformation
+    class _(NodeTransformer):
+        def visit_Name(self, node: Name) -> Any:
+            if node.id in reserved_names:
+                return node
+            node.id = as_var(node.id)
+            return node
+
+    @apply_node_transformation
+    class _(NodeTransformer):
+        def visit_FunctionDef(self, node: FunctionDef):
+            nonlocal function_name, context
+            if node is not function_head:
+                return node
+            function_body = []
+            function_name = node.name
+            function_args = [arg.arg for arg in node.args.args]
+            inlined_start = 1
+
+            if inspect.ismethod(function):
+                inlined_start += 1
+
+            iterator_name = function_args[inlined_start - 1]
+            function_args[:inlined_start] = []
+            arity = len(function_args)
+
+            try:
+                vararg = as_arg(node.args.vararg.arg)
+            except Exception:
+                if arity != len(inline_args):
+                    raise ArgumentCountMismatch
+            else:
+                context[vararg] = inline_args[arity:]
+
+            for name, value in zip(function_args, inline_args):
+                targets = [Name(id=as_var(name), ctx=Store())]
+                if isinstance(value, (int, str, bytes)):
+                    context[as_var(name)] = value
+                else:
+                    context[as_arg(name)] = value
+                    function_body.append(Assign(
+                        targets=targets,
+                        value=Call(
+                            func=Name(id='next', ctx=Load()),
+                            args=[Name(id=as_arg(name), ctx=Load())],
+                            keywords=[]
+                        )))
+
+            if node.args.vararg:
+                name = node.args.vararg.arg
+                function_body.append(Assign(
+                    targets=[
+                        Name(id=as_var(name), ctx=Store())
+                    ],
+                    value=Call(
+                        func=Name(id='tuple', ctx=Load()),
+                        args=[GeneratorExp(
+                            elt=Call(
+                                func=Name(id='next', ctx=Load()),
+                                args=[Name(id=as_tmp(name), ctx=Load())],
+                                keywords=[]
+                            ),
+                            generators=[comprehension(
+                                is_async=0,
+                                target=Name(id=as_tmp(name), ctx=Store()),
+                                iter=Name(id=as_arg(name), ctx=Load()),
+                                ifs=[]
+                            )]
+                        )],
+                        keywords=[]
+                    )
+                ))
+
+            function_body.extend(node.body)
+            context[as_arg(iterator_name)] = iterator
+            function_body = [For(
+                target=Name(id=as_var(iterator_name), ctx=Store()),
+                iter=Name(id=as_arg(iterator_name), ctx=Load()),
+                body=function_body,
+                orelse=[]
+            )]
+
+            node.body = function_body
+            node.args.args = [arg(arg=as_var('self'))]
+            node.args.vararg = None
+            node.decorator_list = []
+            return node
+
+    @apply_node_transformation
+    class _(NodeTransformer):
+        def visit_Return(self, node: Return) -> Any:
+            value = node.value
+            if mask is not None:
+                value = BinOp(left=value, op=BitAnd(), right=constant(mask))
+            return Expr(value=Yield(value=value))
+
+    bin = compile(code, '<inlined>', 'exec', optimize=2)
+    exec(bin, context, context)
+    return context[function_name]
---
+++ test/lib/test_inline.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import itertools
+
+from refinery.lib.inline import iterspread
+
+from .. import TestBase
+
+
+class TestIterSpread(TestBase):
+
+    def test_simple_01(self):
+        def test(a, b, c):
+            return a + b - c
+        inlined = iterspread(test, range(10, 16), itertools.count(), 5)
+        result = list(inlined(None))
+        self.assertListEqual(result, [5, 7, 9, 11, 13, 15])
+
+    def test_varargs(self):
+        class cls:
+            def test(self, a, b, *value):
+                f, g = value
+                return self.value + a + b - (f * g)
+            value = 100
+        obj = cls()
+        inlined = iterspread(obj.test, range(4), 100, itertools.count(), itertools.count())
+        result = list(inlined(obj))
+        self.assertListEqual(result, [200, 200, 198, 194])
+
+    def test_empty_varargs(self):
+        class cls:
+            def test(self, a, b, *c):
+                r = self.value + a + b
+                for k in c:
+                    r -= k
+                return r
+            value = 100
+        obj = cls()
+        inlined = iterspread(obj.test, range(4), 100)
+        result = list(inlined(obj))
+        self.assertListEqual(result, [200, 201, 202, 203])
+
+    def test_masked(self):
+        def test(a, b): return a - b
+        inlined = bytes(iterspread(test, range(0x10), 0x55, mask=0xFF)(None))
+        self.assertEqual(inlined, bytes(b - 0x55 & 0xFF for b in range(0x10)))
+
+    def test_reserved_names_unbound(self):
+        def test(a, b):
+            return a - abs(b)
+        inlined = bytes(iterspread(test, range(0x10), -0x55, mask=0xFF)(None))
+        self.assertEqual(inlined, bytes(b - 0x55 & 0xFF for b in range(0x10)))
+
+    def test_reserved_names_bound(self):
+        class cls:
+            def __init__(self):
+                def test(a, b):
+                    return self.value + a - abs(b)
+                self.test = test
+            value = 100
+        obj = cls()
+        inlined = bytes(iterspread(obj.test, range(0x10), -0x55, mask=0xFF)(obj))
+        self.assertEqual(inlined, bytes(b + 100 - 0x55 & 0xFF for b in range(0x10)))
