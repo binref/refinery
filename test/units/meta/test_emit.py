54cdbac77abd08175bd280261de1cb25f8285fe8
jesko <rattle@nullteilerfrei.de>
2021-11-01 11:15:47 +0100

    adds clipboard tests

--- test/units/meta/test_emit.py
+++ test/units/meta/test_emit.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import pyperclip
+import contextlib
+
+from refinery.lib.loader import load_pipeline
 from .. import TestUnitBase
+
+
+@contextlib.contextmanager
+def temporary_clipboard(data: str):
+    backup = pyperclip.paste()
+    pyperclip.copy(data)
+    try:
+        yield None
+    finally:
+        pyperclip.copy(backup)
 
 
 class TestEmitter(TestUnitBase):
@@ -16,3 +30,15 @@ class TestEmitter(TestUnitBase):
     def test_prepend_and_append(self):
         emit = self.load('Hello', 'x::', 'World')
         self.assertEqual(emit(B'cruel'), B'Hello\ncruel\nWorld')
+
+    def test_emit_keeps_metadata_01(self):
+        with temporary_clipboard('baz'):
+            pl = load_pipeline('emit a [| put foo bar | emit | cfmt {foo}{} ]')
+            pl = bytes(pl())
+        self.assertEqual(pl, b'barbaz')
+
+    def test_emit_keeps_metadata_02(self):
+        with temporary_clipboard('baz'):
+            pl = load_pipeline('emit bort | push [[| rex (?P<foo>...)t | pop ]| emit | cfmt {foo}{} ]')
+            pl = bytes(pl())
+        self.assertEqual(pl, b'borbaz')
