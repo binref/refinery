a90bff258fd47a1724997ba674d539fc00b30afc
jesko <rattle@nullteilerfrei.de>
2021-11-10 01:37:00 +0100

    performance improvements

--- refinery/units/blockwise/map.py
+++ refinery/units/blockwise/map.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from typing import Dict, Optional, Sequence
 from refinery.units.blockwise import arg, BlockTransformation
 
 
@@ -8,6 +9,8 @@ class map(BlockTransformation):
     Each block of the input data which occurs as a block of the index argument
     is replaced by the corresponding block of the image argument.
     """
+    _map: Optional[Dict[int, int]]
+
     def __init__(
         self,
         index: arg(help='index characters'),
@@ -15,12 +18,27 @@ class map(BlockTransformation):
         blocksize=1
     ):
         super().__init__(blocksize=blocksize, index=index, image=image)
+        self._map = None
 
     def process(self, data):
-        self._map = dict(zip(
-            self.chunk(self.args.index),
-            self.chunk(self.args.image)))
-        return super().process(data)
+        index: Sequence[int] = self.args.index
+        image: Sequence[int] = self.args.image
+        if len(set(index)) != len(index):
+            raise ValueError('The index sequence contains duplicates.')
+        if len(index) > len(image):
+            raise ValueError('The index sequence is longer than the image sequence.')
+        if self.bytestream:
+            mapping = dict(zip(index, image))
+            mapping = bytes(mapping.get(c, c) for c in range(0x100))
+            if not isinstance(data, bytearray):
+                data = bytearray(data)
+            data[:] = (mapping[b] for b in data)
+            return data
+        try:
+            self._map = dict(zip(self.chunk(index), self.chunk(image)))
+            return super().process(data)
+        finally:
+            self._map = None
 
     def process_block(self, token):
         return self._map.get(token, token)
