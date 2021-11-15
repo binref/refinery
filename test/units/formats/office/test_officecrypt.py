1cfb2cf137d13e87ede6034fee7e1f54e6954b27
jesko <rattle@nullteilerfrei.de>
2021-11-16 00:32:18 +0100

    adds officecrypt test

---
+++ test/units/formats/office/test_officecrypt.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from ... import TestUnitBase
+from .test_doctxt import PARAGRAPHS
+
+
+class TestOfficeCrypt(TestUnitBase):
+
+    def test_simple_samples(self):
+        crypt = self.load('space-cowboy')
+        doctxt = self.ldu('doctxt')
+        data = self.download_sample('e12a6f21e62a300ee86a26d8a1f876113bebf1b52709421d4894f832dd54bcf1')
+        output = str(data | crypt | doctxt)
+        for p in PARAGRAPHS:
+            self.assertIn(p, output)
