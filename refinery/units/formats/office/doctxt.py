47d23ef5caced77add7b377acfaabf9b752a4015
jesko <rattle@nullteilerfrei.de>
2021-11-15 21:19:22 +0100

    style consistency / cosmetics

--- refinery/units/formats/office/doctxt.py
+++ refinery/units/formats/office/doctxt.py
@@ -15,7 +15,7 @@ class doctxt(Unit):
     Extract text from Word Documents
     """
 
-    @Unit.Requires("olefile", optional=False)
+    @Unit.Requires('olefile', optional=False)
     def _olefile():
         import olefile
         return olefile
@@ -49,23 +49,23 @@ class doctxt(Unit):
     def _extract_ole(self, data: bytearray) -> bytes:
         stream = MemoryFile(data)
         with self._olefile.OleFileIO(stream) as ole:
-            s: self._olefile.OleFileIO = ole.openstream("WordDocument")
+            s: self._olefile.OleFileIO = ole.openstream('WordDocument')
             doc: bytes = s.read()
             fib: bytes = doc[:1472]
             table: bytes = self._load_table(ole, fib)
             piece_table: bytes = self._load_piece_table(table)
             text: str = self._get_text(doc, piece_table)
-            return text.encode("utf-8")
+            return text.encode('utf-8')
 
     def _extract_docx(self, data: Chunk) -> bytes:
         NAMESPACE: str = (
-            "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}"
+            '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
         )
-        PARAGRAPH: str = f"{NAMESPACE}p"
-        TEXT: str = f"{NAMESPACE}t"
+        PARAGRAPH: str = F'{NAMESPACE}p'
+        TEXT: str = F'{NAMESPACE}t'
 
         for part in xtzip().unpack(data):
-            if part.path != "word/document.xml":
+            if part.path != 'word/document.xml':
                 continue
             xml_content: bytes = part.get_data()
             root: ElementTree.Element = ElementTree.fromstring(xml_content)
@@ -76,55 +76,55 @@ class doctxt(Unit):
                     if node.text:
                         texts.append(node.text)
                 if texts:
-                    paragraphs.append("".join(texts))
+                    paragraphs.append(''.join(texts))
             if not paragraphs:
-                raise ValueError("found no text")
-            return "\n\n".join(paragraphs).encode("utf-8")
+                raise ValueError('found no text')
+            return '\n\n'.join(paragraphs).encode('utf-8')
 
     def _extract_odt(self, data: bytes):
         def _extract_text(node: ElementTree.Element):
-            NAMESPACE2: str = "{urn:oasis:names:tc:opendocument:xmlns:text:1.0}"
-            PARAGRAPH: str = f"{NAMESPACE2}p"
-            SPAN: str = f"{NAMESPACE2}span"
-            SPACE: str = f"{NAMESPACE2}s"
-            res: str = ""
-            for element in node.findall("*"):
+            NAMESPACE2: str = '{urn:oasis:names:tc:opendocument:xmlns:text:1.0}'
+            PARAGRAPH: str = F'{NAMESPACE2}p'
+            SPAN: str = F'{NAMESPACE2}span'
+            SPACE: str = F'{NAMESPACE2}s'
+            res: str = ''
+            for element in node.findall('*'):
                 tag: str = element.tag
                 text: str = element.text
                 if tag in [PARAGRAPH, SPAN]:
                     if text:
                         res += text
                 elif tag == SPACE:
-                    res += " "
+                    res += ' '
                 res += _extract_text(element)
 
                 if tag == PARAGRAPH:
-                    res += "\n\n"
+                    res += '\n\n'
             return res
 
-        NAMESPACE: str = "{urn:oasis:names:tc:opendocument:xmlns:office:1.0}"
-        BODY: str = f"{NAMESPACE}body"
-        TEXT: str = f"{NAMESPACE}text"
+        NAMESPACE: str = '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}'
+        BODY: str = F'{NAMESPACE}body'
+        TEXT: str = F'{NAMESPACE}text'
         res = None
         for part in xtzip().unpack(data):
-            if part.path != "content.xml":
+            if part.path != 'content.xml':
                 continue
             xml_content: bytes = part.get_data()
             root: ElementTree.Element = ElementTree.fromstring(xml_content)
 
             body: ElementTree.Element = root.find(BODY)
             text: ElementTree.Element = body.find(TEXT)
-            res: str = _extract_text(text).encode("utf-8")
+            res: str = _extract_text(text).encode('utf-8')
         if not res:
-            raise ValueError("found no text")
+            raise ValueError('found no text')
         return res
 
     def _get_unit32(self, data: bytes, offset: int) -> int:
-        return struct.unpack("<I", data[offset : offset + 4])[0]
+        return struct.unpack('<I', data[offset : offset + 4])[0]
 
     def _table_stream_name(self, fib: bytes) -> str:
         bit = (fib[0xB] >> 1) & 1
-        return f"{bit}Table"
+        return F'{bit}Table'
 
     def _load_table(self, ole, fib: bytes) -> bytes:
         offset: int = self._get_unit32(fib, 0x1A2)
@@ -155,7 +155,7 @@ class doctxt(Unit):
         for i in range(piece_count + 1):
             character_positions.append(self._get_unit32(piece_table, i * 4))
 
-        text: str = ""
+        text: str = ''
         for i in range(piece_count):
             cp_start: int = character_positions[i]
             cp_end: int = character_positions[i + 1]
@@ -167,14 +167,14 @@ class doctxt(Unit):
             cb: int = cp_end - cp_start
             if is_ansi:
                 fc = fc // 2
-                encoding: str = "cp1252"
+                encoding: str = 'cp1252'
             else:
-                encoding: str = "utf16"
+                encoding: str = 'utf16'
                 cb *= 2
             raw: bytes = doc[fc : fc + cb]
             if is_ansi:
-                raw = raw.replace(b"\r", b"\n")
+                raw = raw.replace(B'\r', B'\n')
             else:
-                raw = raw.replace(b"\x00\r", b"\x00\n")
+                raw = raw.replace(B'\x00\r', B'\x00\n')
             text += raw.decode(encoding)
         return text
