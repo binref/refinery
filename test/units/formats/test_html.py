5c51f5947a34b6dbb93321d73e9722eb57f11084
jesko <rattle@nullteilerfrei.de>
2022-03-17 09:12:02 +0100

    adjusts tests

--- test/units/formats/test_html.py
+++ test/units/formats/test_html.py
@@ -7,51 +7,54 @@ from .. import TestUnitBase
 
 class TestHTMLExtractor(TestUnitBase):
 
-    def test_simple_01(self):
+    _TEST_DOCUMENT = inspect.cleandoc(
+        """
+        <html>
+            <head>
+                <title>Test &nbsp;</title>
+                <script>alert(9)</script>
+                <woober oink=9 />
+            </head>
+            <body>
+                <p>Paragraph 1 &#62; &#x3E;</p>
+                <p>Paragraph 2</p>
+                <p>Paragraph 3</p>
+                <div><div>Half-Open
+            </body>
+            </div>
+        </html>
+        """
+    ).encode('utf8')
 
-        @inspect.getdoc
-        class data:
-            """
-            <html>
-                <head>
-                    <title>Test &nbsp;</title>
-                    <script>alert(9)</script>
-                    <woober oink=9 />
-                </head>
-                <body>
-                    <p>Paragraph 1 &#62; &#x3E;</p>
-                    <p>Paragraph 2</p>
-                    <p>Paragraph 3</p>
-                    <div><div>Half-Open
-                </body>
-                </div>
-            </html>
-            """
-
-        data = data.encode('utf8')
-
+    def test_listing(self):
+        listing = str(self._TEST_DOCUMENT | self.load('-l', '??*'))
         self.assertEqual(
-            str(data | self.load('-l', '*.inner')),
+            listing,
             '\n'.join((
-                'html.inner',
-                'html/head.inner',
-                'html/head/title.inner',
-                'html/head/script.inner',
-                'html/head/woober.inner',
-                'html/body.inner',
-                'html/body/p(1).inner',
-                'html/body/p(2).inner',
-                'html/body/p(3).inner',
-                'html/body/div.inner',
-                'html/body/div/div.inner',
+                'html',
+                'html/head',
+                'html/head/title',
+                'html/head/script',
+                'html/head/woober',
+                'html/body',
+                'html/body/p(1)',
+                'html/body/p(2)',
+                'html/body/p(3)',
+                'html/body/div',
+                'html/body/div/div',
             ))
         )
+
+    def test_extraction(self):
         self.assertEqual(
-            str(data | self.load('p(1).inner')),
+            str(self._TEST_DOCUMENT | self.load('p(1)')),
             'Paragraph 1 &#62; &#x3E;'
         )
+
+    def test_extraction_outer(self):
+        result = str(self._TEST_DOCUMENT | self.load('*/head', outer=True))
         self.assertEqual(
-            str(data | self.load('head.outer')),
+            result,
             '\n'.join((
                 '    <head>',
                 '        <title>Test &nbsp;</title>',
