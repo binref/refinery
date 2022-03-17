5d94c13b92bcba7e9873bc7f16b37767f6b9f822
jesko <rattle@nullteilerfrei.de>
2022-03-17 12:35:38 +0100

    adds test and bugfix for path pattern

---
+++ test/units/meta/test_iffp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# flake8: noqa
+from refinery.lib.loader import load_pipeline
+from .. import TestUnitBase
+
+
+class TestIfPattern(TestUnitBase):
+
+    def test_simple_01(self):
+        ps = BR'"C:\\work\\is\\fun\\"'.hex()
+        result = load_pipeline(RF'emit H:{ps} | carve -d string [| iffp path ]')
+        result = result()
+        self.assertEqual(result, B'C:\\work\\is\\fun\\')
