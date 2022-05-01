6c570ded03e119fcfe9275bdf2a753bb5214b551
jesko <rattle@nullteilerfrei.de>
2022-05-01 14:10:14 +0200

    adds test

--- test/units/meta/test_push.py
+++ test/units/meta/test_push.py
@@ -37,3 +37,10 @@ class TestMetaPushPop(TestUnitBase):
     def test_nested_push_pop(self):
         pl = L('emit FOOBAR [| push [| snip 3: | push [| snip :1 | peek | pop a ]| cca var:a | pop b ]| ccp var:b ]')
         self.assertEqual(pl(), B'BARBFOOBAR')
+
+    def test_multiple_pops(self):
+        data = B'$a = "foo"; $b = "bar"; $c = "baz"; decode-decode("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", $a, $b, $c)'
+        pl = L('push [[| carve -dm5 string | pop foo bar baz ]| carve -sd string | cfmt {foo}-{bar}-{baz}-{} ]')
+        result = pl(data)
+        self.assertEqual(result,
+            B'foo-bar-baz-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
