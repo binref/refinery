35a5cbe3ca22fd7be4e2973f94f7b8582999628a
jesko <rattle@nullteilerfrei.de>
2022-05-09 11:16:12 +0200

    adds padding tests

---
+++ test/units/encoding/test_b32.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from .. import TestUnitBase
+
+
+class TestB32(TestUnitBase):
+
+    def test_too_much_padding(self):
+        unit = self.load()
+        self.assertEqual(B'KRSXG5A====' | unit | bytes, B'Test')
+
+    def test_too_little_padding(self):
+        unit = self.load()
+        self.assertEqual(B'KRSXG5A' | unit | bytes, B'Test')
+
+    def test_correct_existing_padding(self):
+        unit = self.load()
+        self.assertEqual(B'KRSXG5A=' | unit | bytes, B'Test')
