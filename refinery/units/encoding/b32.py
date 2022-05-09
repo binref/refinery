062eca2338edd5bf7f02b5da28fe8e54ba582c13
jesko <rattle@nullteilerfrei.de>
2022-05-09 11:14:21 +0200

    detect and fix incorrect base32 padding

--- refinery/units/encoding/b32.py
+++ refinery/units/encoding/b32.py
@@ -12,5 +12,17 @@ class b32(Unit):
     def reverse(self, data):
         return base64.b32encode(data)
 
-    def process(self, data):
+    def process(self, data: bytearray):
+        before_padding = 0
+        for before_padding in range(len(data), 0, -1):
+            if data[before_padding - 1:before_padding] != B'=':
+                break
+        padding_size = -before_padding % 8
+        missing = before_padding + padding_size - len(data)
+        if missing > 0:
+            self.log_info(F'detected incorrect padding: added {missing} padding characters')
+            data.extend(B'=' * missing)
+        if missing < 0:
+            self.log_info(F'detected incorrect padding: removed {-missing} padding characters')
+            data[padding_size + before_padding:] = []
         return base64.b32decode(data, casefold=True)
