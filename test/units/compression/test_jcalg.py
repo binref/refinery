9a33ae947352d58ceeb918cca79911efeecaa216
jesko <rattle@nullteilerfrei.de>
2022-05-25 00:41:30 +0200

    fix typo

--- test/units/compression/test_jcalg.py
+++ test/units/compression/test_jcalg.py
@@ -4,7 +4,7 @@ from refinery.lib.loader import load_pipeline as L
 from .. import TestUnitBase
 
 
-class TestLZIP(TestUnitBase):
+class TestJCALG(TestUnitBase):
 
     def test_vnc_backdoor_sample(self):
         data = self.download_sample('6d9e2f54382ea697203d714424caefdacf1524c001efbaa7c33320738301808d')
