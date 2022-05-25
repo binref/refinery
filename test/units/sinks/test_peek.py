3d8907e72024373b5671e5f5aa2affc01dae739d
jesko <rattle@nullteilerfrei.de>
2022-05-25 12:33:05 +0200

    fixes test; re-release

--- test/units/sinks/test_peek.py
+++ test/units/sinks/test_peek.py
@@ -195,7 +195,7 @@ class TestPeek(TestUnitBase):
     def test_printable_decoded(self):
         desired = inspect.cleandoc(
             """
-            --CODEC=UTF8--------------------------------------------------------------------
+            -----------------------------------------------------------------------[utf8]---
                 Another one got caught today, it's all over the papers.  "Teenager
             Arrested in Computer Crime Scandal", "Hacker Arrested after Bank Tampering"...
                 Damn kids.  They're all alike.
