30476c4c9da76059b46d744b8297438c24fdd5a1
jesko <rattle@nullteilerfrei.de>
2022-05-31 01:25:31 +0200

    bugfix and interface improvement

--- refinery/units/blockwise/xkey.py
+++ refinery/units/blockwise/xkey.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from collections import Counter
 from refinery.units.blockwise import Arg, BlockTransformationBase
+from refinery.lib.argformats import sliceobj
 
 
 class xkey(BlockTransformationBase):
@@ -14,29 +15,35 @@ class xkey(BlockTransformationBase):
     """
     def __init__(
         self,
-        min: Arg.Number('-n', help='minimum length to try, the default is {default}.') = 1,
-        max: Arg.Number('-m', help='maximum length to try, the default is {default}.') = 0x200,
+        range: Arg(type=sliceobj, help='range of length values to try, the default is 1:512.') = slice(1, 0x200),
         blocksize=1,
     ):
-        super().__init__(min=min, max=max, blocksize=blocksize)
+        super().__init__(range=range, blocksize=blocksize)
 
     def process(self, data: bytearray):
         score = 0
         guess = None
+        bounds: slice = self.args.range
 
         if self.bytestream:
             data = memoryview(data)
         else:
             data = [bytes(c) for c in self.chunk(data, raw=True)]
 
-        for length in range(self.args.min , min(self.args.max, len(data))):
+        n = len(data)
+        start = bounds.start or 1
+        step = bounds.step or 1
+        stop = min(bounds.stop or n, n)
+        self.log_debug(F'redeived input range [{bounds.start}:{bounds.stop}:{bounds.step}], using [{start}:{stop}:{step}]')
+
+        for length in range(start, stop, step):
             _guess = [Counter(data[j::length]).most_common(1)[0] for j in range(length)]
             _score = sum(count for _, count in _guess)
-            if _score == score and len(_guess) > 10 and len(_guess) % len(guess) == 0:
-                self.log_info('found same score at twice the length of best performer, exiting')
+            if _score == score and guess and len(guess) > 10 and len(_guess) % len(guess) == 0:
+                self.log_info('found same score at multiple of length of best performer, exiting')
                 break
             if _score > score:
-                self.log_info(F'got score {_score*100/len(data):5.2f}% for length {length}')
+                self.log_info(F'got score {_score*100/n:5.2f}% for length {length}')
                 score = _score
                 guess = (value for value, _ in _guess)
                 guess = bytearray(guess) if self.bytestream else B''.join(guess)
