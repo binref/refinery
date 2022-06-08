457e69a9fabfdbc2d4b9bf51983ee9f7a6c7dc11
jesko <rattle@nullteilerfrei.de>
2022-06-08 23:36:57 +0200

    adds seed value option to xxhash

--- refinery/units/crypto/hash/xxhash.py
+++ refinery/units/crypto/hash/xxhash.py
@@ -8,5 +8,12 @@ class xxh(HashUnit):
     """
     Implements the xxHash hashing algorithm.
     """
+    def __init__(
+        self,
+        seed: HashUnit.Arg.Number(metavar='seed', help='specify the seed value; the default is {default}') = 0,
+        text=False
+    ):
+        super().__init__(text, seed=seed)
+
     def _algorithm(self, data):
-        return xxhash(data)
+        return xxhash(data, self.args.seed)
