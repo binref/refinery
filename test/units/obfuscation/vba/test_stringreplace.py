81a23e1af611dd03a232d38cf0b720b232dda1ac
jesko <rattle@nullteilerfrei.de>
2022-08-21 13:24:54 +0200

    adds test and bugfix

---
+++ test/units/obfuscation/vba/test_stringreplace.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from ... import TestUnitBase
+
+
+class TestStringReplace(TestUnitBase):
+
+    def setUp(self):
+        super().setUp()
+        self.unit = self.load()
+
+    def test_trivial(self):
+        self.assertEqual(
+            self.unit.deobfuscate(
+                '''impairingsgutta = Replace("ADs@j|P3FODBs@j|P3F.Sts@j|P3Fs@j|P3Fs@j|P3Fream", "s@j|P3F", "")'''),
+            'impairingsgutta = "ADODB.Stream"'
+        )
