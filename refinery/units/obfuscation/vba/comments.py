8b0f38aec982d65dcecf8857a21b34d45910e641
jesko <rattle@nullteilerfrei.de>
2022-08-21 13:54:07 +0200

    adds test and bugfix

--- refinery/units/obfuscation/vba/comments.py
+++ refinery/units/obfuscation/vba/comments.py
@@ -7,9 +7,4 @@ from refinery.units.obfuscation import Deobfuscator
 
 class deob_vba_comments(Deobfuscator):
     def deobfuscate(self, data):
-        return re.sub(
-            R"^\s{0,20}(?:'|rem\b|dim\b).*$\n\r?",
-            '',
-            data,
-            flags=re.MULTILINE | re.IGNORECASE
-        )
+        return re.sub(R"(?im)^\s{0,20}(?:'|rem\b|dim\b).*(?:\Z|$\n\r?)", '', data)
---
+++ test/units/obfuscation/vba/test_comments.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from ... import TestUnitBase
+
+
+class TestCommentRemover(TestUnitBase):
+
+    def setUp(self):
+        super().setUp()
+        self.unit = self.load()
+
+    def test_simple(self):
+        self.assertEqual(self.unit.deobfuscate(r'''
+            ' Test
+            b = a
+            ' Test''').strip(), r'b = a')
