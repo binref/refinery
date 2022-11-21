638097c232c99b3a39df21291e66b1ed7f83f679
jesko <rattle@nullteilerfrei.de>
2022-11-21 01:57:16 +0100

    removes dependence on scope

--- refinery/units/meta/mvc.py
+++ refinery/units/meta/mvc.py
@@ -8,14 +8,8 @@ class mvc(Unit):
     """
     Clean up meta variables in the current scope.
     """
-    def __init__(self):
-        super().__init__(age=age)
-
     def process(self, data: Chunk):
         meta = metavars(data)
-        scope = data.scope
         for key in list(meta.keys()):
-            if meta.get_scope(key, scope) < scope:
-                continue
             meta.discard(key)
         return data
