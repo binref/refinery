f0886e5f14aebe5126280f6c5a49d3274a25c54e
jesko <rattle@nullteilerfrei.de>
2022-11-22 16:57:29 +0100

    improve tests

--- test/units/meta/test_mvg.py
+++ test/units/meta/test_mvg.py
@@ -18,9 +18,13 @@ class TestMetaVarGlobal(TestMetaBase):
         pl = L('emit FOO [| nop [[| put x BAR | mvg -t ]| nop ]| cfmt {}{x} ]')
         self.assertEqual(pl(), B'FOOBAR')
 
-    def test_mvg_04(self):
+    def test_cannot_propagate_variables_with_ambiguous_values(self):
         pl = L('emit FOO [| rex . [| put x | mvg ]| cfmt {}{x} ]')
         self.assertEqual(pl(), B'FOO{x}')
+
+    def test_can_propagate_variables_with_unambiguous_values(self):
+        pl = L('emit FOO [| rex . [| put x BAR | mvg ]| cfmt {}{x} ]')
+        self.assertEqual(pl(), B'FOOBAR')
 
     def test_scope_cannot_be_increased(self):
         pl = L('emit s: [| put x alpha [| nop [| put x beta | mvg ]| nop ]| cfmt {x} ]')
