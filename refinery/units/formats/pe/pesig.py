f13a379411a901d667d6c8fae944f648eb074acd
jesko <rattle@nullteilerfrei.de>
2023-01-09 11:40:22 +0100

    converts pesig to LIEF

--- refinery/units/formats/pe/pesig.py
+++ refinery/units/formats/pe/pesig.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from struct import unpack
-from pefile import PE, DIRECTORY_ENTRY
+import lief
 
-from refinery.units import Unit, RefineryPartialResult
+from refinery.units import Unit
+from refinery.lib.structures import MemoryFileRO
 
 
 class pesig(Unit):
@@ -12,26 +12,10 @@ class pesig(Unit):
     i.e. the digital signatures in DER format.
     """
 
-    _SECDIRID = DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_SECURITY']
-
     def __init__(self): pass
 
     def process(self, data: bytearray) -> bytearray:
-        pe = PE(data=data, fast_load=True)
-        pe.parse_data_directories(directories=[self._SECDIRID])
-        security = pe.OPTIONAL_HEADER.DATA_DIRECTORY[self._SECDIRID]
-        self.log_info(F'signature offset: 0x{security.VirtualAddress:08X}')
-        self.log_info(F'signature length: 0x{security.Size:08X}')
-        if security.VirtualAddress == 0 or security.Size == 0:
-            raise ValueError(F'IMAGE_DIRECTORY_ENTRY_SECURITY ({self._SECDIRID}) is corrupt.')
-        sgnoff = security.VirtualAddress + 8
-        sgnend = sgnoff + security.Size
-        length, revision, certtype = unpack('<IHH', data[sgnoff - 8:sgnoff])
-        signature = data[sgnoff:sgnend]
-
-        if len(signature) + 8 != length:
-            raise RefineryPartialResult(
-                F'Found {len(signature) + 8} bytes of signature, but length should be {length}.',
-                partial=signature)
-
-        return signature
+        with MemoryFileRO(data) as stream:
+            pe = lief.PE.parse(stream)
+        for signature in pe.signatures:
+            yield bytearray(signature.raw_der)
