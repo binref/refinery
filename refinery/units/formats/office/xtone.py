814dfaee4eeafc8832ff388ba6b6a816594fe444
jesko <rattle@nullteilerfrei.de>
2023-02-14 19:09:37 +0100

    use pyOneNote library over crappy carve method

--- refinery/units/formats/office/xtone.py
+++ refinery/units/formats/office/xtone.py
@@ -3,47 +3,31 @@
 from typing import Optional
 from uuid import UUID
 
-from refinery import Unit
-from refinery.lib.structures import Struct, StructReader
+from refinery.units.formats import PathExtractorUnit, UnpackResult
+from refinery.lib.structures import MemoryFile
+from refinery.lib.mime import get_cached_file_magic_info
 
 
-class FileDataStoreObject(Struct):
-    MAGIC = UUID('e716e3bd-6526-1145-a4c4-8d4d0b7a9eac').bytes
-
-    def __init__(self, reader: StructReader):
-        if reader.read(16) != self.MAGIC:
-            raise ValueError('invalid GUID at the beginning of FileDataStoreObject')
-        self.size = reader.u64()
-        reader.u32()  # reserved
-        reader.u64()  # unused
-        self.data = reader.read(self.size)
-        reader.byte_align(8)
-        if UUID(bytes=bytes(reader.read(16))) != UUID('22a7fb71-790f-0b4a-bb13-899256426b24'):
-            raise ValueError('invalid GUID at the end of FileDataStoreObject')
-        self.end = reader.tell()
-
-
-class xtone(Unit):
+class xtone(PathExtractorUnit):
     """
     Extract embedded files from OneNote documents.
     """
+    @PathExtractorUnit.Requires('pyonenote', optional=False)
+    def _pyOneNote():
+        import pyOneNote
+        import pyOneNote.OneDocument
+        return pyOneNote.OneDocument
 
-    def process(self, data: bytearray):
-        position = 0
-        view = memoryview(data)
-        while True:
-            position = data.find(FileDataStoreObject.MAGIC, position)
-            if position < 0:
-                break
+    def unpack(self, data: bytearray):
+        with MemoryFile(memoryview(data)) as stream:
+            one = self._pyOneNote.OneDocment(stream)
+        for guid, file in one.get_files().items():
+            chunk = file['content']
             try:
-                object = FileDataStoreObject(view[position:])
-            except Exception as error:
-                self.log_warn(F'error parsing structure at offset 0x{position:X}: {error!s}')
-                position += 1
-            else:
-                yield object.data
-            finally:
-                position += object.end
+                extension = file['extension']
+            except KeyError:
+                extension = F'.{get_cached_file_magic_info(chunk).extension}'
+            yield UnpackResult(F'{guid}{extension}', chunk)
 
     @classmethod
     def handles(cls, data: bytearray) -> Optional[bool]:
