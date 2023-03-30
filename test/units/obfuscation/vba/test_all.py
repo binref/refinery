3b04e6e957c03714847deb5fddf5cdd2f8dd5cdb
jesko <rattle@nullteilerfrei.de>
2023-03-30 10:35:39 +0200

    adds vba test

--- test/units/obfuscation/vba/test_all.py
+++ test/units/obfuscation/vba/test_all.py
@@ -9,3 +9,11 @@ class TestVBADeobfuscator(TestUnitBase):
         data = BR'''Execute chr(311-(&HF1))&chr(1112-(&H3E3))&chr(422-(&H138))&chr(1064-(&H3C5))'''
         result = self.load().process(data)
         self.assertEqual(result, b'Execute "Func"')
+
+    def test_real_world_02(self):
+        data = self.download_sample('07e25cb7d427ac047f53b3badceacf6fc5fb395612ded5d3566a09800499cd7d')
+        unit = self.load()
+        self.assertIn(
+            r'POwerShell.exe -noProfilE -ExEcutionPolicy Bypass -Command C:\ProgramData\UPFCRQOFGHVNBVUABXGFIW\UPFCRQOFGHVNBVUABXGFIW.bat',
+            data | unit | str
+        )
