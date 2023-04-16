7546a20712563d14b9de22358a2f38aff1787f77
jesko <rattle@nullteilerfrei.de>
2023-04-16 11:42:40 +0200

    minor requirement spelling correction

--- refinery/units/formats/email.py
+++ refinery/units/formats/email.py
@@ -29,7 +29,7 @@ class xtmail(PathExtractorUnit):
         yield UnpackResult('headers.json',
             lambda jsn=jh: json.dumps(jsn, indent=4).encode(self.codec))
 
-    @PathExtractorUnit.Requires('extract_msg', optional=False)
+    @PathExtractorUnit.Requires('extract-msg', optional=False)
     def _extract_msg():
         import extract_msg.message
         return extract_msg.message
