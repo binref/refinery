74f5b438385c8d865d7cf476e574a470abb8bafe
jesko <rattle@nullteilerfrei.de>
2023-05-04 13:43:04 +0200

    adds reverse to csv unit

--- refinery/units/formats/csv.py
+++ refinery/units/formats/csv.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from typing import List, Dict, Any
+
+import csv as _csv
+import io
+import json
+
 from refinery.units import Unit
 from refinery.lib.structures import MemoryFile
 from refinery.lib.tools import isodate
@@ -16,11 +22,39 @@ class csv(Unit):
     ):
         super().__init__(quote=quote, delim=delim)
 
-    def process(self, data):
-        import csv
-        import io
-        import json
+    def reverse(self, data: bytearray):
+        quote = self.args.quote.decode(self.codec)
+        delim = self.args.delim.decode(self.codec)
 
+        try:
+            table: List[Dict[str, Any]] = json.loads(data)
+        except Exception:
+            table: List[Dict[str, Any]] = [json.loads(line) for line in data.splitlines()]
+
+        if not isinstance(table, list):
+            raise ValueError('Input must be a JSON list.')
+
+        keys = {}
+        # A dictionary is used here over a set because dictionaries remember insertion order.
+        # When feeding the unit a sequence of JSON objects, the user would likely expect the
+        # column order in the resulting CSV to derive from the entry oder in the JSON data.
+
+        for row in table:
+            for key in row:
+                if not isinstance(key, str):
+                    continue
+                keys[key] = None
+
+        keys = list(keys)
+        out = MemoryFile()
+
+        with io.TextIOWrapper(out, self.codec, newline='') as stream:
+            writer = _csv.writer(stream, quotechar=quote, delimiter=delim, skipinitialspace=True)
+            for row in table:
+                writer.writerow([str(row.get(key, '')) for key in keys])
+            return out.getvalue()
+
+    def process(self, data):
         quote = self.args.quote.decode(self.codec)
         delim = self.args.delim.decode(self.codec)
 
@@ -33,7 +67,7 @@ class csv(Unit):
             return field
 
         with io.TextIOWrapper(MemoryFile(data), self.codec) as stream:
-            rows = csv.reader(stream, quotechar=quote, delimiter=delim, skipinitialspace=True)
+            rows = _csv.reader(stream, quotechar=quote, delimiter=delim, skipinitialspace=True)
             keys = next(rows)
             for row in rows:
                 out = {key: convert(value) for key, value in zip(keys, row)}
