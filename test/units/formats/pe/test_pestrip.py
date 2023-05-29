54d23385fc9f2767a4f66d6d1d58723081dcc318
jesko <rattle@nullteilerfrei.de>
2023-05-29 15:36:35 +0200

    fixes resource tests

--- test/units/formats/pe/test_pestrip.py
+++ test/units/formats/pe/test_pestrip.py
@@ -21,12 +21,19 @@ class TestPEStrip(TestUnitBase):
         stripped = bloated | self.load(aggressive=True) | bytearray
         self.assertLessEqual(len(stripped), 5_000_000)
         self.assertEqual(
-            '1d8b1a5830ee16507307978f1f690547b8e161b6a0e6af00b71bd4c77396c8ef',
+            '173c0724027b16fd77df7dfe1c7e2186124c35b1e6e31a3ff22c7954acbe3847',
             stripped | self.ldu('sha256', text=True) | str)
-
-    def test_resource_bloat_02(self):
-        data = self.download_sample('a50a4c0a38520a9f02cf59aa70c930b0491d4d67fe338a317b272d7802b6ecfb')
-        bloated = data | self.ldu('xt') | bytearray
-        self.assertGreaterEqual(len(bloated), 100_000_000)
         stripped = bloated | self.load(resources=True) | bytearray
         self.assertLessEqual(len(stripped), 5_000_000)
+
+    def test_resource_bloat_02(self):
+        data = self.download_sample('49fce89423749a4b0883430a077dd71d52fca0e25acf201588d0c5d186a1d33a')
+        bloated = data | self.ldu('xt') | bytearray
+        self.assertGreaterEqual(len(bloated), 100_000_000)
+        stripped = bloated | self.load(aggressive=True) | bytearray
+        self.assertLessEqual(len(stripped), 10_000_000)
+        self.assertEqual(
+            '42d73123eaa3aede2dbab5f7a6fe5b7f452e70741576364930fb430ef6c477a8',
+            stripped | self.ldu('sha256', text=True) | str)
+        stripped = bloated | self.load(resources=True) | bytearray
+        self.assertLessEqual(len(stripped), 15_000_000)
