0d4039daadf50f800b322e7ca088c01aacd1c3c1
Cindy Xiao <git@cxiao.net>
2023-08-06 00:23:20 -0700

    feat: Get header, load command, linked image information in machometa, handle fat binaries

--- refinery/units/formats/macho/machometa.py
+++ refinery/units/formats/macho/machometa.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from io import BytesIO
-from typing import List
+from typing import Dict, List
 
-from ktool import load_image, Image
+from ktool import load_image, load_macho_file, Image, MachOFileType
 
 from refinery.units import Arg, Unit
 from refinery.units.sinks.ppjson import ppjson
@@ -17,49 +17,94 @@ class machometa(Unit):
     def __init__(
         self, all: Arg('-c', '--custom',
             help='Unless enabled, all default categories will be extracted.') = True,
+        header: Arg('-H', help='Parse basic data from the Mach-O header.') = False,
+        linked_images: Arg('-K', help='Parse all library images linked by the Mach-O.') = False,
+        load_commands: Arg('-D', help='Parse load commands from the Mach-O header.') = False,
         exports: Arg('-E', help='List all exported functions.') = False,
         imports: Arg('-I', help='List all imported functions.') = False,
         tabular: Arg('-t', help='Print information in a table rather than as JSON') = False,
     ):
         super().__init__(
+            header=all or header,
+            linked_images=all or linked_images,
+            load_commands=load_commands,
             imports=imports,
             exports=exports,
             tabular=tabular,
         )
-        
-    def parse_imports(self, macho: Image, data=None) -> List:
+
+    def parse_macho_header(self, macho_image: Image, data=None) -> Dict:
+        info = {}
+        dyld_header = macho_image.macho_header.dyld_header
+        if dyld_header is not None:
+            info = dyld_header.serialize()
+        return info
+
+    def parse_linked_images(self, macho_image: Image, data=None) -> List:
         info = []
-        for imp in macho.imports:
+        linked_images = macho_image.linked_images
+        for linked_image in linked_images:
+            info.append(linked_image.serialize())
+        return info
+
+    def parse_load_commands(self, macho_image: Image, data=None) -> List:
+        info = []
+        load_commands = macho_image.macho_header.load_commands
+        for load_command in load_commands:
+            info.append(load_command.serialize())
+        return info
+
+    def parse_imports(self, macho_image: Image, data=None) -> List:
+        info = []
+        for imp in macho_image.imports:
             info.append(imp.name)
         return info
 
-    def parse_exports(self, macho: Image, data=None) -> List:
+    def parse_exports(self, macho_image: Image, data=None) -> List:
         info = []
-        for exp in macho.exports:
+        for exp in macho_image.exports:
             info.append(exp.name)
         return info
 
     def process(self, data: bytearray):
         result = {}
-        macho = load_image(fp=BytesIO(data))
+        macho = load_macho_file(fp=BytesIO(data), use_mmaped_io=False)
+        if macho.type is MachOFileType.FAT:
+            result['FileType'] = 'FAT'
+        elif macho.type is MachOFileType.THIN:
+            result['FileType'] = 'THIN'
 
-        for switch, resolver, name in [
-            (self.args.imports, self.parse_imports, 'Imports'),
-            (self.args.exports, self.parse_exports, 'Exports'),
-        ]:
-            if not switch:
-                continue
-            self.log_debug(F'parsing: {name}')
-            try:
-                info = resolver(macho, data)
-            except Exception as E:
-                self.log_info(F'failed to obtain {name}: {E!s}')
-                continue
-            if info:
-                result[name] = info
+        result['Slices'] = []
 
-        result['Base Name'] = macho.base_name
-        result['Install Name'] = macho.install_name
+        for macho_slice in macho.slices:
+            slice_result = {}
+            macho_image = load_image(fp=macho_slice)
+
+            for switch, resolver, name in [
+                (self.args.header, self.parse_macho_header, 'Header'),
+                (self.args.linked_images, self.parse_linked_images, 'Linked Images'),
+                (self.args.load_commands, self.parse_load_commands, 'Load Commands'),
+                (self.args.imports, self.parse_imports, 'Imports'),
+                (self.args.exports, self.parse_exports, 'Exports'),
+            ]:
+                if not switch:
+                    continue
+                self.log_debug(F'parsing: {name}')
+                try:
+                    info = resolver(macho_image, data)
+                except Exception as E:
+                    self.log_info(F'failed to obtain {name}: {E!s}')
+                    continue
+                if info:
+                    slice_result[name] = info
+
+            if macho_image.uuid is not None:
+                uuid: bytes = macho_image.uuid
+                slice_result['UUID'] = uuid.hex()
+            slice_result['Base Name'] = macho_image.base_name
+            slice_result['Install Name'] = macho_image.install_name
+
+            result['Slices'].append(slice_result)
 
         if result:
             yield from ppjson(tabular=self.args.tabular)._pretty_output(result, indent=4, ensure_ascii=False)
