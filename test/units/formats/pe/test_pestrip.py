408f79f386102685efe5589708a04d6d363035d3
jesko <rattle@nullteilerfrei.de>
2023-08-13 21:11:56 +0200

    adjust tests

--- test/units/formats/pe/test_pestrip.py
+++ test/units/formats/pe/test_pestrip.py
@@ -9,7 +9,7 @@ class TestPEStrip(TestUnitBase):
         data = self.download_sample('31c353e37946bf459b2a724fbbf2308caf82c96ae48ffcfa798ce5b117f5a8ba')
         bloated = data | self.ldu('xt') | bytearray
         self.assertGreaterEqual(len(bloated), 100_000_000)
-        stripped = bloated | self.load(sections=True) | bytearray
+        stripped = bloated | self.ldu('petrim') | self.load(sections=True) | bytearray
         self.assertLessEqual(len(stripped), 10_000_000)
         check = stripped | self.ldu('vsect', '.?0?') | self.ldu('trim', b'\0') | self.ldu('sha256', text=True) | str
         self.assertEqual(check, 'ec018cb120e9c8af36e794d1205f649f1bc5fc3590f21e22b248201cdbd28cdd')
