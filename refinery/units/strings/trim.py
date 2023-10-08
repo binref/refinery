56a31b283a0266cbc4a1895c9f0edcb6704fa414
jesko <rattle@nullteilerfrei.de>
2023-10-09 00:30:52 +0200

    recovers performance of previous implementation

--- refinery/units/strings/trim.py
+++ refinery/units/strings/trim.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import re
+from typing import Tuple
 
 from refinery.units import Arg, Unit
 
@@ -19,25 +19,69 @@ class trim(Unit):
     ):
         super().__init__(junk=junk, left=left, right=right, unpad=unpad, nocase=nocase)
 
+    def _trimfast(self, view: memoryview, *junks: bytes, right=False) -> Tuple[bool, memoryview]:
+        done = False
+        pos = 0
+        while not done:
+            done = True
+            for junk in junks:
+                temp = junk
+                size = len(junk)
+                done = True
+                if right and self.args.unpad:
+                    for k in range(size):
+                        n = size - k
+                        if view[pos:pos + n] == junk[k:]:
+                            pos += n
+                            done = False
+                            break
+                if view[pos:pos + size] == temp:
+                    m = len(temp)
+                    while True:
+                        mm = m << 1
+                        if view[pos + m:pos + mm] != temp:
+                            break
+                        temp += temp
+                        m = mm
+                    temp = memoryview(temp)
+                    while m >= size:
+                        if view[pos:pos + m] == temp[:m]:
+                            done = False
+                            pos += m
+                        m //= 2
+                if right or not self.args.unpad:
+                    continue
+                while size > 0:
+                    if view[pos:pos + size] == temp[:size]:
+                        done = False
+                        pos += size
+                        break
+                    size -= 1
+        return pos
+
     def process(self, data: bytearray):
-        junks = self.args.junk
-
-        if not junks:
-            pattern = B'\\s*'
+        junk = list(self.args.junk)
+        if not junk:
+            import string
+            space = string.whitespace.encode('ascii')
+            junk = [space[k - 1:k] for k in range(1, len(space))]
+        lpos = 0
+        rpos = 0
+        if self.args.nocase:
+            work = data.lower()
+            junk = [j.lower() for j in junk]
         else:
-            pattern = B'|'.join(re.escape(junk) for junk in junks)
-            pattern = B'(%s)*' % pattern
-            if self.args.unpad:
-                partial = B'|'.join(
-                    B''.join(B'\\x%02X?' % byte for byte in junk) for junk in junks)
-                pattern = B'%s(%s)' % (pattern, partial)
-
-        def left_and_right():
-            if self.args.left:
-                yield B'(^%s)' % pattern
-            if self.args.right:
-                yield B'(%s$)' % pattern
-
-        pattern = B'|'.join(p for p in left_and_right())
-        flags = re.IGNORECASE if self.args.nocase else 0
-        return re.sub(pattern, B'', data, flags=flags)
+            work = data
+        if self.args.left:
+            lpos = self._trimfast(memoryview(work), *junk)
+        if self.args.right:
+            work.reverse()
+            junk = [bytes(reversed(j)) for j in junk]
+            rpos = self._trimfast(memoryview(work), *junk, right=True)
+            work.reverse()
+        view = memoryview(data)
+        if lpos:
+            view = view[+lpos:]
+        if rpos:
+            view = view[:-rpos]
+        return view
