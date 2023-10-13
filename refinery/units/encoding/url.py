4442dffb4c5f33a3518a048cf2548360c9866191
jesko <rattle@nullteilerfrei.de>
2023-10-13 21:07:20 +0200

    backwards compatibility

--- refinery/units/encoding/url.py
+++ refinery/units/encoding/url.py
@@ -35,10 +35,10 @@ class url(Unit):
                 offset += 2
             return result
         elif self.args.plus:
-            def replace(m: re.Match[bytes]):
+            def replace(m):
                 c = m[0][0]
                 return b'+' if c == 0x20 else B'%%%02X' % c
         else:
-            def replace(m: re.Match[bytes]):
+            def replace(m):
                 return B'%%%02X' % m[0][0]
         return re.sub(B'[^a-zA-Z0-9_.-~\\/]', replace, data)
