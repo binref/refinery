007cbf18cd947ba8638ddb35201e0ae57c3e114b
jesko <rattle@nullteilerfrei.de>
2023-11-18 09:19:21 +0100

    fixes test

--- test/units/formats/office/test_xtdoc.py
+++ test/units/formats/office/test_xtdoc.py
@@ -10,15 +10,7 @@ class TestDocExtractor(TestUnitBase):
 
     def test_maldoc(self):
         data = self.download_sample('969ff75448ea54feccc0d5f652e00172af8e1848352e9a5877d705fc97fa0238')
-        pipeline = L('xtdoc', 'WordDoc') | L('push') \
-            [ L('drp')
-            | L('pop', 'junk')
-            | L('repl', 'var:junk')
-            | L('carve', '-ds', 'b64')
-            | L('u16')
-            | L('deob-ps1')
-            | L('repl', 'var:junk', 'http')
-            | L('xtp', 'url')
-            ]
+        pipeline = self.load_pipeline(
+            'xtdoc WordDoc | push [| drp | pop junk | repl var:junk | carve -ds b64 | u16 | deob-ps1 | repl var:junk http | xtp url ]')
         c2s = pipeline(data)
         self.assertIn(B'http://depannage-vehicule-maroc'B'.com/wp-admin/c/', c2s)
