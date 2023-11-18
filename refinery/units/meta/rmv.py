cda192699d01b64135fbe28fc3fb38dbd9952a91
jesko <rattle@nullteilerfrei.de>
2023-11-18 15:26:30 +0100

    fix name and docstring

--- refinery/units/meta/rmv.py
+++ refinery/units/meta/rmv.py
@@ -4,11 +4,11 @@ from refinery.units import Unit, Chunk, Arg
 from refinery.lib.meta import metavars
 
 
-class mvc(Unit):
+class rmv(Unit):
     """
-    Removes meta variables that were created in the current frame. If no variable names are given,
-    the unit removes all meta variables that were created in the current frame. Note that this can
-    recover variables from outer frames that were previously shadowed.
+    Short for "ReMove Variable": Removes meta variables that were created in the current frame. If no
+    variable names are given, the unit removes all of them. Note that this can recover variables from
+    outer frames that were previously shadowed.
     """
     def __init__(self, *names: Arg(type=str, metavar='name', help='Name of a variable to be removed.')):
         super().__init__(names=names)
