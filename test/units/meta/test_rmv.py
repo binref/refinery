33928fe743b0166238dbdb062e762d8718c982f6
jesko <rattle@nullteilerfrei.de>
2023-11-18 15:28:02 +0100

    clean up tests and documentation after rename

--- test/units/meta/test_rmv.py
+++ test/units/meta/test_rmv.py
@@ -4,8 +4,8 @@ from . import TestMetaBase
 from refinery.lib.loader import load_pipeline as L
 
 
-class TestMetaVarClear(TestMetaBase):
+class TestRemoveMetaVar(TestMetaBase):
 
-    def test_mvc_01(self):
-        pl = self.load_pipeline('emit FOO [| put x BAR [| put x BOO | mvc x | cfmt {}{x} ]| cfmt {}{x} ]')
+    def test_rmv_01(self):
+        pl = self.load_pipeline('emit FOO [| put x BAR [| put x BOO | rmv x | cfmt {}{x} ]| cfmt {}{x} ]')
         self.assertEqual(pl(), B'FOO{x}BAR')
