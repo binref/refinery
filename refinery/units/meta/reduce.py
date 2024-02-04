5be8029f458c52de513e0ada1c95ee1b74410c3f
jesko <rattle@nullteilerfrei.de>
2024-02-04 18:33:04 +0100

    ok now it makes sense ... I think

--- refinery/units/meta/reduce.py
+++ refinery/units/meta/reduce.py
@@ -11,33 +11,28 @@ from refinery.lib.loader import load_pipeline
 class reduce(Unit):
     """
     The reduce unit applies an arbitrary pipeline repeatedly to reduce the current frame to a
-    single chunk.
+    single chunk. The first chunk in the frame serves as initialization.
     """
 
     def __init__(self,
         *reduction: Arg(type=str, metavar='pipeline', help=(
-            'The remaining command line is a refinery pipeline. The input for this pipeline '
-            'is the currently accumulated data and the next chunk to be combined is passed in '
-            'a temporary meta variable.'
+            'The remaining command line is a refinery pipeline. While reducing the frame, the '
+            'binary contents of each chunk are placed in a temporary variable, then the chunk '
+            'body is replaced with the accumulated data, and then processed with this pipeline. '
+            'The result is written back to the accumulator and the process repeats.'
         )),
-        init: Arg.Binary('-i',
-            help='Optionally specify the initial buffer. Defaults to the first chunk.') = None,
         just: Arg.Number('-j',
             help='Optionally specify a maximum number of chunks to process beyond the first.') = 0,
         temp: Arg.String('-t', metavar='name',
             help='The name of the temporary variable. The default is "{default}".') = 't',
     ):
-        super().__init__(reduction=reduction, temp=temp, init=init, just=just)
+        super().__init__(reduction=reduction, temp=temp, just=just)
 
     def filter(self, chunks: Iterable[Chunk]):
         it = iter(chunks)
         just = self.args.just
         name = self.args.temp
-        accu = self.args.init
-        if accu is None:
-            accu = next(it)
-        else:
-            accu = self.labelled(accu)
+        accu = next(it)
         init = ' '.join(self.args.reduction)
         unit: Unit = load_pipeline(init)
         meta = dict(accu.meta)
@@ -47,6 +42,9 @@ class reduce(Unit):
             unit.args(accu)
             self.log_info('current input:', accu, clip=True)
             accu[:] = unit.act(accu)
+        temp = [key for key in accu.meta.keys() if key not in meta]
+        for key in temp:
+            accu.meta.discard(key)
         accu.meta.update(meta)
         yield accu
         yield from it
--- test/units/meta/test_reduce.py
+++ test/units/meta/test_reduce.py
@@ -11,7 +11,7 @@ class TestReduce(TestUnitBase):
         self.assertEqual(pl(), B'012345')
 
     def test_variables_are_retained(self):
-        pl = L('emit 5 4 3 2 1 0 [| swap q | reduce --init "" ccp var:q ]')
+        pl = L('emit 5 4 3 2 1 0 [| put q | reduce ccp var:q ]')
         self.assertEqual(pl(), B'012345')
 
     def test_addition(self):
@@ -19,14 +19,15 @@ class TestReduce(TestUnitBase):
         self.assertEqual(pl(), b'10040')
 
     def test_just_parameter(self):
+        def p(n):
+            return n * (n - 1) // 2
         chunk = next(self.load_pipeline(
-            'emit rep[9]:h:01 | chop 1 ['
-            '| put x index'
+            'emit range::9 | chop 1 ['
+            '| put x le:c:'
             '| reduce add var:t -j 2 | pick 0 0: | pop a:le'
             '| reduce add var:t -j 2 | pick 0 0: | pop b:le'
             '| reduce add var:x | put c le:c:: ]'))
-        self.assertEqual(chunk['a'], 3)
-        self.assertEqual(chunk['b'], 5)
-        self.assertEqual(chunk['c'], 31)
-        self.assertEqual(chunk['t'], b'\x01')
+        self.assertEqual(chunk['a'], p(3))
+        self.assertEqual(chunk['b'], p(5))
+        self.assertEqual(chunk['c'], p(9))
         self.assertEqual(chunk['x'], 0)
