6bc4178a812e3e2c6abe3001c62d7b87ed0165ab
jesko <rattle@nullteilerfrei.de>
2024-02-16 09:54:00 +0100

    adds regression test for nonsolid NSIS/LZMA

--- test/units/formats/archive/test_xtnsis.py
+++ test/units/formats/archive/test_xtnsis.py
@@ -13,8 +13,14 @@ class TestNSISExtractor(TestUnitBase):
         result = data | unit | self.ldu('xt7z') | self.ldu('pemeta') | json.loads
         self.assertEqual(result['Signature']['Fingerprint'], '6509312e581ef5ba12be11ed427a66f8fd80e819')
 
-    def test_modified_archive_lzma1(self):
+    def test_modified_archive_lzma_solid(self):
         data = self.download_sample('19ccf1d4389f624fb166c5828c1633ea4234c976e044e5b61e53000f4a098be8')
         unit = self.load('diskpart.vbe')
         result = str(data | unit | self.ldu('recode') | self.ldu('wshenc'))
         self.assertIn(U'Извините, вы не указали размер диска!', result)
+
+    def test_modified_archive_lzma_nonsolid(self):
+        data = self.download_sample('ff4ef0ee0915af58ea1388f72730c63c746856a64760e17e4fcdfc559a8b4555')
+        unit = self.load('csrss.bat')
+        result = str(data | unit | self.ldu('recode', 'cp1251'))
+        self.assertIn(U'КАК РАСШИФРОВАТЬ ФАЙЛЫ.TXT', result)
