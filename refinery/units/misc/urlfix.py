0a7990e7d4f479f74962b03872f66ffdac3c2c35
jesko <rattle@nullteilerfrei.de>
2024-05-08 09:54:15 +0200

    adds option to extract query string parameters

--- refinery/units/misc/urlfix.py
+++ refinery/units/misc/urlfix.py
@@ -4,7 +4,6 @@ from typing import Optional
 from urllib.parse import urlparse, urlunparse, parse_qsl, unquote, quote
 
 from refinery.units import Arg, Unit
-from refinery.lib.decorators import unicoded
 
 
 class urlfix(Unit):
@@ -16,32 +15,38 @@ class urlfix(Unit):
     """
     def __init__(
         self,
-        keep: Arg('-k', action='count', help=(
+        meta: Arg.Switch('-m', help='Extract the query string parameters as metadata.') = False,
+        keep: Arg.Counts('-k', help=(
             'If specified once, keeps the it keeps the URL params and query string. If specified '
             'twice, it keeps the URL fragment as well. At this level, the unit still filters out '
             'anything that does not parse as a URL.'
         )) = 0
     ):
-        super().__init__(keep=keep)
+        super().__init__(keep=keep, meta=meta)
 
-    @unicoded
-    def process(self, data: str) -> Optional[str]:
+    def process(self, data):
         def fix(string):
             return quote(unquote(string))
         keep = self.args.keep
-        parsed = urlparse(data)
+        meta = self.args.meta
+        parsed = urlparse(data.decode(self.codec))
         if not parsed.scheme or not parsed.netloc:
             return None
-        new_query = '&'.join(F'{key}={fix(value)}' for key, value in parse_qsl(parsed.query))
+        query_dict = {key: unquote(value) for key, value in parse_qsl(parsed.query)}
+        query_string = '&'.join(F'{key}={quote(value)}' for key, value in query_dict.items())
         replacements = dict(
             netloc=parsed.netloc.lower(),
             params=fix(parsed.params),
             path=fix(parsed.path),
-            query=new_query,
+            query=query_string,
             fragment=fix(parsed.fragment),
         )
         if keep < 2:
             replacements.update(fragment='')
             if keep < 1:
                 replacements.update(params='', query='')
-        return urlunparse(parsed._replace(**replacements))
+        url = urlunparse(parsed._replace(**replacements))
+        url = url.encode(self.codec)
+        if meta:
+            url = self.labelled(url, **query_dict)
+        return url
