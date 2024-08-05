a52ae5c66a99a078cc9fa4aee1e2b21ecab61362
jesko <rattle@nullteilerfrei.de>
2024-08-05 21:10:00 +0200

    attempt to update pypcap

--- refinery/units/formats/pcap.py
+++ refinery/units/formats/pcap.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf - 8 -* -
-from typing import NamedTuple, Union, TYPE_CHECKING
+from typing import NamedTuple, List, Union, TYPE_CHECKING
 
 from refinery.units import Arg, Unit
 from refinery.lib.vfs import VirtualFileSystem, VirtualFile
@@ -11,6 +11,7 @@ import logging
 
 if TYPE_CHECKING:
     from ipaddress import IPv4Address, IPv6Address
+    from pcapkit.foundation.reassembly.data.tcp import Datagram
     TIPAddr = Union[IPv4Address, IPv6Address]
 
 
@@ -67,18 +68,11 @@ class pcap(Unit):
     def __init__(self, merge: Arg.Switch('-m', help='Merge both parts of each TCP conversation into one chunk.') = False):
         super().__init__(merge=merge)
 
-    @Unit.Requires('pypcapkit[scapy]<0.16.0', 'all')
+    @Unit.Requires('pypcapkit[scapy]>=1.3', 'all')
     def _pcapkit():
+        import scapy.layers.tls.session # noqa
         import pcapkit
         return pcapkit
-
-    @Unit.Requires('dictdumper==0.8.4', 'all')
-    def _pcapkit_python37_bugfix():
-        """
-        TODO: This artificial requirement pins a dependency of pypcapkit that otherwise breaks for
-        Python 3.7; see issue [#51](https://github.com/binref/refinery/issues/51). Once that issue
-        is resolved, this should be removed.
-        """
 
     def process(self, data):
         with NoLogging():
@@ -87,19 +81,19 @@ class pcap(Unit):
 
         merge = self.args.merge
 
-        with VirtualFileSystem() as fs:
+        with NoLogging(), VirtualFileSystem() as fs:
             vf = VirtualFile(fs, data, 'pcap')
             extraction = pcapkit.extract(
-                fin=vf.path, engine='scapy', store=False, nofile=True, extension=False, tcp=True, strict=True)
-            tcp: list = list(extraction.reassembly.tcp)
+                fin=vf.path, engine='scapy', store=False, nofile=True, extension=False, tcp=True, reassembly=True, reasm_strict=True)
+            tcp: List[Datagram] = list(extraction.reassembly.tcp)
 
         tcp.sort(key=lambda p: min(p.index, default=0))
 
         count, convo = 0, None
         src_buffer = MemoryFile()
         dst_buffer = MemoryFile()
-        for stream in tcp:
-            this_convo = Conversation.FromID(stream.id)
+        for datagram in tcp:
+            this_convo = Conversation.FromID(datagram.id)
             if this_convo != convo:
                 if count and merge:
                     if src_buffer.tell():
@@ -110,12 +104,12 @@ class pcap(Unit):
                         dst_buffer.truncate(0)
                 count = count + 1
                 convo = this_convo
-            for packet in stream.packets:
+            for data in datagram.packet:
                 if not merge:
-                    yield self.labelled(packet.data, **this_convo.src_to_dst(), stream=count)
+                    yield self.labelled(data, **this_convo.src_to_dst(), stream=count)
                 elif this_convo.src == convo.src:
-                    src_buffer.write(packet.data)
+                    src_buffer.write(data)
                 elif this_convo.dst == convo.src:
-                    dst_buffer.write(packet.data)
+                    dst_buffer.write(data)
                 else:
                     raise RuntimeError(F'direction of packet {convo!s} in conversation {count} is unknown')
