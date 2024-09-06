e577c0412625d6c28508f3a228607715aaf941a4
jesko <rattle@nullteilerfrei.de>
2024-09-06 10:22:23 +0200

    changes reduce to work more like loop

--- refinery/units/meta/reduce.py
+++ refinery/units/meta/reduce.py
@@ -2,54 +2,44 @@
 # -*- coding: utf-8 -*-
 from typing import Iterable
 
-from itertools import islice
-
 from refinery.units import Arg, Unit, Chunk
-from refinery.lib.loader import load_pipeline
+from refinery.lib.argformats import DelayedBinaryArgument
 
 
 class reduce(Unit):
     """
-    The reduce unit applies an arbitrary pipeline repeatedly to reduce the current frame to a
+    The reduce unit applies an arbitrary multibin suffix repeatedly to reduce a complete frame to a
     single chunk. The first chunk in the frame serves as initialization.
     """
 
     def __init__(self,
-        *reduction: Arg(type=str, metavar='pipeline', help=(
-            'The remaining command line is a refinery pipeline. While reducing the frame, the '
-            'binary contents of each chunk are placed in a temporary variable, then the chunk '
-            'body is replaced with the accumulated data, and then processed with this pipeline. '
-            'The result is written back to the accumulator and the process repeats.'
+        suffix: Arg(type=str, help=(
+            'The remaining command line is a multibin suffix. The reduction accumulator is initialized '
+            'with the first chunk in the frame. Then, each remaining chunk is processed with the given '
+            'suffix and the result is used to overwrite the accumulator.'
         )),
         just: Arg.Number('-j',
             help='Optionally specify a maximum number of chunks to process beyond the first.') = 0,
         temp: Arg.String('-t', metavar='name',
-            help='The name of the temporary variable. The default is "{default}".') = 't',
+            help='The name of the accumulator variable. The default is "{default}".') = 't',
     ):
-        super().__init__(reduction=reduction, temp=temp, just=just)
+        super().__init__(suffix=suffix, temp=temp, just=just)
 
     def filter(self, chunks: Iterable[Chunk]):
         it = iter(chunks)
         just = self.args.just
         name = self.args.temp
         accu = next(it)
-        init = ' '.join(self.args.reduction)
-        unit: Unit = load_pipeline(init)
-        meta = dict(accu.meta)
-        for chunk in islice(it, 0, just) if just else it:
-            accu.meta.update(chunk.meta)
-            accu[name] = chunk
-            unit.args(accu)
-            self.log_info('current input:', accu, clip=True)
-            pos = 0
-            for chunk in unit.act(accu):
-                end = pos + len(chunk)
-                accu[pos:end] = chunk
-                pos = end
-            del accu[end:]
-        temp = [key for key in accu.meta.keys() if key not in meta]
-        for key in temp:
-            accu.meta.discard(key)
-        accu.meta.update(meta)
+        if not just:
+            scope = it
+        else:
+            import itertools
+            self.log_info(F'reducing only the next {just} chunks')
+            scope = itertools.islice(it, 0, just)
+        for chunk in scope:
+            chunk.meta[name] = accu
+            accu[:] = DelayedBinaryArgument(self.args.suffix, reverse=True, seed=chunk)(chunk)
+            self.log_debug('reduced:', accu, clip=True)
+        accu.meta.discard(name)
         yield accu
         yield from it
--- test/units/meta/test_reduce.py
+++ test/units/meta/test_reduce.py
@@ -7,15 +7,15 @@ from .. import TestUnitBase
 class TestReduce(TestUnitBase):
 
     def test_concatenation(self):
-        pl = L('emit 5 4 3 2 1 0 [| reduce ccp var:t ]')
+        pl = L('emit 5 4 3 2 1 0 [| reduce cca[var:t] ]')
         self.assertEqual(pl(), B'012345')
 
     def test_variables_are_retained(self):
-        pl = L('emit 5 4 3 2 1 0 [| put q | reduce ccp var:q ]')
+        pl = L('emit 5 4 3 2 1 0 [| put q | reduce cca[var:q] ]')
         self.assertEqual(pl(), B'012345')
 
     def test_addition(self):
-        pl = L('emit 12 20 8 10000 [| pack -B4 | reduce add t | pack -RB4 ]')
+        pl = L('emit 12 20 8 10000 [| pack -B4 | reduce add[t] | pack -RB4 ]')
         self.assertEqual(pl(), b'10040')
 
     def test_just_parameter(self):
@@ -24,9 +24,9 @@ class TestReduce(TestUnitBase):
         chunk = next(self.load_pipeline(
             'emit range::9 | chop 1 ['
             '| put x le:c:'
-            '| reduce add var:t -j 2 | pick 0 0: | pop a:le'
-            '| reduce add var:t -j 2 | pick 0 0: | pop b:le'
-            '| reduce add var:x | put c le:c:: ]'))
+            '| reduce add[t] -j 2 | pick 0 0: | pop a:le'
+            '| reduce add[t] -j 2 | pick 0 0: | pop b:le'
+            '| reduce add[t] | put c le:c:: ]'))
         self.assertEqual(chunk['a'], p(3))
         self.assertEqual(chunk['b'], p(5))
         self.assertEqual(chunk['c'], p(9))
