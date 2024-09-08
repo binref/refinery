91f6fd08bf33d98991e4611afaf5252db3417d0e
jesko <rattle@nullteilerfrei.de>
2024-09-08 17:13:12 +0200

    implements #57; regex-based early-abort for loop

--- refinery/units/meta/loop.py
+++ refinery/units/meta/loop.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from refinery.units import Arg, Unit
-from refinery.lib.argformats import DelayedBinaryArgument
+from refinery.units.pattern import Arg, RegexUnit
+from refinery.lib.argformats import regexp, DelayedBinaryArgument
+from refinery.lib.tools import exception_to_string
 
 
-class loop(Unit):
+class loop(RegexUnit):
     """
     Applies a given multibin suffix to the input chunk repeatedly. For example, the following
     command would carve the largest base64-encoded buffer from the input, decode it, and then
@@ -13,16 +14,61 @@ class loop(Unit):
         emit data | loop 20 csd[b64]:zl
 
     Notably, the argument after the count is a suffix, which means that handlers are applied
-    from left to right (not from right to left).
+    from left to right (not from right to left). The loop is aborted and the previous result
+    returned if an error occurs while evaluating the suffix, or when the output is empty.
     """
 
     def __init__(
-        self, count: Arg.Number(metavar='count', help='The number of repeated applications of the suffix.'),
-        suffix: Arg(type=str, help='A multibin expression suffix.')
+        self,
+        count: Arg.Number(metavar='count', help='The number of repeated applications of the suffix.'),
+        suffix: Arg(type=str, help='A multibin expression suffix.'),
+        whiLe: Arg('-w', '--while', type=regexp, metavar='RE',
+            help='Halt when the given regular expression does not match the data.'),
+        until: Arg('-u', '--until', type=regexp, metavar='RE',
+            help='Halt when the given regular expression matches the data.'),
+        fullmatch=False, multiline=False, ignorecase=False,
     ):
-        super().__init__(count=count, suffix=suffix)
+        super().__init__(
+            count=count,
+            suffix=suffix,
+            whiLe=whiLe,
+            until=until,
+            fullmatch=fullmatch,
+            multiline=multiline,
+            ignorecase=ignorecase,
+        )
 
     def process(self, data):
-        for _ in range(self.args.count):
-            data[:] = DelayedBinaryArgument(self.args.suffix, reverse=True, seed=data)(data)
+        _count = self.args.count
+        _width = len(str(_count))
+        _while = self._while
+        _until = self._until
+
+        for k in range(_count):
+            if _while and not _while(data):
+                self.log_info(F'step {k:0{_width}}: stopping, while-condition violated')
+                break
+            if _until and _until(data):
+                self.log_info(F'step {k:0{_width}}: stopping, until-condition satisfied')
+                break
+            try:
+                out = DelayedBinaryArgument(
+                    self.args.suffix, reverse=True, seed=data)(data)
+            except Exception as error:
+                msg = exception_to_string(error)
+                self.log_info(F'step {k:0{_width}}: stopping after error; {msg}')
+                break
+            if not out:
+                self.log_info(F'step {k:0{_width}}: stopping after empty result')
+                break
+            data[:] = out
+
         return data
+
+    @property
+    def _while(self):
+        return self._make_matcher(self.args.whiLe)
+
+    @property
+    def _until(self):
+        return self._make_matcher(self.args.until)
