355dcbd1a07b8ef97c6e7957b32951bb29b4d5be
jesko <rattle@nullteilerfrei.de>
2024-10-07 14:12:17 +0200

    separates a3x tests

--- test/units/formats/test_a3x.py
+++ test/units/formats/test_a3x.py
@@ -5,16 +5,14 @@ from .. import TestUnitBase
 
 class TestA3X(TestUnitBase):
 
-    def test_real_world_01(self):
-        sample_1 = self.download_sample(
+    def test_ea06(self):
+        sample = self.download_sample(
             '3b775e678568cd3d55187443a3f7aae2116b7e9762b3c3879f5e1c6225434b25')
-        sample_2 = self.download_sample(
-            '9b66a8ea0f1c64965b06e7a45afbe56f2d4e6d5ef65f32446defccbebe730813')
-        taint = sample_1.replace(
+        taint = sample.replace(
             b'\xA3\x48\x4B\xBE\x98\x6C\x4A\xA9\x99\x4C\x53\x0A\x86\xD6\x48\x7D',
             b'\xDE\xFA\xCE\xD0\xDE\xFA\xCE\xD0\xDE\xFA\xCE\xD0\xDE\xFA\xCE\xD0'
         )
-        for data in [taint, sample_1]:
+        for data in [taint, sample]:
             out = data | self.load() | {'path': [...]}
             self.assertEqual(len(out), 4)
             for key, value in out.items():
@@ -26,7 +24,10 @@ class TestA3X(TestUnitBase):
             self.assertContains(out['script.au3'], Br'FileInstall("DrWatson.exe", @SYSTEMDIR & "\1096\DrWatson.exe")')
             self.assertContains(out['script.au3'], b'jan1_milan'b'@yahoo'b'.com')
 
-        out = sample_2 | self.load() | {'path': [...]}
+    def test_ea05(self):
+        sample = self.download_sample(
+            '9b66a8ea0f1c64965b06e7a45afbe56f2d4e6d5ef65f32446defccbebe730813')
+        out = sample | self.load() | {'path': [...]}
         self.assertEqual(len(out), 1)
         for key, value in out.items():
             self.assertEqual(len(value), 1)
