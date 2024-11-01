352d871234332b008a61fd6297057185bb881632
Lukas Kuzmiak <lukash@backstep.net>
2024-11-01 16:43:28 -0700

    Add support for Base64 (b62 unit)

---
+++ refinery/units/encoding/b62.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from refinery.units.encoding.base import base
+
+
+class b62(base):
+    """
+    Base62 encoding and decoding. 
+    """
+    def __init__(self):
+        super().__init__(b'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
