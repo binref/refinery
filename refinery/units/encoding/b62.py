d6a8b361e9a07eb4b23093f4090fb49f6174298c
Lukas Kuzmiak <lukash@backstep.net>
2024-11-02 22:22:31 -0700

    remove trailing whites space

--- refinery/units/encoding/b62.py
+++ refinery/units/encoding/b62.py
@@ -5,7 +5,7 @@ from refinery.units.encoding.base import base
 
 class b62(base):
     """
-    Base62 encoding and decoding. 
+    Base62 encoding and decoding.
     """
     def __init__(self):
         super().__init__(b'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz')
