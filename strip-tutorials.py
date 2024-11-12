0bb905867656f7c2664d95a073294c60b1d6e994
jesko <rattle@nullteilerfrei.de>
2024-11-12 20:54:40 +0100

    GitHub requires execution count, let's normalize it to 1

--- strip-tutorials.py
+++ strip-tutorials.py
@@ -6,10 +6,13 @@ Strips the Jupyter Notebooks in the Tutorial section of run count information.
 import json
 import pathlib
 
+CK = 'execution_count'
+
 for path in pathlib.Path.cwd().glob('./tutorials/*.ipynb'):
     with path.open('r') as fd:
         notebook = json.load(fd)
     for cell in notebook['cells']:
-        cell.pop('execution_count', None)
+        if CK in cell:
+            cell[CK] = 1
     with path.open('w') as fd:
         json.dump(notebook, fd, indent=1)
