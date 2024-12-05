42a1c5f02f2f56a4c3f3774c23a7d36e3d6c26ca
jesko <rattle@nullteilerfrei.de>
2024-12-05 19:02:02 +0100

    switch vmemref to smda

--- refinery/units/formats/exe/vmemref.py
+++ refinery/units/formats/exe/vmemref.py
@@ -4,16 +4,12 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Container
 
 from refinery.units import Arg, Unit
-from refinery.lib.executable import Executable, ET, CompartmentNotFound
-from refinery.lib.structures import MemoryFile
+from refinery.lib.executable import Range, Executable, CompartmentNotFound
 from refinery.lib.tools import NoLogging
 
 
 if TYPE_CHECKING:
-    from angr.project import Project
-    from angr.analyses.cfg.cfg_emulated import CFGEmulated
-    from angr.knowledge_plugins.functions.function import Function
-    from cle.memory import Clemory
+    from smda.common.SmdaFunction import SmdaFunction
 
 
 class vmemref(Unit):
@@ -24,54 +20,53 @@ class vmemref(Unit):
     at the given offset.
     """
 
-    @Unit.Requires('angr', 'all')
-    def _angr():
-        import angr
-        import angr.project
-        import angr.engines
-        return angr
+    @Unit.Requires('smda', 'all')
+    def _smda():
+        import smda
+        import smda.Disassembler
+        import smda.DisassemblyResult
+        return smda
 
     def _memory_references(
         self,
-        function: Function,
-        memory: Clemory,
-        functions: Container[int],
-        pointer_size: int,
+        exe: Executable,
+        function: SmdaFunction,
+        codes: Container[Range],
         max_dereference: int = 1
     ):
-        pointer_size //= 8
-        references = []
-        code = set()
-        for block in function.blocks:
-            code.update(block.instruction_addrs)
-        try:
-            constants = function.code_constants
-        except Exception:
-            pass
-        else:
-            def is_valid_data_address(address):
-                if not isinstance(address, int):
-                    return False
-                if address not in memory:
-                    return False
+        def is_valid_data_address(address):
+            if not isinstance(address, int):
+                return False
+            if address not in exe:
+                return False
+            if address in instructions:
+                return False
+            for code in codes:
                 if address in code:
                     return False
-                if address in functions:
-                    return False
-                return True
+            return True
 
-            def dereference(address):
-                data = bytes(memory[k] for k in range(address, address + pointer_size))
-                return int.from_bytes(data, 'little')
+        def dereference(address):
+            return int.from_bytes(exe[address:address + pointer_size], exe.byte_order().value)
 
-            for address in constants:
+        pointer_size = exe.pointer_size // 8
+        instructions = {op.offset for op in function.getInstructions()}
+
+        references = set()
+
+        for op in function.getInstructions():
+            try:
+                refs = list(op.getDataRefs())
+            except Exception:
+                continue
+            for address in refs:
                 try:
                     address = int(address)
                 except Exception:
                     continue
                 times_dereferenced = 0
                 while is_valid_data_address(address) and address not in references:
-                    references.append(address)
+                    references.add(address)
                     times_dereferenced += 1
                     if max_dereference and max_dereference > 0 and times_dereferenced > max_dereference:
                         break
@@ -84,43 +79,74 @@ class vmemref(Unit):
 
     def __init__(
         self,
-        address: Arg.Number(metavar='ADDR', help='Specify the address of a function to scan.'),
-        base: Arg.Number('-b', metavar='ADDR', help='Optionally specify a custom base address B.') = None,
+        *address: Arg.Number(metavar='ADDR', help=(
+            'Specify the address of a function to scan. If no argument is given, the unit will scan'
+            ' all functions for memory references.')),
+        take: Arg.Number('-t', metavar='SIZE', help=(
+            'Optionally specify the number of bytes to read from each reference; by default, all '
+            'data until the end of the section is returned.')) = None,
+        base: Arg.Number('-b', metavar='ADDR',
+            help='Optionally specify a custom base address B.') = None,
     ):
-        super().__init__(address=address, base=base)
+        super().__init__(address=address, take=take, base=base)
 
     def process(self, data):
-        address = self.args.address
-        executable = Executable.Load(data, self.args.base)
-        code = executable.location_from_address(address).virtual.box
+        smda = self._smda
+        take = self.args.take
+        exe = Executable.Load(data, self.args.base)
+        fmt = exe.pointer_size // 4
+        addresses = self.args.address
 
-        self.log_info(R'loading project into angr')
+        self.log_info(R'disassembling and exploring call graph using smda')
         with NoLogging():
-            project: Project = self._angr.Project(MemoryFile(data), load_options={'auto_load_libs': False})
+            cfg = smda.Disassembler.SmdaConfig()
+            cfg.CALCULATE_SCC = False
+            cfg.CALCULATE_NESTING = False
+            cfg.TIMEOUT = 600
+            dsm = smda.Disassembler.Disassembler(cfg)
+            _input = data
+            if not isinstance(_input, bytes):
+                _input = bytes(data)
+            graph = dsm.disassembleUnmappedBuffer(_input)
 
-        self.log_info(F'scanning function at 0x{address:X}')
-        with NoLogging():
-            cfg: CFGEmulated = project.analyses.CFGEmulated(
-                call_depth=0,
-                starts=[address],
-                enable_symbolic_back_traversal=True,
-                address_whitelist=code.range(),
-            )
+        self.log_info('collecting code addresses for memory reference exclusion list')
+        visits = set()
+        avoid = set()
 
-        function = cfg.functions[address]
-        code_addresses = cfg.functions
+        for symbol in exe.symbols():
+            if not symbol.code:
+                continue
+            avoid.add(exe.location_from_address(symbol.address).virtual.box)
 
-        if executable.type is ET.PE:
-            code_addresses = code
+        if addresses:
+            reset = visits.clear
+        else:
+            def reset(): pass
+            self.log_info('scanning executable for functions')
+            with NoLogging():
+                addresses = [pfn.offset for pfn in graph.getFunctions()]
+                addresses.sort()
 
-        self.log_info(R'extracting memory references from lifted function')
-        for ref in self._memory_references(
-            function,
-            project.loader.memory,
-            code_addresses,
-            executable.pointer_size
-        ):
-            try:
-                yield executable[ref:]
-            except CompartmentNotFound:
-                self.log_info(F'memory reference could not be resolved: 0x{ref:0{executable.pointer_size // 4}X}')
+        for a in addresses:
+            reset()
+            address, function = min(graph.xcfg.items(), key=lambda t: (t[0] >= a, abs(t[0] - a)))
+            self.log_debug(F'scanning function: 0x{address:0{fmt}X}')
+            refs = list(self._memory_references(exe, function, avoid))
+            refs.sort(reverse=True)
+            last_start = None
+            for ref in refs:
+                if ref in visits:
+                    continue
+                visits.add(ref)
+                try:
+                    box = exe.location_from_address(ref)
+                    end = box.physical.box.upper
+                    if take is not None:
+                        end = min(ref + take, end)
+                    if last_start is not None:
+                        end = min(last_start, end)
+                    last_start = box.physical.position
+                except CompartmentNotFound:
+                    self.log_info(F'memory reference could not be resolved: 0x{ref:0{fmt}X}')
+                else:
+                    yield exe.data[last_start:end]
