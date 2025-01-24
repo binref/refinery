0d0754748f7e47ff9a418852e54c952709addb6a
jesko <rattle@nullteilerfrei.de>
2025-01-24 15:16:32 +0100

    removes superfluous intermediate class

--- refinery/units/crypto/hash/maru.py
+++ refinery/units/crypto/hash/maru.py
@@ -4,14 +4,12 @@ from refinery.lib.maru import maru32digest
 from refinery.units.crypto.hash import HashUnit, Arg
 
 
-class MaruHash(HashUnit, abstract=True):
-    def __init__(self, seed: Arg.Number(help='optional seed value') = 0, text=False):
-        super().__init__(seed=seed, text=text)
-
-
-class maru(MaruHash):
+class maru(HashUnit):
     """
     Returns the 64bit maru hash of the input data.
     """
+    def __init__(self, seed: Arg.Number(help='optional seed value') = 0, text=False):
+        super().__init__(seed=seed, text=text)
+
     def _algorithm(self, data: bytes) -> bytes:
         return maru32digest(data, self.args.seed)
