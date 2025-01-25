1d2455c082d0b978d19093ede761b147b5e17611
jesko <rattle@nullteilerfrei.de>
2025-01-25 08:38:35 +0100

    adjust tests

--- test/units/formats/java/test_jvdasm.py
+++ test/units/formats/java/test_jvdasm.py
@@ -6,10 +6,12 @@ from ... import TestUnitBase
 class TestJavaDisassembler(TestUnitBase):
 
     def test_keylogger_sample(self):
-        unit = self.load(join=True)
+        unit = self.load(join=True, gray=True)
         data = self.download_sample('31055a528f9a139104a0ce8f4da6b4b89a37a800715292ae7f8f190b2a7b6582')
         dasm = unit(data)
 
-        self.assertIn(B'lookupswitch    defaultjmp => 0x00000054', dasm)
-        self.assertIn(B'tableswitch     defaultjmp => 0x00000058', dasm)
+        self.assertIn(B'lookupswitch    ___default => 0x0000095e', dasm)
+        self.assertIn(B'lookupswitch    ___default => 0x000002b2', dasm)
+        self.assertIn(B'tableswitch     ___default => 0x00000049', dasm)
+        self.assertIn(B'00000049: 2b', dasm)
         self.assertIn(B'invokestatic    org.jnativehook.keyboard.NativeKeyEvent::getKeyText', dasm)
