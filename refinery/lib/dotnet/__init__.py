2dff3c1c06444ef512bb39f4a516b94c170493b8
jesko <rattle@nullteilerfrei.de>
2025-02-13 12:45:20 +0100

    adds LDC int parser

--- refinery/lib/dotnet/__init__.py
+++ refinery/lib/dotnet/__init__.py
@@ -3,3 +3,14 @@
 """
 A library to parse .NET headers and metadata.
 """
+
+
+def integer_from_ldc(ins: bytes):
+    """
+    This function parses an integer value from the bytes representing an LDC instruction.
+    """
+    if len(ins) == 1:
+        return ins[0] - 0x16
+    if ins[0] == 0x1F:
+        return ins[1]
+    return int.from_bytes(ins[1:], 'little')
