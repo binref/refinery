60f33c5a85421dcd4ddaf1813c05bf9d289f03d6
jesko <rattle@nullteilerfrei.de>
2025-02-13 10:32:14 +0100

    adds caching to type guesser for performance

--- refinery/units/formats/pe/dotnet/dnfields.py
+++ refinery/units/formats/pe/dotnet/dnfields.py
@@ -24,49 +24,58 @@ class dnfields(PathExtractorUnit):
     the data and additional heuristics are used to guess the size of the data
     type.
     """
-    _SIZEMAP = {
-        '^s?byte$'       : 1,
-        '^s?char$'       : 2,
-        '^[us]?int.?16$' : 2,
-        '^[us]?int.?32$' : 4,
-        '^[us]?int.?64$' : 8,
-    }
-
-    def _guess_field_info(self, tables, data, t) -> FieldInfo:
-        pattern = (
-            BR'(\x20....|\x1F.)'                # ldc.i4  count
-            BR'\x8D(...)([\x01\x02])'           # newarr  col|row
-            BR'\x25'                            # dup
-            BR'\xD0\x%02x\x%02x\x%02x\x04'      # ldtoken t
-            BR'(?:.{0,12}?'                      # ...
-            BR'\x80(...)\x04)?' % (             # stsfld variable
-                (t >> 0x00) & 0xFF,
-                (t >> 0x08) & 0xFF,
-                (t >> 0x10) & 0xFF
-            )
-        )
-        for match in re.finditer(pattern, data, flags=re.DOTALL):
-            count, j, r, name = match.groups()
-            count, j, r = struct.unpack('<LLB', B'%s%s\0%s' % (count[1:].ljust(4, B'\0'), j, r))
-            if name:
-                try:
-                    name = struct.unpack('<L', B'%s\0' % name)
-                    name = name[0]
-                    name = tables[4][name - 1].Name
-                except Exception as E:
-                    self.log_info(F'attempt to parse field name failed: {E!s}')
-                    name = None
-            element = tables[r][j - 1]
-            for pattern, size in self._SIZEMAP.items():
-                if re.match(pattern, element.TypeName, flags=re.IGNORECASE):
-                    return FieldInfo(element.TypeName, count, size, name)
-
     def unpack(self, data):
         header = DotNetHeader(data, parse_resources=False)
         tables = header.meta.Streams.Tables
         fields = tables.FieldRVA
+
         if not fields:
             return
+
+        icache = {}
+
+        def _guess_field_info(t: int, signature: bytes, sizemap: dict = {
+            '^s?byte$'       : 1,
+            '^s?char$'       : 2,
+            '^[us]?int.?16$' : 2,
+            '^[us]?int.?32$' : 4,
+            '^[us]?int.?64$' : 8,
+        }) -> FieldInfo:
+            try:
+                return icache[signature]
+            except KeyError:
+                pass
+
+            pattern = (
+                BR'(\x20....|\x1F.)'                # ldc.i4  count
+                BR'\x8D(...)([\x01\x02])'           # newarr  col|row
+                BR'\x25'                            # dup
+                BR'\xD0\x%02x\x%02x\x%02x\x04'      # ldtoken t
+                BR'(?:.{0,12}?'                     # ...
+                BR'\x80(...)\x04)?' % (             # stsfld variable
+                    (t >> 0x00) & 0xFF,
+                    (t >> 0x08) & 0xFF,
+                    (t >> 0x10) & 0xFF
+                )
+            )
+            for match in re.finditer(pattern, data, flags=re.DOTALL):
+                count, j, r, name = match.groups()
+                count, j, r = struct.unpack('<LLB', B'%s%s\0%s' % (count[1:].ljust(4, B'\0'), j, r))
+                element = tables[r][j - 1]
+                for pattern, size in sizemap.items():
+                    if not re.match(pattern, element.TypeName, flags=re.IGNORECASE):
+                        continue
+                    if name:
+                        try:
+                            name = struct.unpack('<L', B'%s\0' % name)
+                            name = name[0]
+                            name = tables[4][name - 1].Name
+                        except Exception as E:
+                            self.log_info(F'attempt to parse field name failed: {E!s}')
+                            name = None
+                    i = icache[signature] = FieldInfo(element.TypeName, count, size, name)
+                    return i
+
         iwidth = len(str(len(fields)))
         rwidth = max(len(F'{field.RVA:X}') for field in fields)
         rwidth = max(rwidth, 4)
@@ -78,7 +87,8 @@ class dnfields(PathExtractorUnit):
             remaining_field_indices.discard(_index - 1)
             fname = field.Name
             ftype = None
-            if len(field.Signature) == 2:
+            signature: bytes = field.Signature
+            if len(signature) == 2:
                 # Crude signature parser for non-array case. Reference:
                 # https://www.codeproject.com/Articles/42649/NET-File-Format-Signatures-Under-the-Hood-Part-1
                 # https://www.codeproject.com/Articles/42655/NET-file-format-Signatures-under-the-hood-Part-2
@@ -94,9 +104,9 @@ class dnfields(PathExtractorUnit):
                     0x0B: FieldInfo('UInt64', 1, 8, None),  # noqa
                     0x0C: FieldInfo('Single', 1, 4, None),  # noqa
                     0x0D: FieldInfo('Double', 1, 8, None),  # noqa
-                }.get(field.Signature[1], None)
+                }.get(signature[1], None)
             else:
-                guess = self._guess_field_info(tables, data, _index)
+                guess = _guess_field_info(_index, signature)
             if guess is None:
                 self.log_debug(lambda: F'field {k:0{iwidth}d} with signature {field.Signature.hex()}: unable to guess type information')
                 continue
