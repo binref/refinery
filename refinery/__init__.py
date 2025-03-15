9389313f751cfd520342e5a6f784dd54f6a34605
jesko <rattle@nullteilerfrei.de>
2025-03-15 08:21:00 +0100

    bugfix release

--- refinery/__init__.py
+++ refinery/__init__.py
@@ -34,7 +34,7 @@ various `refinery.units.Unit`s can be combined.
 4. `refinery.units`: writing custom units, add command-line arguments, and how to use refinery
    units within Python code.
 """
-__version__ = '0.8.10'
+__version__ = '0.8.11'
 __distribution__ = 'binary-refinery'
 
 from typing import Dict, List, Optional, Type, TypeVar, Iterable
