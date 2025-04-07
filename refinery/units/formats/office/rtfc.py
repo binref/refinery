4a02aa22ce5882fca355bfbcd27cdb93acdf64a5
jesko <rattle@nullteilerfrei.de>
2025-04-07 13:04:39 +0200

    adds rtfc unit

---
+++ refinery/units/formats/office/rtfc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from refinery.units import Unit
+
+
+class rtfc(Unit):
+    """
+    Implements the RTF compression format. This compression algorithm is used, for example, to
+    compress RTF data in Outlook messages.
+    """
+    @Unit.Requires('compressed_rtf', 'formats', 'office', 'default', 'extended')
+    def _rtfc():
+        import compressed_rtf
+        return compressed_rtf
+
+    def process(self, data):
+        return self._rtfc.decompress(data)
+
+    def reverse(self, data):
+        return self._rtfc.compress(data)
