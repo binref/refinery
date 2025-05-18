8d05746ede161111fb6b916afb530eafd2059d92
jesko <rattle@nullteilerfrei.de>
2025-05-18 18:39:05 +0200

    try backing off for test sample downloads

--- samples/__init__.py
+++ samples/__init__.py
@@ -1,8 +1,10 @@
 from typing import Optional, Dict
 
 import hashlib
-import os
+import urllib.error
 import urllib.request
+import time
+import threading
 
 from refinery.units.crypto.cipher.aes import aes
 
@@ -12,38 +14,51 @@ class SampleStore:
 
     def __init__(self):
         self.cache = {}
+        self._lock = threading.Lock()
+        self._wait = 0.1
 
-    def download(self, sha256hash: str, key: Optional[str] = None):
+    def _download(self, sha256hash: str, key: Optional[str] = None, timeout: int = 60):
         def tobytearray(r):
             if isinstance(r, bytearray):
                 return r
             return bytearray(r)
         key = key or 'REFINERYTESTDATA'
         key = key.encode('latin1')
-        sha256hash = sha256hash.lower()
-        timeout = 60
+        remaining = timeout
+        wait = self._wait
+        backoff = 0
         req = F'https://github.com/binref/refinery-test-data/blob/master/{sha256hash}.enc?raw=true'
-        try:
-            with urllib.request.urlopen(req, timeout=timeout) as response:
-                encoded_sample = tobytearray(response.read())
-        except Exception:
-            api = os.environ['MALSHARE_API']
-            req = F'https://malshare.com/api.php?api_key={api}&action=getfile&hash={sha256hash}'
-            with urllib.request.urlopen(req, timeout=timeout) as response:
-                result = tobytearray(response.read())
-        else:
-            result = encoded_sample | aes(mode='CBC', key=key) | bytearray
-            if not result or hashlib.sha256(result).hexdigest().lower() != sha256hash:
-                raise ValueError('sample did not decode correctly')
-        self.cache[sha256hash] = result
-        return result
+        while remaining > 0:
+            clock = time.time()
+            time.sleep(wait)
+            try:
+                with urllib.request.urlopen(req, timeout=remaining) as response:
+                    encoded_sample = tobytearray(response.read())
+            except urllib.error.HTTPError as error:
+                if error.code != 429:
+                    raise
+                backoff += 1
+                wait *= 2
+            else:
+                if not backoff:
+                    wait = max(0.1, wait / 2)
+                self._wait = wait
+                result = encoded_sample | aes(mode='CBC', key=key) | bytearray
+                if not result or hashlib.sha256(result).hexdigest().lower() != sha256hash:
+                    raise ValueError(F'The sample {sha256hash} did not decode correctly with key {key}.')
+                self.cache[sha256hash] = result
+                return result
+            remaining -= time.time() - clock
+        raise LookupError(F'Timeout exceeded while looking for {sha256hash}, backed off {backoff} times.')
 
     def get(self, sha256hash: str, key: Optional[str] = None):
-        for cached, value in self.cache.items():
-            if cached.casefold() == sha256hash.casefold():
-                return value
-        else:
-            return self.download(sha256hash, key)
+        sha256hash = sha256hash.lower()
+        with self._lock:
+            for cached, value in self.cache.items():
+                if cached == sha256hash:
+                    return value
+            else:
+                return self._download(sha256hash, key)
 
     def __getitem__(self, sha256hash: str):
         return self.get(sha256hash)
