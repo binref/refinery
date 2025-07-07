7d5147522eed041c4ff8665c2f0b4b3d9821c6a2
jesko <rattle@nullteilerfrei.de>
2025-07-07 17:33:39 +0200

    test and bugfix for ipv6 pattern

--- test/lib/test_patterns.py
+++ test/lib/test_patterns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from __future__ import annotations
+
 import re
 
 from inspect import getdoc as multiline
@@ -8,26 +10,39 @@ from refinery.lib.patterns import indicators, formats
 from .. import TestBase
 
 
-class TestIndicators(TestBase):
+class RegexTextBase(TestBase):
+
+    def assertMatches(self, pattern: indicators | formats, _string: str):
+        for conv, string in ((str, _string), (bytes, _string.encode('latin1'))):
+            self.assertTrue(re.fullmatch(conv(pattern), string),
+                msg=F'The string "{string}" did not match the pattern {pattern.name} as {conv.__name__}')
+            self.assertListEqual([string], [match[0] for match in re.finditer(conv(pattern), string)],
+                msg=F'The string "{string}" not recovered by pattern {pattern.name} as {conv.__name__}')
+
+
+class TestIndicators(RegexTextBase):
 
     def test_ipv4_too_large_ocets(self):
         self.assertFalse(re.fullmatch(str(indicators.ipv4), '127.0.0.288'))
 
     def test_ipv4_almost_too_large_ocet(self):
-        self.assertTrue(re.fullmatch(str(indicators.ipv4), '13.203.240.255'))
+        self.assertMatches(indicators.ipv4, '13.203.240.255')
+
+    def test_ipv6_colon_colon_digit(self):
+        self.assertMatches(indicators.ipv6, '2b14:301:126:3c::7')
 
     def test_telegram_url(self):
-        self.assertTrue(re.fullmatch(str(indicators.url), 'https://t.me/binaryrefinerytest'))
+        self.assertMatches(indicators.url, 'https://t.me/binaryrefinerytest')
 
     def test_telegram_domain(self):
-        self.assertTrue(re.fullmatch(str(indicators.domain), 't.me'))
+        self.assertMatches(indicators.domain, 't.me')
 
     def test_date(self):
-        self.assertTrue(re.fullmatch(str(indicators.date), '2030-01-01T09:00:00'))
-        self.assertTrue(re.fullmatch(str(indicators.date), '2020-01-01T12:07:00'))
+        self.assertMatches(indicators.date, '2030-01-01T09:00:00')
+        self.assertMatches(indicators.date, '2020-01-01T12:07:00')
 
 
-class TestFormats(TestBase):
+class TestFormats(RegexTextBase):
 
     def test_ps1str_here_string_match(self):
         @multiline
@@ -60,8 +75,9 @@ class TestFormats(TestBase):
         self.assertTrue(data.endswith('</command:command>'))
 
     def test_guid_in_path(self):
-        self.assertTrue(indicators.path.fullmatch(
-            RB'C:\Users\W10PRO~1\AppData\Local\Temp\{CAE44DB5-22DC-4A76-B334-E77C8D459505}\word_data.bin'))
+        self.assertMatches(indicators.path,
+            R'C:\Users\W10PRO~1\AppData\Local\Temp\{CAE44DB5-22DC-4A76-B334-E77C8D459505}\word_data.bin')
 
     def test_two_part_path(self):
-        self.assertTrue(indicators.path.fullmatch(RB'/root/something_something_in_the_root.txt'))
+        self.assertMatches(indicators.path,
+            R'/root/something_something_in_the_root.txt')
