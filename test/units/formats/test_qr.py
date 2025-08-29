993c145a454115a66f7fd156a6c07b5df636ff19
jesko <rattle@nullteilerfrei.de>
2025-08-29 07:20:06 +0200

    adds QR code test

---
+++ test/units/formats/test_qr.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from .. import TestUnitBase
+
+
+class TestQRDecoder(TestUnitBase):
+
+    def test_real_world_qr_code_phishing(self):
+        data = self.download_sample('6bc6d99524b46def23295d8a52c8973651338053d142386fd5d4e9c25501c071')
+        test = data | self.ldu('xt', 'word/media/image2.png') | self.load() | self.ldu('urlfix') | str
+        self.assertEqual(test, 'https:''//''tolviaes''.''ru''.''com/MdsopLs/')
