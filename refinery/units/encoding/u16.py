e5e16c4db5ea862263d31c0bb69ea5cad03af07b
jesko <rattle@nullteilerfrei.de>
2025-09-07 14:57:15 +0200

    use text encoding guesser from id library

--- refinery/units/encoding/u16.py
+++ refinery/units/encoding/u16.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from refinery.units import Unit
+from refinery.lib.id import guess_text_encoding
 
 
 class u16(Unit):
@@ -8,18 +9,12 @@ class u16(Unit):
     Encodes and decodes UTF-16 encoded string data.
     """
 
-    def reverse(self, data):
+    def reverse(self, data: bytearray):
         return data.decode(self.codec).encode('utf-16LE')
 
-    def process(self, data):
+    def process(self, data: bytearray):
         return data.decode('utf-16').encode(self.codec)
 
     @classmethod
     def handles(cls, data: bytearray):
-        view = memoryview(data)
-        if len(view) % 2 != 0:
-            return False
-        if not any(view[1:0x100:2]):
-            return True
-        if not any(view[0:0x100:2]):
-            return any(view[:4])
+        return guess_text_encoding(data) == 2
