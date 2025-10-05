40480a8ae83a2eb6bcc6c0588749e8c3cd718620
jesko <rattle@nullteilerfrei.de>
2025-10-05 10:20:50 +0200

    adds disabled pypdf support in case we need it for #97

--- refinery/units/formats/pdf.py
+++ refinery/units/formats/pdf.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 import json
 import re
 
-from typing import TYPE_CHECKING
+from itertools import islice
+from typing import TYPE_CHECKING, cast
 
 if TYPE_CHECKING:
     from pikepdf import Object
     from pymupdf import Page
+    from pypdf.generic import EncodedStreamObject
 
 from refinery.lib.mime import get_cached_file_magic_info
 from refinery.lib.structures import MemoryFile
@@ -23,6 +25,12 @@ class xtpdf(ArchiveUnit):
     """
     Extract objects from PDF documents.
     """
+    # @ArchiveUnit.Requires('pypdf>=3.1.0')
+    # def _pypdf2():
+    #     import pypdf
+    #     import pypdf.generic
+    #     return pypdf
+
     @ArchiveUnit.Requires('pikepdf<=9.5', ['formats', 'default', 'extended'])
     def _pikepdf():
         import pikepdf
@@ -36,7 +44,92 @@ class xtpdf(ArchiveUnit):
         import pymupdf
         return pymupdf
 
-    def _walk_raw(self, blob: Object, memo: list[Object] | None = None, *keys):
+    def _walk_pypdf2(self, blob, memo: set[int] | None = None, *path):
+        lib = self._pypdf2
+
+        while isinstance(blob, lib.generic.IndirectObject):
+            try:
+                blob = blob.get_object()
+            except Exception:
+                break
+        if memo is None:
+            memo = {id(blob)}
+        elif id(blob) in memo:
+            return
+        else:
+            memo.add(id(blob))
+        try:
+            name = blob['/F']
+            blob = blob['/EF']['/F']
+        except Exception:
+            pass
+        else:
+            def unhex(match):
+                return bytes.fromhex(match[1]).decode('latin1')
+            name = re.sub('#([0-9a-fA-F]{2})', unhex, name)
+            path = *path[:-1], F'/{name}'
+        try:
+            def extract():
+                with NoLogging():
+                    return get_data()
+            if TYPE_CHECKING:
+                blob = cast(EncodedStreamObject, blob)
+            get_data = blob.get_data
+        except AttributeError:
+            pass
+        else:
+            yield UnpackResult(''.join(path), extract, kind='object')
+            return
+
+        if isinstance(blob, lib.generic.ByteStringObject):
+            yield UnpackResult(''.join(path), blob, kind='bytes')
+            return
+        if isinstance(blob, lib.generic.TextStringObject):
+            yield UnpackResult(''.join(path), blob.encode(self.codec), kind='string')
+            return
+
+        if isinstance(blob, (
+            lib.generic.BooleanObject,
+            lib.generic.ByteStringObject,
+            lib.generic.FloatObject,
+            lib.generic.NameObject,
+            lib.generic.NullObject,
+            lib.generic.NumberObject,
+            lib.generic.RectangleObject,
+        )):
+            # unhandled PDF objects
+            return
+
+        if isinstance(blob, lib.generic.TreeObject):
+            blob = list(blob)
+
+        pdf = lib.generic.PdfObject
+
+        if isinstance(blob, list):
+            if (
+                len(blob) % 2 == 0
+                and all(isinstance(key, str) for key in islice(iter(blob), 0, None, 2))
+                and all(isinstance(key, pdf) for key in islice(iter(blob), 1, None, 2))
+            ):
+                blob = dict(zip(*([iter(blob)] * 2)))
+            else:
+                for key, value in enumerate(blob):
+                    yield from self._walk_pypdf2(value, memo, *path, F'/{key}')
+                return
+
+        if not isdict(blob):
+            return
+
+        assert isinstance(blob, dict)
+
+        for key, value in blob.items():
+            if not isinstance(key, str):
+                continue
+            if not key.startswith('/'):
+                key = F'/{key}'
+            yield from self._walk_pypdf2(value, memo, *path, key)
+
+    def _walk_pike(self, blob: Object, memo: list[Object] | None = None, *keys):
         if memo is None:
             memo = [blob]
         elif blob in memo:
@@ -72,14 +165,14 @@ class xtpdf(ArchiveUnit):
                 nested[key] = value
                 done.add(key)
             for key, value in nested.items():
-                yield from self._walk_raw(value, memo, *keys, key)
+                yield from self._walk_pike(value, memo, *keys, key)
             if meta:
                 yield UnpackResult(path, blob.to_json(dereference=True))
                 return
         elif isinstance(blob, pike.Array):
             for key, value in enumerate(iter(blob)):
                 if isinstance(value, pike.Object):
-                    yield from self._walk_raw(value, memo, *keys, F'/{key}')
+                    yield from self._walk_pike(value, memo, *keys, F'/{key}')
             return
 
         try:
@@ -100,10 +193,9 @@ class xtpdf(ArchiveUnit):
         try:
             mu = self._mupdf.open(stream=data, filetype='pdf')
         except Exception:
-            mu = None
-            password = ''
+            mu = password = None
         else:
-            if password := self.args.pwd or '':
+            if password := self.args.pwd or None:
                 if mu.is_encrypted:
                     mu.authenticate(password)
                 else:
@@ -112,9 +204,14 @@ class xtpdf(ArchiveUnit):
             elif mu.is_encrypted:
                 raise ValueError('This PDF is password protected.')
 
-        with MemoryFile(data, output=bytes) as stream:
-            pdf = self._pikepdf.open(stream, password=password)
-            yield from self._walk_raw(pdf.trailer, None, 'raw')
+        with MemoryFile(data, output=bytes) as stream, NoLogging():
+            try:
+                pdf = self._pikepdf.open(stream, password=(password or ''))
+                yield from self._walk_pike(pdf.trailer, None, 'raw')
+            except Exception:
+                raise
+                pdf = self._pypdf2.PdfReader(stream, password=password)
+                yield from self._walk_pypdf2(pdf.trailer, None, 'raw')
 
         if mu is None:
             return
