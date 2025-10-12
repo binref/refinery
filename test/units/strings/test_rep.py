e277a0698592f5bff4030b74f3ceceba3528e2a2
jesko <rattle@nullteilerfrei.de>
2025-10-12 16:18:26 +0200

    adds a disabled failing test for variable visibility in handler prefixes

--- test/units/strings/test_rep.py
+++ test/units/strings/test_rep.py
@@ -10,3 +10,7 @@ class TestRep(TestUnitBase):
     def test_sequence(self):
         unit = self.load('range:4:7', 't')
         self.assertListEqual(list(chunk['t'] for chunk in B"" | unit), list(range(4, 7)))
+
+    def __test_variables_are_known_in_prefix(self):
+        test = 0 | self.load_pipeline('put a ref [| put b rep[e:len(a)]:$ | pf {a}{b} ]') | str
+        self.assertEqual(test, 'ref$$$')
