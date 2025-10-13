712605177347d7a7e2225c86b18656e94bfc7fcf
jesko <rattle@nullteilerfrei.de>
2025-10-13 10:59:22 +0200

    adds more missing magics

--- test/lib/test_id.py
+++ test/lib/test_id.py
@@ -6,6 +6,12 @@ class TestIDLib(TestBase):
     def test_all_pyc_magics(self):
         from refinery.lib.shared import xdis
         from refinery.lib.id import PycMagicPattern
-        for magic, version in xdis.magics.versions.items():
-            self.assertIsNotNone(PycMagicPattern.fullmatch(magic),
-                msg=F'pattern does not match magic {magic.hex()} for version {version}')
+        mismatches = [
+            (magic, version) for magic, version in xdis.magics.versions.items()
+            if PycMagicPattern.fullmatch(magic) is None
+        ]
+        errors = '\n'.join([
+            F'- {magic.hex().upper()} for version {version}' for magic, version in mismatches
+        ])
+        self.assertListEqual(mismatches, [],
+            msg=F'the following pyc magics were not matches:\n{errors}')
