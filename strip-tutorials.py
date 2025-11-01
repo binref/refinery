2fdf51bde215faee11c78c50975827092f83f119
jesko <rattle@nullteilerfrei.de>
2025-11-01 18:38:09 +0100

    use an incremental counter

--- strip-tutorials.py
+++ strip-tutorials.py
@@ -9,10 +9,11 @@ CK = 'execution_count'
 
 for path in pathlib.Path.cwd().glob('tutorials/notebooks/*.ipynb'):
     print(F'fixing {path}')
+    ctr = 0
     with path.open('r', encoding='utf8') as fd:
         notebook = json.load(fd)
     for cell in notebook['cells']:
         if CK in cell:
-            cell[CK] = 1
+            cell[CK] = ctr = ctr + 1
     with path.open('w') as fd:
         json.dump(notebook, fd, indent=1)
