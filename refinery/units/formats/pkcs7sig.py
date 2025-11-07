8b9b21b224b839f787f7663f0f42d46c5dc29da0
jesko <rattle@nullteilerfrei.de>
2025-11-07 18:57:43 +0100

    improve docstring for discoverability

--- refinery/units/formats/pkcs7sig.py
+++ refinery/units/formats/pkcs7sig.py
@@ -10,7 +10,10 @@ class pkcs7sig(Unit):
     """
     Converts PKCS7 encoded signatures into a human-readable JSON representation. This can be used
     to parse authenticode signatures appended to files that are not PE files to get the same output
-    that is produced by the pemeta unit.
+    that is produced by the pemeta unit. For example, this can be used to parse the code signature
+    embedded in MSI files as follows:
+
+        emit sample.msi | xtmsi Meta/DigitalSignature | pkcs7sig
     """
     def __init__(self, tabular: Param[bool, Arg('-t', help='Print information in a table rather than as JSON')] = False):
         super().__init__(tabular=tabular)
