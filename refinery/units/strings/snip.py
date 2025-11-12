6a0f7db8ef866c9aa495f0447ffad97c075cd3af
jesko <rattle@nullteilerfrei.de>
2025-11-12 22:24:36 +0100

    improves docstring

--- refinery/units/strings/snip.py
+++ refinery/units/strings/snip.py
@@ -6,12 +6,13 @@ from refinery.units import Arg, Unit
 
 class snip(Unit):
     """
-    Snips the input data based on a Python slice expression. For example, the
-    initialization `slice 0::1 1::1` would yield a unit that first extracts
-    every byte at an even position and then, every byte at an odd position. In
-    this case, multiple outputs are produced. The unit can be used in reverse
-    mode, in which case the specified ranges are deleted sequentially from the
-    input.
+    Snips the input data based on a Python slice expression. For example, the initialization
+
+        sbip 0::1 1::1
+
+    would yield a unit that first extracts every byte at an even position and then, every byte at
+    an odd position. In this case, multiple outputs are produced. This is a versatile tool that can
+    be used to skip certain bytes, extract substrings, or even reverse the input.
     """
     def __init__(
         self,
