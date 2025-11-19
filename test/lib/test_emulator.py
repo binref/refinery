9a7fb6b78e73231d289bd7121025cd0b540c0e9c
jesko <rattle@nullteilerfrei.de>
2025-11-19 09:32:19 +0100

    spreads out emulator tests

--- test/lib/test_emulator.py
+++ test/lib/test_emulator.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from refinery.lib.emulator import Arch, Hook, Emulator, UnicornEmulator, SpeakeasyEmulator, IcicleEmulator
+import unittest
+
+from refinery.lib.emulator import Arch, Hook, UnicornEmulator, SpeakeasyEmulator, IcicleEmulator
 from .. import TestBase
 
 
-def _makeEmulator(e: type[Emulator]):
+def _makeEmulator(e: type[UnicornEmulator]):
     class Emu(e):
         writes = []
 
@@ -25,7 +27,7 @@ def _makeEmulator(e: type[Emulator]):
 
 class TestEmulator(TestBase):
 
-    def test_single_stepping_x32(self):
+    def _test_single_stepping_x32(self, base_emu):
 
         data = bytes.fromhex(
             'A1 FF FF 01 00'        # mov     eax, [0x1FFFF]
@@ -36,80 +38,89 @@ class TestEmulator(TestBase):
             '66 A3 A2 B3 01 10'     # mov     [0x1001B3A2], ax
         )
 
-        for BaseEmulator in (
-            IcicleEmulator,
-            SpeakeasyEmulator,
-            UnicornEmulator,
-        ):
-            emulator = _makeEmulator(BaseEmulator)(data, arch=Arch.X32, hooks=Hook.Memory)
-            emulator.reset()
-            ip = emulator.base
+        emulator = _makeEmulator(base_emu)(data, arch=Arch.X32, hooks=Hook.Memory)
+        emulator.reset()
+        ip = emulator.base
 
-            ea = emulator.step(ip)
-            self.assertEqual(emulator.get_register('eax'), 0)
-            self.assertEqual(ea, ip := ip + 5)
+        ea = emulator.step(ip)
+        self.assertEqual(emulator.get_register('eax'), 0)
+        self.assertEqual(ea, ip := ip + 5)
 
-            ea = emulator.step(ip)
-            self.assertEqual(emulator.get_register('eax'), 2)
-            self.assertEqual(ea, ip := ip + 5)
+        ea = emulator.step(ip)
+        self.assertEqual(emulator.get_register('eax'), 2)
+        self.assertEqual(ea, ip := ip + 5)
 
-            ea = emulator.step(ip)
-            self.assertEqual(emulator.mem_read(0x1001B3A0, 2), B'\x02\x00')
-            self.assertEqual(ea, ip := ip + 6)
+        ea = emulator.step(ip)
+        self.assertEqual(emulator.mem_read(0x1001B3A0, 2), B'\x02\x00')
+        self.assertEqual(ea, ip := ip + 6)
 
-            ea = emulator.step(ip, 2)
-            self.assertEqual(emulator.pop(), ip + 11)
-            self.assertEqual(emulator.pop(), 0x1BB)
-            self.assertNotEqual(ea, ip := ip + 11)
-            emulator.set_register('eax', 0x1BB)
+        ea = emulator.step(ip, 2)
+        self.assertEqual(emulator.pop(), ip + 11)
+        self.assertEqual(emulator.pop(), 0x1BB)
+        self.assertNotEqual(ea, ip := ip + 11)
+        emulator.set_register('eax', 0x1BB)
 
-            ea = emulator.step(ip)
-            self.assertEqual(emulator.mem_read(0x1001B3A2, 2), B'\xBB\x01')
+        ea = emulator.step(ip)
+        self.assertEqual(emulator.mem_read(0x1001B3A2, 2), B'\xBB\x01')
 
-            self.assertEqual(emulator.writes.pop(), 443)
-            emulator.writes.pop()
-            self.assertListEqual(emulator.writes, [2, 443])
+        self.assertEqual(emulator.writes.pop(), 443)
+        emulator.writes.pop()
+        self.assertListEqual(emulator.writes, [2, 443])
 
-    def test_single_stepping_x64(self):
+    def test_single_stepping_x32_ic(self):
+        self._test_single_stepping_x32(IcicleEmulator)
+
+    def test_single_stepping_x32_uc(self):
+        self._test_single_stepping_x32(UnicornEmulator)
+
+    def test_single_stepping_x32_se(self):
+        self._test_single_stepping_x32(SpeakeasyEmulator)
+
+    def _test_single_stepping_x64(self, base_emu):
 
         data = bytes.fromhex(
             'B8 02 00 00 00'        # mov     eax, 2
-            '66 89 05 19 DC 01 00'  # mov     [eip+0x1DC19], ax
+            '66 89 05 19 DC 01 00'  # mov     [rip+0x1DC19], ax
             '66 B9 92 13'           # mov     cx, 0x1392
             'FF 15 AF 11 01 00'     # call    htons
-            '66 89 05 0A DC 01 00'  # mov     [eip+0x1DC0A], ax
+            '66 89 05 0A DC 01 00'  # mov     [rip+0x1DC0A], ax
         )
 
-        for BaseEmulator in (
-            IcicleEmulator,
-            SpeakeasyEmulator,
-            UnicornEmulator,
-        ):
-            emulator = _makeEmulator(BaseEmulator)(data, arch=Arch.X64, hooks=Hook.Memory)
-            emulator.reset()
-            ip = base = emulator.base
+        emulator = _makeEmulator(base_emu)(data, arch=Arch.X64, hooks=Hook.Memory)
+        emulator.reset()
 
-            ea = emulator.step(ip)
-            self.assertEqual(emulator.get_register('eax'), 2)
-            self.assertEqual(ea, ip := ip + 5)
+        ip = base = emulator.base
 
-            ea = emulator.step(ip)
-            self.assertEqual(emulator.mem_read(base + 0x1dc25, 2), B'\x02\x00')
-            self.assertEqual(ea, ip := ip + 7)
+        ea = emulator.step(ip)
+        self.assertEqual(emulator.get_register('eax'), 2)
+        self.assertEqual(ea, ip := ip + 5)
 
-            ea = emulator.step(ip, 2)
-            self.assertEqual(emulator.pop(), ip := ip + 10)
-            self.assertEqual(emulator.get_register('ecx') & 0xFFFF, 0x1392)
-            emulator.set_register('eax', 0x1BB)
+        ea = emulator.step(ip)
+        self.assertEqual(emulator.mem_read(base + 0x1dc25, 2), B'\x02\x00')
+        self.assertEqual(ea, ip := ip + 7)
 
-            ea = emulator.step(ip)
-            self.assertEqual(emulator.mem_read(base + 0x1dc27, 2), B'\xBB\x01')
+        ea = emulator.step(ip, 2)
+        self.assertEqual(emulator.pop(), ip := ip + 10)
+        self.assertEqual(emulator.get_register('ecx') & 0xFFFF, 0x1392)
+        emulator.set_register('eax', 0x1BB)
 
-            self.assertEqual(emulator.writes.pop(), 443)
-            emulator.writes.pop()
-            self.assertListEqual(emulator.writes, [2])
+        ea = emulator.step(ip)
+        self.assertEqual(emulator.mem_read(base + 0x1dc27, 2), B'\xBB\x01')
 
-    def test_registers(self):
+        self.assertEqual(emulator.writes.pop(), 443)
+        emulator.writes.pop()
+        self.assertListEqual(emulator.writes, [2])
+
+    def test_single_stepping_x64_ic(self):
+        self._test_single_stepping_x64(IcicleEmulator)
+
+    def test_single_stepping_x64_uc(self):
+        self._test_single_stepping_x64(UnicornEmulator)
+
+    def test_single_stepping_x64_se(self):
+        self._test_single_stepping_x64(SpeakeasyEmulator)
+
+    def _test_registers(self, base_emu):
         code = (
             b'\x55'                       # push   rbp
             b'\x53'                       # push   rbx
@@ -123,13 +134,19 @@ class TestEmulator(TestBase):
             b'\x90'                       # nop
         )
 
-        for BaseEmulator in (
-            IcicleEmulator,
-            UnicornEmulator,
-        ):
-            emulator = _makeEmulator(BaseEmulator)(code, arch=Arch.X64)
-            emulator.reset()
-            emulator.set_register('edx', 1)
-            emulator.emulate(0, len(code))
-            value = emulator.pop().to_bytes(8, 'little')
-            self.assertEqual(value, B'REFINERY')
+        emulator = _makeEmulator(base_emu)(code, arch=Arch.X64)
+        emulator.reset()
+        emulator.set_register('edx', 1)
+        emulator.emulate(emulator.base, len(code))
+        value = emulator.pop().to_bytes(8, 'little')
+        self.assertEqual(value, B'REFINERY')
+
+    def test_register_ic(self):
+        self._test_registers(IcicleEmulator)
+
+    def test_register_uc(self):
+        self._test_registers(UnicornEmulator)
+
+    @unittest.skip('Fails for unknown reasons')
+    def test_register_se(self):
+        self._test_registers(SpeakeasyEmulator)
