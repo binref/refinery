30ec079397900cfdeed2671a3c9ecd0aade11596
jesko <rattle@nullteilerfrei.de>
2025-11-19 09:45:56 +0100

    add options for various styles

--- refinery/units/sinks/hl.py
+++ refinery/units/sinks/hl.py
@@ -24,14 +24,30 @@ class hl(Unit):
             help='Optionally specify the input language to be highlighted.')] = None,
         style: Param[str | None, Arg.String('-s', group='STYLE',
             help='Optionally specify a color style supported by Pygments.')] = None,
-        dark: Param[bool, Arg.Switch('-d', group='STYLE',
-            help='Use the Pygments color scheme for a dark brackground.')] = False,
-        light: Param[bool, Arg.Switch('-l', group='STYLE',
-            help='Use the Pygments color scheme for a light background.')] = False,
+        github: Param[bool, Arg.Switch('-G', group='STYLE',
+            help='Use github-flavored styling.')] = False,
+        solarized: Param[bool, Arg.Switch('-S', group='STYLE',
+            help='Use solarized-flavored styling.')] = False,
+        gruvbox: Param[bool, Arg.Switch('-B', group='STYLE',
+            help='Use gruvbox-flavored styling.')] = False,
+        dark: Param[bool, Arg.Switch('-d',
+            help='Assume a dark brackground.')] = False,
+        light: Param[bool, Arg.Switch('-l',
+            help='Assume a light background.')] = False,
     ):
         if dark and light:
             raise ValueError('The "dark" and "light" options cannot simultaneously be set.')
-        return super().__init__(lexer=lexer, style=style, dark=dark, light=light)
+        if sum(1 for opt in (github, solarized, gruvbox, style) if opt) > 1:
+            raise ValueError('More than one styling option was set.')
+        return super().__init__(
+            lexer=lexer,
+            style=style,
+            dark=dark,
+            light=light,
+            github=github,
+            solarized=solarized,
+            gruvbox=gruvbox,
+        )
 
     @Unit.Requires('Pygments', ['display', 'extended'])
     def _pygments():
@@ -41,6 +57,9 @@ class hl(Unit):
         import pygments.style
         import pygments.token
         return pygments
+
+    def _style_variant(self):
+        return 'light' if self.args.light else 'dark'
 
     def process(self, data):
         lib = self._pygments
@@ -67,14 +86,21 @@ class hl(Unit):
                 lexer = lib.lexers.guess_lexer(data)
 
         self.log_info(F'using lexer for {lexer.name.lower()}')
+        t256 = lib.formatters.Terminal256Formatter
 
-        if self.args.light:
+        if self.args.github:
+            tf = t256(style=F'github-{self._style_variant()}')
+        elif self.args.solarized:
+            tf = t256(style=F'solarized-{self._style_variant()}')
+        elif self.args.gruvbox:
+            tf = t256(style=F'gruvbox-{self._style_variant()}')
+        elif self.args.light:
             tf = lib.formatters.TerminalFormatter(bg='light')
         elif self.args.dark:
             tf = lib.formatters.TerminalFormatter(bg='dark')
         else:
             if not (style := self.args.style):
-                class style(lib.style.Style):
+                class _style(lib.style.Style):
                     background_color = 'default'
                     R = 'ansibrightred'
                     C = 'ansibrightcyan'
@@ -92,8 +118,24 @@ class hl(Unit):
                         token.Number      : C,
                         token.Literal     : C,
                     }
-            tf = lib.formatters.Terminal256Formatter(style=style)
+                style = _style
+            tf = t256(style=style)
 
         out = lib.highlight(data, lexer, tf)
         out = F'{out}{colorama.Style.RESET_ALL}'
         return out.encode(self.codec)
+
+
+class hlg(hl, docs='{0}{s}This variant uses GitHub styling.'):
+    def __init__(self, lexer: str | None = None, dark: bool = False, light: bool = False):
+        super().__init__(lexer, dark=dark, light=light, github=True)
+
+
+class hls(hl, docs='{0}{s}This variant uses solarized styling.'):
+    def __init__(self, lexer: str | None = None, dark: bool = False, light: bool = False):
+        super().__init__(lexer, dark=dark, light=light, solarized=True)
+
+
+class hlb(hl, docs='{0}{s}This variant uses gruvbox styling.'):
+    def __init__(self, lexer: str | None = None, dark: bool = False, light: bool = False):
+        super().__init__(lexer, dark=dark, light=light, gruvbox=True)
