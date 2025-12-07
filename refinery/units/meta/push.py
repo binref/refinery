e124ffbefbd5a8b5b23c83482184949d5f8d8579
jesko <rattle@nullteilerfrei.de>
2025-12-07 13:40:16 +0100

    cosmetics

--- refinery/units/meta/push.py
+++ refinery/units/meta/push.py
@@ -6,18 +6,20 @@ from refinery.units import Arg, Chunk, Unit
 
 class push(Unit):
     """
-    The unit inserts an additional chunk before each input chunk and moves the original
-    data out of scope. This chunk is considered the "original" data, while the one inserted
-    in front of it is used as an intermediate result. By default, this intermediate data is
-    a copy of the input data. For example:
+    The unit inserts an additional chunk before each input chunk and moves the original data out of
+    scope. This chunk is considered the "original" data, while the one inserted in front of it is
+    used as an intermediate result. By default, this intermediate data is a copy of the input data.
+    For example:
 
         emit key=value | push [[| rex =(.*)$ {1} | pop v ]| repl var:v censored ]
 
-    will output `key=censored`. The application of `refinery.rex` turns the (duplicated)
-    data into just the value, which is then stored in the variable `v`. The application
-    of `refinery.repl` replaces this value with the hard-coded string `censored`.
+    will output `key=censored`. The application of `refinery.rex` turns the (duplicated) data into
+    just the value, which is then stored in the variable `v`. Next, application of `refinery.repl`
+    replaces this value with the hard-coded string `censored`.
     """
-    def __init__(self, data: Param[buf, Arg(help='The data to be pushed, by default a copy of the input.')] = B''):
+    def __init__(self, data: Param[buf,
+        Arg(help='The data to be pushed, by default a copy of the input.')] = B''
+    ):
         super().__init__(data=data)
 
     def process(self, data: Chunk):
