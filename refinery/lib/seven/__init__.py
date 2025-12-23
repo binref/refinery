1b1e818ae6f1a1be3764206f6eae617d53b27a70
jesko <rattle@nullteilerfrei.de>
2025-12-23 08:16:22 +0100

    adds a docstring

---
+++ refinery/lib/seven/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module contains compression algorithms that were ported from the 7zip source code. They are
+pure Python implementations, so their performance is fairly poor, but at the time of writing there
+is no reasonable alternative if we want to use these algorithms.
+"""
