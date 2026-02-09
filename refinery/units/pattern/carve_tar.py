2d3ed732bdde62ee31c6e246223b51a13430362a
jesko <rattle@nullteilerfrei.de>
2026-02-09 21:33:39 +0100

    adds tar file carver

---
+++ refinery/units/pattern/carve_tar.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+import tarfile
+
+from refinery.lib.structures import MemoryFile
+from refinery.units import Unit
+
+
+class carve_tar(Unit):
+    """
+    Extracts anything from the input data that looks like a tar archive file.
+    """
+    def process(self, data: bytearray):
+        memory = memoryview(data)
+        stream = MemoryFile(data)
+        offset = 0
+        while (p := data.find(B'ustar', offset)) > 0:
+            stream.seekset(start := p - 0x101)
+            try:
+                success = False
+                with tarfile.open(mode='r|*', fileobj=stream) as t:
+                    while t.next():
+                        success = True
+            except Exception:
+                success = False
+            if success:
+                offset = stream.tell()
+                yield memory[start:offset]
+            else:
+                offset = p + 1
