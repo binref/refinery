fa4b620ee93626cf389681e1198434620566c413
jesko <rattle@nullteilerfrei.de>
2026-03-05 22:19:19 +0100

    adds regression test

--- test/units/formats/test_msi.py
+++ test/units/formats/test_msi.py
@@ -12,6 +12,36 @@ class TestMSI(TestUnitBase):
     def test_real_world_01(self):
         data = self.download_sample('5316330427de98c60661233369909132f6b838b5ff111f7f29efe3e90636a34a')
         msi = data | self.load() | {'path': ...}
+        for path in (
+            'MsiTables/ActionText.csv',
+            'MsiTables/AdminExecuteSequence.csv',
+            'MsiTables/AdminUISequence.csv',
+            'MsiTables/AdvtExecuteSequence.csv',
+            'MsiTables/Binary.csv',
+            'MsiTables/CheckBox.csv',
+            'MsiTables/Component.csv',
+            'MsiTables/Control.csv',
+            'MsiTables/ControlCondition.csv',
+            'MsiTables/ControlEvent.csv',
+            'MsiTables/CustomAction.csv',
+            'MsiTables/Dialog.csv',
+            'MsiTables/Directory.csv',
+            'MsiTables/Error.csv',
+            'MsiTables/EventMapping.csv',
+            'MsiTables/Feature.csv',
+            'MsiTables/FeatureComponents.csv',
+            'MsiTables/InstallExecuteSequence.csv',
+            'MsiTables/InstallUISequence.csv',
+            'MsiTables/LaunchCondition.csv',
+            'MsiTables/Property.csv',
+            'MsiTables/RadioButton.csv',
+            'MsiTables/Registry.csv',
+            'MsiTables/TextStyle.csv',
+            'MsiTables/UIText.csv',
+            'MsiTables/Upgrade.csv',
+            'MsiTables/Validation.csv',
+        ):
+            self.assertIn(path, msi)
         meta = json.loads(msi['MsiTables.json'])
         self.assertEqual(meta['Property'][19]['Value'], "Típica")
         self.assertContains(msi['Action/basicsmarch.js'], b'FDWPLOVWRX(PIENNNAXXI+ QRQHCMZIEY,NAXXIPIENN+ BJHZRJLZFX)')
