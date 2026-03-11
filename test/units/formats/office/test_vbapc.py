748711fd5fcd82bf16ad725965fc956f6187e264
jesko <rattle@nullteilerfrei.de>
2026-03-11 08:06:55 +0100

    fixes the test

--- test/units/formats/office/test_vbapc.py
+++ test/units/formats/office/test_vbapc.py
@@ -8,13 +8,15 @@ class TestVBAPC(TestUnitBase):
         data = self.download_sample('4bdc8e660ff4fb05e5b6c0a2dd70c537817f46ac3270d779fdddc8e459829c08')
         unit = self.load()
         code = list(data | unit)
-        self.assertIn(B'http://109.94.209'B'.91/12340.txt', code[0])
+        self.assertTrue(any(
+            B'http://109.94.209'B'.91/12340.txt' in c for c in code))
 
     def test_maldoc_02(self):
         data = self.download_sample('ee103f8d64cd8fa884ff6a041db2f7aa403c502f54e26337c606044c2f205394')
         unit = self.load()
         code = list(data | unit)
-        self.assertIn(B'ActiveDocument.Content.Find.Execute FindText:="$1", ReplaceWith:=dowKarolYou, Replace:=wdReplaceAll', code[0])
+        self.assertTrue(any(
+            B'ActiveDocument.Content.Find.Execute FindText:="$1", ReplaceWith:=dowKarolYou, Replace:=wdReplaceAll' in c for c in code))
 
     def test_stomped_document_01(self):
         data = self.download_sample('6d8a0f5949adf37330348cc9a231958ad8fb3ea3a3d905abe5e72dbfd75a3d1d')
