8eae5ec99df0ace4d2f718851ab890cedf3f43a3
jesko <rattle@nullteilerfrei.de>
2026-03-11 08:08:31 +0100

    easy switch for running tests with coverage

--- mptest.bat
+++ mptest.bat
@@ -1,1 +1,5 @@
-@pytest -n auto -m "not cosmetics" --dist=loadgroup --disable-warnings %* test 2>%~n0.errors
+if "%1"=="--cov" (
+    @pytest -n auto -m "not cosmetics" --dist=loadgroup --disable-warnings --cov-config=.coveragerc --cov refinery test 2>NUL
+) else (
+    @pytest -n auto -m "not cosmetics" --dist=loadgroup --disable-warnings %* test 2>%~n0.errors
+)
