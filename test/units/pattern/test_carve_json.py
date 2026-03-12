412316341cfa986c9a77542b400d4795b97c2c25
jesko <rattle@nullteilerfrei.de>
2026-03-12 10:43:09 +0100

    adds cheap tests

--- test/units/pattern/test_carve_json.py
+++ test/units/pattern/test_carve_json.py
@@ -35,3 +35,97 @@ class TestCarveJSON(TestUnitBase):
             "problem": false
         }""".encode('latin1')
         self.assertEqual(data, data | self.load() | bytes)
+
+    def test_carve_simple_dict(self):
+        data = b'garbage{"key":"value"}more_garbage'
+        unit = self.load()
+        parsed = data | unit | json.loads
+        self.assertEqual(parsed, {"key": "value"})
+
+    def test_carve_nested_json(self):
+        data = b'junk{"a":{"b":1}}junk'
+        unit = self.load()
+        parsed = data | unit | json.loads
+        self.assertEqual(parsed, {"a": {"b": 1}})
+
+    def test_carve_list_with_all_flag(self):
+        data = b'garbage[1,2,3]more'
+        unit = self.load('-a')
+        results = data | unit | []
+        self.assertTrue(len(results) > 0)
+        parsed = json.loads(bytes(results[0]))
+        self.assertEqual(parsed, [1, 2, 3])
+
+    def test_carve_no_json(self):
+        data = b'no json here'
+        unit = self.load()
+        results = data | unit | []
+        self.assertEqual(len(results), 0)
+
+    def test_carve_dict_only_default(self):
+        data = b'garbage[1,2,3]more'
+        unit = self.load()
+        results = data | unit | []
+        self.assertEqual(len(results), 0)
+
+    def test_carve_json_from_surrounding_text(self):
+        data = b'Here is some log output: {"status":"ok","code":200} and then more text follows.'
+        unit = self.load()
+        parsed = data | unit | json.loads
+        self.assertEqual(parsed['status'], 'ok')
+        self.assertEqual(parsed['code'], 200)
+
+    def test_carve_deeply_nested_object(self):
+        obj = {"level1": {"level2": {"level3": {"level4": "deep_value"}}}}
+        payload = b'PREFIX' + json.dumps(obj).encode() + b'SUFFIX'
+        unit = self.load()
+        result = unit(payload)
+        parsed = json.loads(result)
+        self.assertEqual(parsed['level1']['level2']['level3']['level4'], 'deep_value')
+
+    def test_carve_json_array_with_all_flag(self):
+        data = b'before [{"a":1},{"b":2}] after'
+        unit = self.load('-a')
+        results = data | unit | []
+        self.assertEqual(len(results), 1)
+        parsed = json.loads(bytes(results[0]))
+        self.assertIsInstance(parsed, list)
+        self.assertEqual(len(parsed), 2)
+        self.assertEqual(parsed[0]['a'], 1)
+        self.assertEqual(parsed[1]['b'], 2)
+
+    def test_carve_multiple_json_objects_in_stream(self):
+        obj1 = {"first": True}
+        obj2 = {"second": False}
+        data = b'AAA' + json.dumps(obj1).encode() + b'BBB' + json.dumps(obj2).encode() + b'CCC'
+        unit = self.load()
+        results = data | unit | []
+        self.assertEqual(len(results), 2)
+        parsed1 = json.loads(bytes(results[0]))
+        parsed2 = json.loads(bytes(results[1]))
+        self.assertEqual(parsed1, {"first": True})
+        self.assertEqual(parsed2, {"second": False})
+
+    def test_malformed_json_is_skipped(self):
+        data = b'start {"valid":"json"} middle {"broken: no_end more text'
+        unit = self.load()
+        results = data | unit | []
+        self.assertEqual(len(results), 1)
+        parsed = json.loads(bytes(results[0]))
+        self.assertEqual(parsed, {"valid": "json"})
+
+    def test_empty_object_is_skipped(self):
+        data = b'text {} more text {"key":"val"} end'
+        unit = self.load()
+        results = data | unit | []
+        self.assertEqual(len(results), 1)
+        parsed = json.loads(bytes(results[0]))
+        self.assertEqual(parsed, {"key": "val"})
+
+    def test_nested_arrays_with_all_flag(self):
+        data = b'noise [[1,2],[3,4]] noise'
+        unit = self.load('-a')
+        results = data | unit | []
+        self.assertEqual(len(results), 1)
+        parsed = json.loads(bytes(results[0]))
+        self.assertEqual(parsed, [[1, 2], [3, 4]])
