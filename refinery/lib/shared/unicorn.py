89206be2a3d102c796dba083ed67adee3399aa1f
jesko <rattle@nullteilerfrei.de>
2026-03-13 20:49:23 +0100

    removes buggy setuptools import

--- refinery/lib/shared/unicorn.py
+++ refinery/lib/shared/unicorn.py
@@ -4,8 +4,6 @@ from refinery.lib.tools import NoLogging
 
 @dependency('unicorn>=2.0.1.post1', ['default', 'extended'])
 def unicorn():
-    import importlib
-    importlib.import_module('setuptools')
     with NoLogging():
         import unicorn
         import unicorn.unicorn
