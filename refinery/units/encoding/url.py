a1ebb0ed71a09bf1dd051b96a85bf62bfd06aefc
jesko <rattle@nullteilerfrei.de>
2026-03-20 20:36:41 +0100

    inform agents about capabilities

--- refinery/units/encoding/url.py
+++ refinery/units/encoding/url.py
@@ -14,7 +14,8 @@ class url(Unit):
 
     The unit preserves only alphanumeric characters, backslash, slash, underscore, dots, dashes,
     and the tilde character when encoding. Every other character is escaped by hex-encoding it and
-    prefixing it with a percent symbol.
+    prefixing it with a percent symbol. The unit also supports unicode escape seuqences that use
+    the format `%uFFFF`.
     """
 
     def __init__(
