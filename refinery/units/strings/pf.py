6e1f235f83fee5c433065722b62c8e6be372da96
jesko <rattle@nullteilerfrei.de>
2026-04-05 16:01:00 +0200

    use escape sequences in pf by default

--- refinery/units/strings/pf.py
+++ refinery/units/strings/pf.py
@@ -9,16 +9,15 @@ class pf(Unit):
     """
     Print, format, and transform data using a format string expression.
 
-    Stands for "Print Format". The positional format string placeholder `{}` will be replaced
-    by the incoming data, named
-    placeholders have to exist as meta variables in the current chunk. For example, the following
-    pipeline can be used to print all files in a given directory with their corresponding SHA-256
-    hash:
+    Stands for "Print Format". The positional format string placeholder `{}` will be replaced by
+    the incoming data, named placeholders have to exist as meta variables in the current chunk.
+    For example, the following pipeline can be used to print all files in a given directory with
+    their corresponding SHA-256 hash:
 
         ef ** [| sha256 -t | pf {} {path} ]]
 
     By default, format string arguments are simply joined along a space character to form a single
-    format string.
+    format string. Backslash escape sequences are unescaped by default.
     """
 
     def __init__(
@@ -29,27 +28,30 @@ class pf(Unit):
             help='Separator to insert between format strings. The default is a space character.')] = ' ',
         multiplex: Param[bool, Arg.Switch('-m', group='SEP',
             help='Do not join the format strings along the separator, generate one output for each.')] = False,
-        unescape: Param[bool, Arg.Switch('-e', help='Interpret escape sequences in format strings.')] = False,
+        raw: Param[bool, Arg.Switch('-r', help='Do not interpret backslash-escape sequences in format strings.')] = False,
     ):
         def fixfmt(fmt: bytes | str):
-            if unescape:
-                if isinstance(fmt, str):
-                    fmt = fmt.encode('latin1')
-                return bytes(fmt).decode('unicode-escape')
-            elif not isinstance(fmt, str):
+            if not isinstance(fmt, str):
                 fmt = bytes(fmt).decode(self.codec)
             return fmt
         _formats = [fixfmt(f) for f in formats]
         if not multiplex:
             _formats = [fixfmt(separator).join(_formats)]
-        super().__init__(formats=_formats, variable=variable)
+        super().__init__(formats=_formats, variable=variable, raw=raw)
 
     def process(self, data):
         meta = metavars(data)
         args = [data]
         variable = self.args.variable
+        escaped = not self.args.raw
         for spec in self.args.formats:
-            result = meta.format_bin(spec, codec=self.codec, args=args, lenient=True)
+            result = meta.format_bin(
+                spec,
+                codec=self.codec,
+                args=args,
+                escaped=escaped,
+                lenient=True
+            )
             if variable:
                 result = self.labelled(data, **{variable: result})
             yield result
--- test/units/strings/test_pf.py
+++ test/units/strings/test_pf.py
@@ -7,7 +7,7 @@ class TestFormatter(TestUnitBase):
 
     def test_with_escape_sequence(self):
         data = B'refinery!'
-        unit = self.load(R'{}\n', unescape=True)
+        unit = self.load(R'{}\n')
         self.assertEqual(unit(data), B'refinery!\n')
 
     def test_utf8_metadata_value(self):
@@ -17,23 +17,23 @@ class TestFormatter(TestUnitBase):
         self.assertEqual(result.decode(unit.codec), message)
 
     def test_linebreak_01(self):
-        self.assertEqual(B'XX' | self.load('{{\\n:_}}{}') | str, '{\\n:_}XX')
+        self.assertEqual(B'XX' | self.load('{{\\n:_}}{}', raw=True) | str, '{\\n:_}XX')
 
     def test_linebreak_02(self):
-        self.assertEqual(B'XX' | self.load('A\\nB\\n{}') | str, 'A\\nB\\nXX')
+        self.assertEqual(B'XX' | self.load('A\\nB\\n{}', raw=True) | str, 'A\\nB\\nXX')
 
     def test_linebreak_03(self):
-        self.assertEqual(B'XX' | self.load('A{\\n!n}B\\n{}') | str, 'A\nB\\nXX')
-        self.assertEqual(B'XX' | self.load('A{0A!h}B\\n{}') | str, 'A\nB\\nXX')
-        self.assertEqual(B'XX' | self.load('A{%0a!q}B\\n{}') | str, 'A\nB\\nXX')
-        self.assertEqual(B'XX' | self.load('{A\\nB\\n!n}{}') | str, 'A\nB\nXX')
+        self.assertEqual(B'XX' | self.load('A{\\n!n}B\\n{}', raw=True) | str, 'A\nB\\nXX')
+        self.assertEqual(B'XX' | self.load('A{0A!h}B\\n{}', raw=True) | str, 'A\nB\\nXX')
+        self.assertEqual(B'XX' | self.load('A{%0a!q}B\\n{}', raw=True) | str, 'A\nB\\nXX')
+        self.assertEqual(B'XX' | self.load('{A\\nB\\n!n}{}', raw=True) | str, 'A\nB\nXX')
 
     def test_crc32(self):
         self.assertEqual(B'X' | self.load(r'{size} {crc32:hex:be}') | str, '1 3081909835')
 
     def test_escaped_formats(self):
-        for u, g in [(False, 'Y\\nX'), (True, 'Y\nX')]:
-            self.assertEqual(B'X' | self.load(r'Y\n{}', unescape=u) | str, g)
+        for u, g in [(True, 'Y\\nX'), (False, 'Y\nX')]:
+            self.assertEqual(B'X' | self.load(r'Y\n{}', raw=u) | str, g)
 
     def test_formatting_something_that_is_also_a_handler(self):
         self.assertEqual(B'access.h' | self.load('{:px}') | str, 'h')
