aa35f6b03f55072b0aeb8f7fb0fb178a4cb9aa40
jesko <rattle@nullteilerfrei.de>
2026-04-20 07:46:28 +0200

    switch to worksteal

--- mptest.bat
+++ mptest.bat
@@ -1,5 +1,5 @@
 if "%1"=="--cov" (
-    @pytest -n auto -m "not cosmetics" --dist=loadgroup --disable-warnings --cov-config=.coveragerc --cov refinery test 2>NUL
+    @pytest -n auto -m "not cosmetics" --dist=worksteal --disable-warnings --cov-config=.coveragerc --cov refinery test 2>NUL
 ) else (
-    @pytest -n auto -m "not cosmetics" --dist=loadgroup --disable-warnings %* test 2>%~n0.errors
+    @pytest -n auto -m "not cosmetics" --dist=worksteal --disable-warnings %* test 2>%~n0.errors
 )
