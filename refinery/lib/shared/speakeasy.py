6e7ebad7dde89c978eee5cae5fc0e4c51903cacf
jesko <rattle@nullteilerfrei.de>
2026-05-12 20:39:18 +0200

    upgrade to speakeasy v2 (prerelease)

--- refinery/lib/shared/speakeasy.py
+++ refinery/lib/shared/speakeasy.py
@@ -1,7 +1,7 @@
 from refinery.lib.shared import dependency
 
 
-@dependency('speakeasy-emulator-refined==1.6.1b0.post3', 3)
+@dependency('speakeasy-emulator==2.0.0b3', 3)
 def speakeasy():
     import speakeasy
     import speakeasy.profiler
